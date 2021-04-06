aa6822cb7ec15d25a4598e7b708867996cc03201
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-04-06 19:15:22 +0800

    replace Terminus to Terminus, Inc

--- modules/orchestrator/events/publish_ws.go
+++ modules/orchestrator/events/publish_ws.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Terminus
+// Copyright 2021 Terminus, Inc.
 //
 // This program is free software: you can use, redistribute, and/or modify
 // it under the terms of the GNU Affero General Public License, version 3
