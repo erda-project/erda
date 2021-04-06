46d4116b323f6f00919caeb94f1c1fb63b56e878
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-04-06 19:28:42 +0800

    Fix Copyright

--- pkg/aliyunclient/config.go
+++ pkg/aliyunclient/config.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Terminus, Inc.
+// Copyright (c) 2021 Terminus, Inc.
 //
 // This program is free software: you can use, redistribute, and/or modify
 // it under the terms of the GNU Affero General Public License, version 3
@@ -11,20 +11,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package aliyunclient
 
 import (
@@ -32,7 +18,7 @@ import (
 	"time"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/utils"
-)
+)git
 
 type Config struct {
 	AutoRetry         bool              `default:"true"`
