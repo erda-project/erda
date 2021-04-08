aa78f54ed92fc026944a08763dbf78ff37f497e9
luoyu <o.sparkli@outlook.com>
2021-04-08 20:06:02 +0800

    FIX CI problems

--- modules/monitor/common/common.go
+++ modules/monitor/common/common.go
@@ -14,17 +14,6 @@
 package common
 
 import (
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
-
-	"github.com/erda-project/erda-infra/base/servicehub"
-	"github.com/erda-project/erda-infra/modcom/api"
-	"github.com/erda-project/erda-infra/providers/i18n"
-	"github.com/erda-project/erda/modules/monitor/common/addon/env"
-	"github.com/erda-project/erda/pkg/version"
-	"github.com/recallsong/go-utils/config"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -33,79 +22,4 @@ var instanceID = uuid.NewV4().String()
 // InstanceID .
 func InstanceID() string {
 	return instanceID
-}
-
-// Env .
-func Env() {
-	config.LoadEnvFile()
-}
-
-func loadModuleEnvFile(dir string) {
-	path := filepath.Join(dir, ".env."+os.Getenv("DICE_SIZE"))
-	config.LoadEnvFileWithPath(path, false)
-}
-
-// Hub global variable
-var Hub *servicehub.Hub
-
-func prepare() {
-	version.PrintIfCommand()
-	Env()
-	env.Override()
-	for _, fn := range initializers {
-		fn()
-	}
-}
-
-var initializers []func()
-
-// RegisterInitializer .
-func RegisterInitializer(fn func()) {
-	initializers = append(initializers, fn)
-}
-
-// Run .
-func Run(cfg string) {
-	prepare()
-	Hub = servicehub.New(servicehub.WithListener(&listener{}))
-	Hub.Run("", cfg, os.Args...)
-}
-
-// RunWithCfgDir .
-func RunWithCfgDir(dir, name string) {
-	prepare()
-	name = GetEnv("CONF_NAME", name)
-	dir = strings.TrimRight(dir, "/")
-	os.Setenv("CONF_PATH", dir)
-	loadModuleEnvFile(dir)
-	cfg := path.Join(dir, name+GetEnv("CONFIG_SUFFIX", ".yml"))
-	Hub = servicehub.New(servicehub.WithListener(&listener{}))
-	Hub.Run("", cfg, os.Args...)
-}
-
-type listener struct{}
-
-func (l *listener) BeforeInitialization(h *servicehub.Hub, config map[string]interface{}) error {
-	if _, ok := config["i18n"]; !ok {
-		config["i18n"] = nil // i18n is required
-	}
-	return nil
-}
-
-func (l *listener) BeforeExit(h *servicehub.Hub, err error) error {
-	return nil
-}
-
-func (l *listener) AfterInitialization(h *servicehub.Hub) error {
-	api.I18n = h.Service("i18n").(i18n.I18n)
-	return nil
-}
-
-// GetEnv .
-func GetEnv(key, def string) string {
-	v := os.Getenv(key)
-	if len(v) > 0 {
-		return v
-	}
-	return def
 }
