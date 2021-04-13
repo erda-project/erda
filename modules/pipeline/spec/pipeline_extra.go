792ceff14694cd6313525a4eeea90e29150876d4
sfwn <sfwnlin@gmail.com>
2021-04-13 15:33:50 +0800

    pipeline store applied resources info (#167)
    * pkg numeral support calculate MaxFloat64 & MinFloat64
    
    * Add struct to store pipeline applied resources
    
    * Pipeline store applied resources when create graph

--- modules/pipeline/spec/pipeline_extra.go
+++ modules/pipeline/spec/pipeline_extra.go
@@ -128,6 +128,9 @@ type Snapshot struct {
 
 	// OutputValues output 定义和从 task 里采集上来的值
 	OutputValues []apistructs.PipelineOutputWithValue `json:"outputValues,omitempty"`
+
+	// AppliedResources calculated by all actions
+	AppliedResources apistructs.PipelineAppliedResources `json:"appliedResources,omitempty"`
 }
 
 // FromDB 兼容 Snapshot 老数据
