e1fb2e22e2566d0c7afe6357c585bf4ef522a297
sfwn <sfwnlin@gmail.com>
2021-05-06 11:36:13 +0800

    API-TEST support judge whether to use netportal by host

--- pkg/apitestsv2/netportal.go
+++ pkg/apitestsv2/netportal.go
@@ -16,6 +16,10 @@ package apitestsv2
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/erda-project/erda/apistructs"
 	"github.com/erda-project/erda/pkg/customhttp"
@@ -23,10 +27,15 @@ import (
 	"github.com/erda-project/erda/pkg/strutil"
 )
 
-func handleCustomNetportalRequest(apiReq *apistructs.APIRequestInfo, netPortalURL string) (*http.Request, error) {
-	if netPortalURL != "" {
-		apiReq.URL = strutil.Concat(netPortalURL, "/", httpclientutil.RmProto(apiReq.URL))
+const k8sServiceSuffix = ".svc.cluster.local"
+
+func handleCustomNetportalRequest(apiReq *apistructs.APIRequestInfo, netportalOpt *netportalOption) (*http.Request, error) {
+	useNetportal := useNetportal(apiReq.URL, netportalOpt)
+	if !useNetportal {
+		return http.NewRequest(apiReq.Method, apiReq.URL, nil)
 	}
+	// use netportal
+	apiReq.URL = strutil.Concat(netportalOpt.url, "/", httpclientutil.RmProto(apiReq.URL))
 
 	customReq, err := customhttp.NewRequest(apiReq.Method, apiReq.URL, nil)
 	if err != nil {
@@ -39,4 +48,40 @@ func handleCustomNetportalRequest(apiReq *apistructs.APIRequestInfo, netPortalUR
 	}
 
 	return customReq, nil
+}
+
+// useNetportal return true or false to represent use netportal or not.
+func useNetportal(inputURL string, netportalOpt *netportalOption) bool {
+	// cannot use if no netportal url
+	if netportalOpt == nil || netportalOpt.url == "" {
+		return false
+	}
+	// only host have k8s service suffix will use netportal
+	r, err := url.ParseRequestURI(inputURL)
+	if err != nil {
+		logrus.Errorf("failed to parse apitest url, url: %s, err: %v", inputURL, err)
+		// if err, not use netportal
+		return false
+	}
+	hostport := r.Host
+	ss := strings.SplitN(hostport, ":", 2)
+	host := ss[0]
+	// doesn't have k8s svc suffix, do not use nerportal
+	if !strings.HasSuffix(host, k8sServiceSuffix) {
+		return false
+	}
+	// if parsed k8s namespace is blacklist, do not use netportal
+	ns := getK8sNamespace(host)
+	inBlacklist := strutil.Exist(netportalOpt.blacklistOfK8sNamespaceAccess, ns)
+	if inBlacklist {
+		return false
+	}
+	return true
+}
+
+func getK8sNamespace(k8sHost string) string {
+	hostWithoutSuf := strings.TrimSuffix(k8sHost, k8sServiceSuffix)
+	ss := strings.Split(hostWithoutSuf, ".")
+	ns := ss[len(ss)-1]
+	return ns
 }
