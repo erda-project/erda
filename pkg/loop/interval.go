fe7eaf0ab52b3233848fd47ce9461a8f2d89c2c7
joey <zhengcheng.zheng@alibaba-inc.com>
2021-05-19 15:20:22 +0800

    modify loop calculate interval to make interval is accurate

--- pkg/loop/interval.go
+++ pkg/loop/interval.go
@@ -20,8 +20,13 @@ import (
 
 // CalculateInterval 传入当前循环次数，计算间隔时间
 func (l *Loop) CalculateInterval(loopedTimes uint64) time.Duration {
-	// 间隔 等于 初始间隔 乘以 衰退比例的 loopedTimes 次方
-	interval := time.Duration(float64(l.interval) * math.Pow(l.declineRatio, float64(loopedTimes+1)))
+	// if loopedTimes == 0 Means the first execution of the task, the time interval should be 0
+	if loopedTimes == 0 {
+		return time.Duration(0)
+	}
+	// 间隔 等于 初始间隔 乘以 衰退比例的 loopedTimes-1 次方
+	// change loopedTimes+1 to loopedTimes-1 to make interval is accurate
+	interval := time.Duration(float64(l.interval) * math.Pow(l.declineRatio, float64(loopedTimes-1)))
 	// 间隔 不能大于衰退限制
 	if interval > l.declineLimit {
 		interval = l.declineLimit
--- pkg/loop/interval_test.go
+++ pkg/loop/interval_test.go
@@ -22,21 +22,27 @@ import (
 )
 
 func TestLoop_CalculateInterval(t *testing.T) {
-	// 2, 4, 8, 10, 10
+	// 0, 1, 2, 4, 8, 10, 10
 	l := New(WithMaxTimes(math.MaxUint64), WithDeclineLimit(10*time.Second), WithDeclineRatio(2))
 
 	interval := l.CalculateInterval(0)
-	assert.Equal(t, time.Second*2, interval)
+	assert.Equal(t, time.Second*0, interval)
 
 	interval = l.CalculateInterval(1)
-	assert.Equal(t, time.Second*4, interval)
+	assert.Equal(t, time.Second*1, interval)
 
 	interval = l.CalculateInterval(2)
-	assert.Equal(t, time.Second*8, interval)
+	assert.Equal(t, time.Second*2, interval)
 
 	interval = l.CalculateInterval(3)
-	assert.Equal(t, time.Second*10, interval)
+	assert.Equal(t, time.Second*4, interval)
 
 	interval = l.CalculateInterval(4)
+	assert.Equal(t, time.Second*8, interval)
+
+	interval = l.CalculateInterval(5)
+	assert.Equal(t, time.Second*10, interval)
+
+	interval = l.CalculateInterval(6)
 	assert.Equal(t, time.Second*10, interval)
 }
