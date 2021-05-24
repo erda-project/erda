a62ec6b50372f4e70349e536134f9829485a019a
littlejian <17816869670@163.com>
2021-05-24 23:18:09 +0800

    fix: add base64 decoder of request body to avoid WAF recognition (#424)
    Co-authored-by: sfwn <sfwnlin@gmail.com>

---
+++ modules/gittar/pkg/util/http_test.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package util
+
+import (
+	"bytes"
+	"gotest.tools/assert"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestHandleRequest(t *testing.T) {
+	req := &http.Request{
+		Body:   io.NopCloser(bytes.NewReader([]byte("aGVsbG8gZXJkYQ=="))),
+		Header: map[string][]string{},
+	}
+	req.Header.Set("base64-encoded-request-body", "true")
+	HandleRequest(req)
+	b, _ := ioutil.ReadAll(req.Body)
+	assert.Equal(t, "hello erda", string(b))
+}
