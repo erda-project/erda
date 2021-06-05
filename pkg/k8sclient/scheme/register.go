423adf691c2abc2e2749206c09b3e9fb77699778
Asher Liu <root@viper.run>
2021-06-05 22:31:46 +0800

    feature: new kubernetes client with controller-runtime and clientSet (#534)
    Co-authored-by: luobily <lbl167612@alibaba-inc.com>

---
+++ pkg/k8sclient/scheme/register.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package scheme
+
+import (
+	sparkoperatorv1beta2 "github.com/GoogleCloudPlatform/spark-on-k8s-operator/pkg/apis/sparkoperator.k8s.io/v1beta2"
+	elasticsearchv1 "github.com/elastic/cloud-on-k8s/pkg/apis/elasticsearch/v1"
+	istioconfigv1alpha2 "istio.io/client-go/pkg/apis/config/v1alpha2"
+	istionetworkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
+	istionetworkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
+	istiorbacv1alpha1 "istio.io/client-go/pkg/apis/rbac/v1alpha1"
+	istiosecv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8sschema "k8s.io/client-go/kubernetes/scheme"
+)
+
+// LocalSchemeBuilder register crd scheme
+var LocalSchemeBuilder = runtime.SchemeBuilder{
+	k8sschema.AddToScheme,
+	elasticsearchv1.AddToScheme,
+	sparkoperatorv1beta2.AddToScheme,
+	istioconfigv1alpha2.AddToScheme,
+	istionetworkingv1beta1.AddToScheme,
+	istionetworkingv1alpha3.AddToScheme,
+	istiorbacv1alpha1.AddToScheme,
+	istiosecv1beta1.AddToScheme,
+}
