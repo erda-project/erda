6362c72d329bfbd64b6c7750a3848fb8e65ca2f1
littlejian <17816869670@163.com>
2021-06-08 19:39:21 +0800

    feature: move qa apim gittar-adaptor to dop

---
+++ modules/dop/services/i18n/i18n.go
@@ -0,0 +1,106 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package i18n
+
+const (
+	I18nKeyTestPlanSheetName = "tp.export.testplan.sheet.name"
+	I18nKeyTestCaseSheetName = "tp.export.testcase.sheet.name"
+
+	I18nKeyCaseNum          = "tp.export.case.num"
+	I18nKeyCaseName         = "tp.export.case.name"
+	I18nKeyCaseSetName      = "tp.export.case.set.name"
+	I18nKeyCasePriority     = "tp.export.case.priority"
+	I18nKeyCasePrecondition = "tp.export.case.precondition"
+	I18nKeyCaseStepResult   = "tp.export.case.step.and.result"
+	I18nKeyCaseStep         = "tp.export.case.step"
+	I18nKeyCaseExpectResult = "tp.export.case.expect.result"
+	I18nKeyCaseExecutor     = "tp.export.case.executor"
+	I18nKeyCaseExecResult   = "tp.export.case.exec.result"
+
+	I18nKeyCaseAPITest          = "tp.export.case.apitest"
+	I18nKeyCaseAPITestName      = "tp.export.case.apitest.name"
+	I18nKeyCaseAPITestHeaders   = "tp.export.case.apitest.headers"
+	I18nKeyCaseAPITestMethod    = "tp.export.case.apitest.method"
+	I18nKeyCaseAPITestUrl       = "tp.export.case.apitest.url"
+	I18nKeyCaseAPITestParams    = "tp.export.case.apitest.params"
+	I18nKeyCaseAPITestBody      = "tp.export.case.apitest.body"
+	I18nKeyCaseAPITestOutParams = "tp.export.case.apitest.outparams"
+	I18nKeyCaseAPITestAsserts   = "tp.export.case.apitest.asserts"
+
+	I18nKeySpaceSheetName   = "tp.export.space.sheet.name"
+	I18nKeySpaceNum         = "tp.export.space.num"
+	I18nKeySpaceName        = "tp.export.space.name"
+	I18nKeySpaceProjectNum  = "tp.export.space.project.num"
+	I18nKeySpaceDescription = "tp.export.space.description"
+
+	I18nKeySceneSetSheetName   = "tp.export.scene.set.sheet.name"
+	I18nKeySceneSetNum         = "tp.export.scene.set.num"
+	I18nKeySceneSetName        = "tp.export.scene.set.name"
+	I18nKeySceneSetSpaceNum    = "tp.export.scene.set.space.num"
+	I18nKeySceneSetPreNum      = "tp.export.scene.set.pre.num"
+	I18nKeySceneSetDescription = "tp.export.scene.set.description"
+
+	I18nKeySceneSheetName   = "tp.export.scene.sheet.name"
+	I18nKeySceneNum         = "tp.export.scene.num"
+	I18nKeySceneName        = "tp.export.scene.name"
+	I18nKeySceneSceneSetNum = "tp.export.scene.scene.set.num"
+	I18nKeySceneSpaceNum    = "tp.export.scene.space.num"
+	I18nKeyScenePreNum      = "tp.export.scene.pre.num"
+	I18nKeySceneRefSetNum   = "tp.export.scene.ref.set.num"
+	I18nKeySceneDescription = "tp.export.scene.description"
+
+	I18nKeySceneInputSheetName   = "tp.export.scene.input.sheet.name"
+	I18nKeySceneInputNum         = "tp.export.scene.input.num"
+	I18nKeySceneInputName        = "tp.export.scene.input.name"
+	I18nKeySceneInputValue       = "tp.export.scene.input.value"
+	I18nKeySceneInputTemp        = "tp.export.scene.input.temp"
+	I18nKeySceneInputSceneNum    = "tp.export.scene.input.scene.num"
+	I18nKeySceneInputSpaceNum    = "tp.export.scene.input.space.num"
+	I18nKeySceneInputDescription = "tp.export.scene.input.description"
+
+	I18nKeySceneOutputSheetName   = "tp.export.scene.output.sheet.name"
+	I18nKeySceneOutputNum         = "tp.export.scene.output.num"
+	I18nKeySceneOutputName        = "tp.export.scene.output.name"
+	I18nKeySceneOutputValue       = "tp.export.scene.output.value"
+	I18nKeySceneOutputSceneNum    = "tp.export.scene.output.scene.num"
+	I18nKeySceneOutputSpaceNum    = "tp.export.scene.output.space.num"
+	I18nKeySceneOutputDescription = "tp.export.scene.output.description"
+
+	I18nKeySceneStepSheetName  = "tp.export.scene.step.sheet.name"
+	I18nKeySceneStepNum        = "tp.export.scene.step.num"
+	I18nKeySceneStepName       = "tp.export.scene.step.name"
+	I18nKeySceneStepValue      = "tp.export.scene.step.value"
+	I18nKeySceneStepType       = "tp.export.scene.step.type"
+	I18nKeySceneStepPreNum     = "tp.export.scene.step.pre.num"
+	I18nKeySceneStepSceneNum   = "tp.export.scene.step.scene.num"
+	I18nKeySceneStepSpaceNum   = "tp.export.scene.step.space.num"
+	I18nKeySceneStepPreType    = "tp.export.scene.step.pre.type"
+	I18nKeySceneStepApiSpecNum = "tp.export.scene.step.api.spec.num"
+
+	I18nKeyConfigSheetName   = "tp.export.config.sheet.name"
+	I18nKeyConfigScopeName   = "tp.export.config.scope.name"
+	I18nKeyConfigScopeNum    = "tp.export.config.scope.num"
+	I18nKeyConfigNsName      = "tp.export.config.ns.name"
+	I18nKeyConfigDisplayName = "tp.export.config.display.name"
+	I18nKeyConfigDescription = "tp.export.config.description"
+	I18nKeyConfigDomain      = "tp.export.config.domain"
+	I18nKeyConfigHeader      = "tp.export.config.header"
+
+	I18nKeyConfigGlobalSheetName   = "tp.export.config.global.sheet.name"
+	I18nKeyConfigGlobalNsName      = "tp.export.config.global.ns.name"
+	I18nKeyConfigGlobalName        = "tp.export.config.global.name"
+	I18nKeyConfigGlobalType        = "tp.export.config.global.type"
+	I18nKeyConfigGlobalValue       = "tp.export.config.global.value"
+	I18nKeyConfigGlobalDescription = "tp.export.config.global.description"
+)
