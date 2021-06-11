274f666d7442a4de44e9d3ac4301f9ad8ea67750
RecallSong <13607438+recallsong@users.noreply.github.com>
2021-06-11 17:42:27 +0800

    protobuf data validate, config center apis permission (#608)
    * 1、implement nacos apis
    2、protobuf api data validate
    
    * check config center apis permission
    
    * format import

--- modules/msp/configcenter/provider.go
+++ modules/msp/configcenter/provider.go
@@ -14,14 +14,20 @@
 package configcenter
 
 import (
+	"net/http"
+
 	"github.com/jinzhu/gorm"
 
 	"github.com/erda-project/erda-infra/base/logs"
 	"github.com/erda-project/erda-infra/base/servicehub"
 	"github.com/erda-project/erda-infra/pkg/transport"
+	transhttp "github.com/erda-project/erda-infra/pkg/transport/http"
+	"github.com/erda-project/erda-infra/pkg/transport/http/encoding"
 	"github.com/erda-project/erda-proto-go/msp/configcenter/pb"
 	instancedb "github.com/erda-project/erda/modules/msp/instance/db"
+	mperm "github.com/erda-project/erda/modules/msp/instance/permission"
 	"github.com/erda-project/erda/pkg/common/apis"
+	perm "github.com/erda-project/erda/pkg/common/permission"
 )
 
 type config struct {
@@ -33,6 +39,8 @@ type provider struct {
 	Log                 logs.Logger
 	Register            transport.Register `autowired:"service-register" optional:"true"`
 	DB                  *gorm.DB           `autowired:"mysql-client"`
+	Perm                perm.Interface     `autowired:"permission"`
+	MPerm               mperm.Interface    `autowired:"msp.permission"`
 	configCenterService *configCenterService
 }
 
@@ -43,7 +51,25 @@ func (p *provider) Init(ctx servicehub.Context) error {
 		instanceDB:       &instancedb.InstanceDB{DB: p.DB},
 	}
 	if p.Register != nil {
-		pb.RegisterConfigCenterServiceImp(p.Register, p.configCenterService, apis.Options())
+		type ConfigCenterService = pb.ConfigCenterServiceServer
+		pb.RegisterConfigCenterServiceImp(p.Register, p.configCenterService, apis.Options(),
+			transport.WithHTTPOptions(transhttp.WithEncoder(func(rw http.ResponseWriter, r *http.Request, data interface{}) error {
+				// compatibility with api "/api/tmc/config/tenants/{tenantID}/groups/{groupID}" response
+				if resp, ok := data.(*pb.GetGroupPropertiesResponse); ok && resp != nil {
+					m := make(map[string]interface{})
+					for _, item := range resp.Data {
+						m[item.Group] = item.Properties
+					}
+					data = m
+				}
+				return encoding.EncodeResponse(rw, r, data)
+			})),
+			p.Perm.Check(
+				perm.Method(ConfigCenterService.GetGroups, perm.ScopeProject, "config-center_group", perm.ActionGet, p.MPerm.TenantToProjectID("", "TenantID")),
+				perm.Method(ConfigCenterService.GetGroupProperties, perm.ScopeProject, "config-center_properties", perm.ActionGet, p.MPerm.TenantToProjectID("", "TenantID")),
+				perm.Method(ConfigCenterService.SaveGroupProperties, perm.ScopeProject, "config-center_properties", perm.ActionUpdate, p.MPerm.TenantToProjectID("", "TenantID")),
+			),
+		)
 	}
 	return nil
 }
