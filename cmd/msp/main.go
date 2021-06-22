152221ffb21a0aa486eef1e446485027d46d4650
luoyu <O.Sparkli@outlook.com>
2021-06-22 09:29:32 +0800

    gofmt

--- cmd/msp/main.go
+++ cmd/msp/main.go
@@ -21,11 +21,11 @@ import (
 	// modules and providers
 	_ "github.com/erda-project/erda-infra/providers"
 	_ "github.com/erda-project/erda-proto-go/msp/menu/client"
+	_ "github.com/erda-project/erda/modules/msp/apm/tmc/metric"
 	_ "github.com/erda-project/erda/modules/msp/configcenter"
 	_ "github.com/erda-project/erda/modules/msp/instance/permission"
 	_ "github.com/erda-project/erda/modules/msp/menu"
 	_ "github.com/erda-project/erda/modules/msp/registercenter"
-	_ "github.com/erda-project/erda/modules/msp/apm/tmc/metric"
 	_ "github.com/erda-project/erda/pkg/common/permission"
 )
 
