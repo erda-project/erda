c1737819555abbd50b568faee15a831f16975a02
innerpeacez <innerpeace.zhai@gmail.com>
2021-07-01 21:29:47 +0800

    Implement Excption API (#738)
    * method getExceptions()
    
    * method GetExceptionEventIds()
    
    * implement exception api
    
    * license header
    
    * fix lint
    
    * close auto generate keyspace
    
    * update
    
    * format yaml
    
    * format

---
+++ modules/msp/apm/exception/exception.service_test.go
@@ -0,0 +1,194 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package exception
+
+import (
+	context "context"
+	reflect "reflect"
+	testing "testing"
+
+	servicehub "github.com/erda-project/erda-infra/base/servicehub"
+	pb "github.com/erda-project/erda-proto-go/msp/apm/exception/pb"
+)
+
+func Test_exceptionService_GetExceptions(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.GetExceptionsRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.GetExceptionsResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.apm.exception.ExceptionService",
+		//			`
+		//erda.msp.apm.exception:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.GetExceptionsRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.GetExceptionsResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ExceptionServiceServer)
+			got, err := srv.GetExceptions(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("exceptionService.GetExceptions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("exceptionService.GetExceptions() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
+
+func Test_exceptionService_GetExceptionEventIds(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.GetExceptionEventIdsRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.GetExceptionEventIdsResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.apm.exception.ExceptionService",
+		//			`
+		//erda.msp.apm.exception:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.GetExceptionEventIdsRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.GetExceptionEventIdsResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ExceptionServiceServer)
+			got, err := srv.GetExceptionEventIds(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("exceptionService.GetExceptionEventIds() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("exceptionService.GetExceptionEventIds() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
+
+func Test_exceptionService_GetExceptionEvent(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.GetExceptionEventRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.GetExceptionEventResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.apm.exception.ExceptionService",
+		//			`
+		//erda.msp.apm.exception:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.GetExceptionEventRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.GetExceptionEventResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ExceptionServiceServer)
+			got, err := srv.GetExceptionEvent(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("exceptionService.GetExceptionEvent() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("exceptionService.GetExceptionEvent() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
