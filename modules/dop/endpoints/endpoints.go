ef71a63f9f3696dd1408173dee4bec3d09c35cc0
chengjoey <30427474+chengjoey@users.noreply.github.com>
2021-07-04 14:26:03 +0800

    export and import autotest space sync (#761)
    Co-authored-by: erda-bot <81558540+erda-bot@users.noreply.github.com>

--- modules/dop/endpoints/endpoints.go
+++ modules/dop/endpoints/endpoints.go
@@ -350,7 +350,7 @@ func (e *Endpoints) Routes() []httpserver.Endpoint {
 		{Path: "/api/autotests/spaces/{id}", Method: http.MethodGet, Handler: e.GetAutoTestSpace},
 		{Path: "/api/autotests/spaces/{id}", Method: http.MethodDelete, Handler: e.DeleteAutoTestSpace},
 		{Path: "/api/autotests/spaces/actions/copy", Method: http.MethodPost, Handler: e.CopyAutoTestSpaceV2},
-		{Path: "/api/autotests/spaces/actions/export", Method: http.MethodPost, WriterHandler: e.ExportAutoTestSpace},
+		{Path: "/api/autotests/spaces/actions/export", Method: http.MethodPost, Handler: e.ExportAutoTestSpace},
 		{Path: "/api/autotests/spaces/actions/import", Method: http.MethodPost, Handler: e.ImportAutotestSpace},
 
 		// 自动化测试 - 场景
@@ -973,4 +973,8 @@ func init() {
 
 func (e *Endpoints) TestCaseService() *testcase.Service {
 	return e.testcase
+}
+
+func (e *Endpoints) AutotestV2Service() *atv2.Service {
+	return e.autotestV2
 }
