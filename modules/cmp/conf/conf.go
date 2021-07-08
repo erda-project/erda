ea276293100f13a39ee9f37990304e63bef62581
iutx <root@viper.run>
2021-07-08 12:02:54 +0800

    feature: cmp support new import method

--- modules/cmp/conf/conf.go
+++ modules/cmp/conf/conf.go
@@ -22,13 +22,28 @@ import (
 
 // Conf Define the configuration
 type Conf struct {
-	Debug             bool          `env:"DEBUG" default:"false"`
-	EnableEss         bool          `env:"ENABLE_ESS" default:"false"`
-	ListenAddr        string        `env:"LISTEN_ADDR" default:":9027"`
-	SoldierAddr       string        `env:"SOLDIER_ADDR"`
-	SchedulerAddr     string        `env:"SCHEDULER_ADDR"`
-	TaskSyncDuration  time.Duration `env:"TASK_SYNC_DURATION" default:"2h"`
-	TaskCleanDuration time.Duration `env:"TASK_CLEAN_DURATION" default:"24h"`
+	Debug              bool          `env:"DEBUG" default:"false"`
+	EnableEss          bool          `env:"ENABLE_ESS" default:"false"`
+	ListenAddr         string        `env:"LISTEN_ADDR" default:":9027"`
+	SoldierAddr        string        `env:"SOLDIER_ADDR"`
+	SchedulerAddr      string        `env:"SCHEDULER_ADDR"`
+	TaskSyncDuration   time.Duration `env:"TASK_SYNC_DURATION" default:"2h"`
+	TaskCleanDuration  time.Duration `env:"TASK_CLEAN_DURATION" default:"24h"`
+	LocalMode          bool          `env:"LOCAL_MODE" default:"false"`
+	RedisMasterName    string        `default:"my-master" env:"REDIS_MASTER_NAME"`
+	RedisSentinelAddrs string        `default:"" env:"REDIS_SENTINELS_ADDR"`
+	RedisAddr          string        `default:"127.0.0.1:6379" env:"REDIS_ADDR"`
+	RedisPwd           string        `default:"anywhere" env:"REDIS_PASSWORD"`
+	UCClientID         string        `env:"UC_CLIENT_ID"`
+	UCClientSecret     string        `env:"UC_CLIENT_SECRET"`
+	// ory/kratos config
+	OryEnabled           bool   `default:"false" env:"ORY_ENABLED"`
+	OryKratosAddr        string `default:"kratos:4433" env:"KRATOS_ADDR"`
+	OryKratosPrivateAddr string `default:"kratos:4434" env:"KRATOS_PRIVATE_ADDR"`
+
+	ErdaNamespace        string `default:"erda-system" env:"ERDA_NAMESPACE"`
+	ErdaHelmChartVersion string `default:"0.1.0" env:"ERDA_HELM_CHART_VERSION"`
+	ReleaseRepo          string `default:"registry.erda.cloud" env:"RELEASE_REPO"`
 }
 
 var cfg Conf
@@ -68,4 +83,66 @@ func TaskSyncDuration() time.Duration {
 
 func TaskCleanDuration() time.Duration {
 	return cfg.TaskCleanDuration
+}
+
+func LocalMode() bool {
+	return cfg.LocalMode
+}
+
+// RedisMasterName 返回redis master name
+func RedisMasterName() string {
+	return cfg.RedisMasterName
+}
+
+// RedisSentinelAddrs 返回 redis 哨兵地址
+func RedisSentinelAddrs() string {
+	return cfg.RedisSentinelAddrs
+}
+
+// RedisAddr 返回 redis 地址
+func RedisAddr() string {
+	return cfg.RedisAddr
+}
+
+// RedisPwd 返回 redis 密码
+func RedisPwd() string {
+	return cfg.RedisPwd
+}
+
+// UCClientID 返回 UCClientID 选项.
+func UCClientID() string {
+	return cfg.UCClientID
+}
+
+// UCClientSecret 返回 UCClientSecret 选项.
+func UCClientSecret() string {
+	return cfg.UCClientSecret
+}
+
+func OryEnabled() bool {
+	return cfg.OryEnabled
+}
+
+func OryKratosPrivateAddr() string {
+	return cfg.OryKratosPrivateAddr
+}
+
+func OryCompatibleClientID() string {
+	return "kratos"
+}
+
+func OryCompatibleClientSecret() string {
+	return ""
+}
+
+func ErdaNamespace() string {
+	return cfg.ErdaNamespace
+}
+
+func ErdaHelmChartVersion() string {
+	return cfg.ErdaHelmChartVersion
+}
+
+func ReleaseRepo() string {
+	return cfg.ReleaseRepo
 }
