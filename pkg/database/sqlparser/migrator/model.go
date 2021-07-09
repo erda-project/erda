9d111073b577f2496e5dd26fc5a455ac28a725d5
悟空 <rainchan365@163.com>
2021-07-09 10:39:35 +0800

    feature: support migration in python (#813)
    * feature: add migrate cli
    
    * feature: support py migration
    
    * fix register center bug (#791)
    
    * fix register center bug
    
    * fix service ip permission
    
    * fix register-center-bug bug
    
    * fix test.http
    
    feature: add migrate record cmd
    
    feature: add some ut
    
    * pipeline loop task generate params add current task status, because loop is before update current task db storage status (#808)
    
    Co-authored-by: erda-bot <81558540+erda-bot@users.noreply.github.com>
    
    feature: gofmt
    
    * feature: fix an ut
    
    * use %d to replace with %v
    
    Co-authored-by: 澄潭 <ztywto@qq.com>
    
    * feature: fix logrus format
    
    * feature: remove prepare
    
    * feature: support to set which python to execute script
    
    Co-authored-by: RecallSong <13607438+recallsong@users.noreply.github.com>
    Co-authored-by: kakj <18579115540@163.com>
    Co-authored-by: 澄潭 <ztywto@qq.com>

--- pkg/database/sqlparser/migrator/model.go
+++ pkg/database/sqlparser/migrator/model.go
@@ -15,55 +15,53 @@ package migrator
 
 import (
 	"time"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const SchemaMigrationHistory = `schema_migration_history`
 
-type HistoryModel struct {
-	ID        uint64    `gorm:"primarykey"`
-	CreatedAt time.Time `json:"createdAt" gorm:"created_at;type:datetime;not null;default CURRENT_TIMESTAMP"`
-	UpdatedAt time.Time `json:"updatedAt" gorm:"updated_at;type:datetime;not null;default CURRENT_TIMESTAMP; on update CURRENT_TIMESTAMP"`
+const CreateTableHistoryModel = `
+CREATE TABLE IF NOT EXISTS schema_migration_history (
+	id 			BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	created_at 	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at 	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-	ServiceName  string `json:"serviceName"  gorm:"service_name;type:varchar(32);not null;comment:微服务名"`
-	Filename     string `json:"filename"     gorm:"filename;type:varchar(191);not null;comment:脚本类型"`
-	Checksum     string `json:"checksum"     gorm:"checksum;type:char(128);not null;comment:rawtext 的 md5 值"`
-	InstalledBy  string `json:"installedBy"  gorm:"installed_by;type:varchar(32);not null;comment:执行人"`
-	InstalledOn  string `json:"installedOn"  gorm:"install_on;type:varchar(32);not null;comment:执行平台"`
-	LanguageType string `json:"languageType" gorm:"language_type;type:varchar(16);not null;comment:SQL, Go, Java"`
-	Reversed     string `json:"reversed"     gorm:"reversed;type:longtext;not null;comment:反转的 DDL"`
+	service_name	VARCHAR(32) 	NOT NULL COMMENT '微服务名',
+	filename		VARCHAR(191) 	NOT NULL COMMENT '脚本文件名',
+	checksum		CHAR(128)		NOT NULL COMMENT '脚本文本 checksum',
+	installed_by	VARCHAR(32)		NOT NULL COMMENT '执行人',
+	installed_on	VARCHAR(32)		NOT NULL COMMENT '执行平台',
+	language_type	VARCHAR(16)		NOT NULL COMMENT '.sql, .py',
+	reversed 		LONGTEXT 		NOT NULL COMMENT '反转的 DDL',
+	CONSTRAINT uk_filename UNIQUE (filename),
+	CONSTRAINT uk_checksum UNIQUE (checksum)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'erda migration 执行记录表'
+`
+
+type HistoryModel struct {
+	ID        uint64    `gorm:"primary key"`
+	CreatedAt time.Time `json:"createdAt" gorm:"created_at"`
+	UpdatedAt time.Time `json:"updatedAt" gorm:"updated_at"`
+
+	ServiceName  string `json:"serviceName"  gorm:"service_name"`
+	Filename     string `json:"filename"     gorm:"filename"`
+	Checksum     string `json:"checksum"     gorm:"checksum"`
+	InstalledBy  string `json:"installedBy"  gorm:"installed_by"`
+	InstalledOn  string `json:"installedOn"  gorm:"install_on"`
+	LanguageType string `json:"languageType" gorm:"language_type"`
+	Reversed     string `json:"reversed"     gorm:"reversed"`
 }
 
 func (m HistoryModel) TableName() string {
 	return SchemaMigrationHistory
 }
 
-func (m HistoryModel) create() {
-	if ok := DB().Migrator().HasTable(m.TableName()); !ok {
-		_ = DB().Migrator().CreateTable(&m)
+func (m HistoryModel) CreateTable(db *gorm.DB) {
+	db.Logger.LogMode(logger.Silent)
+	if err := db.Exec(CreateTableHistoryModel).Error; err != nil {
+		logrus.WithError(err).Fatal("failed to create table")
 	}
-}
-
-func (m *HistoryModel) insert() error {
-	return DB().Create(m).Error
-}
-
-func (m *HistoryModel) delete(query interface{}, args ...interface{}) error {
-	return DB().Where(query, args...).Delete(m).Error
-}
-
-func (m HistoryModel) exists() bool {
-	return DB().Raw("SHOW TABLES LIKE ?", m.TableName()).RowsAffected == 1
-}
-
-func (m *HistoryModel) records(serviceName ...string) (histories []HistoryModel, affected int64) {
-	tx := DB()
-	if len(serviceName) > 0 {
-		tx = tx.Where("service_name IN (?)", serviceName)
-	}
-
-	if tx = tx.Find(&histories); tx.Error != nil {
-		return nil, 0
-	}
-
-	return histories, tx.RowsAffected
 }
