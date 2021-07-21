88c0e2b82979c77f14df5de522b32bdd8519fc17
pjy <1736989646@qq.com>
2021-07-21 17:56:53 +0800

    modify rule name

--- .erda/migrations/monitor/20210721-monitor-rules-name.sql
+++ .erda/migrations/monitor/20210721-monitor-rules-name.sql
@@ -1,1 +1,1 @@
-update sp_alert_rules set name='平台组件日志存储保护器状态变化' where name='平台组价日志存储保护器状态变化' and enable=1;
+update sp_alert_rules set name='平台组件日志存储保护器状态变化' where alert_index='dice_component_log_protector';
