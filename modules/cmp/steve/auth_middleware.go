f63d2766b1439ff9a3cdcfbc66d1e187fb47a9e7
Craig <chenmiaochen.cmc@alibaba-inc.com>
2021-07-21 10:59:29 +0800

    add copyright

---
+++ modules/cmp/steve/auth_middleware.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package steve
+
+import "net/http"
+
+func AuthMiddleware(next http.Handler) http.Handler {
+
+}
