474193fe72dcf3f9d269dfd20f52341ef4c9404f
innerpeacez <innerpeace.zhai@gmail.com>
2021-07-23 17:20:51 +0800

    msp tenant

---
+++ modules/msp/tenant/db/tenant.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package db
+
+import (
+	"github.com/erda-project/erda/pkg/common/errors"
+	"github.com/jinzhu/gorm"
+)
+
+// MSPTenantDB msp_tenant
+type MSPTenantDB struct {
+	*gorm.DB
+}
+
+func (db *MSPTenantDB) db() *gorm.DB {
+	return db.Table(TableMSPTenant)
+}
+
+func (db *MSPTenantDB) InsertTenant(tenant *MSPTenant) (*MSPTenant, *errors.DatabaseError) {
+	result := db.db().Create(tenant)
+
+	if result.Error != nil {
+		return nil, errors.NewDatabaseError(result.Error)
+	}
+	value := result.Value.(*MSPTenant)
+	return value, nil
+}
+
+func (db *MSPTenantDB) QueryTenant(tenantID string) (*MSPTenant, error) {
+	tenant := MSPTenant{}
+	err := db.db().Where("`id` = ?", tenantID).Find(&tenant).Error
+	if err != nil {
+		return nil, err
+	}
+	return &tenant, nil
+}
---
+++ modules/msp/tenant/provider.go
@@ -0,0 +1,71 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package tenant
+
+import (
+	"github.com/erda-project/erda/pkg/common/apis"
+	"github.com/jinzhu/gorm"
+
+	logs "github.com/erda-project/erda-infra/base/logs"
+	servicehub "github.com/erda-project/erda-infra/base/servicehub"
+	transport "github.com/erda-project/erda-infra/pkg/transport"
+	pb "github.com/erda-project/erda-proto-go/msp/tenant/pb"
+	"github.com/erda-project/erda/modules/msp/tenant/db"
+)
+
+type config struct {
+}
+
+// +provider
+type provider struct {
+	Cfg           *config
+	Log           logs.Logger
+	Register      transport.Register
+	tenantService *tenantService
+	DB            *gorm.DB `autowired:"mysql-client"`
+}
+
+func (p *provider) Init(ctx servicehub.Context) error {
+	p.tenantService = &tenantService{
+		p:           p,
+		MSPTenantDB: &db.MSPTenantDB{DB: p.DB},
+	}
+	if p.Register != nil {
+		pb.RegisterTenantServiceImp(p.Register, p.tenantService, apis.Options())
+	}
+	return nil
+}
+
+func (p *provider) Provide(ctx servicehub.DependencyContext, args ...interface{}) interface{} {
+	switch {
+	case ctx.Service() == "erda.msp.tenant.TenantService" || ctx.Type() == pb.TenantServiceServerType() || ctx.Type() == pb.TenantServiceHandlerType():
+		return p.tenantService
+	}
+	return p
+}
+
+func init() {
+	servicehub.Register("erda.msp.tenant", &servicehub.Spec{
+		Services:             pb.ServiceNames(),
+		Types:                pb.Types(),
+		OptionalDependencies: []string{"service-register"},
+		Description:          "",
+		ConfigFunc: func() interface{} {
+			return &config{}
+		},
+		Creator: func() servicehub.Provider {
+			return &provider{}
+		},
+	})
+}
