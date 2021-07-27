dccbcf4885723324529a9039e74ccec1bc4cf706
悟空 <rainchan365@163.com>
2021-07-27 18:31:00 +0800

    Hotfix/migrator (#1165)
    * feature: fix snapshot
    
    * feature: fix go mod
    
    * feature: trim constrain type
    
    * feature: add ut for snapshot
    
    * fix: fix log
    
    * feature: trim character set from raw sql
    
    * fix: regexp
    
    * feature: advance TestTrimCharacterSetFromRawCreateTableSQL
    
    * fix: Sprintf format %s has arg r.Flen of wrong type int
    
    * fix: fix delete element error
    
    * fix: typo

---
+++ pkg/database/sqlparser/snapshot/snapshot_test.go
@@ -0,0 +1,147 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// This program is free software: you can use, redistribute, and/or modify
+// it under the terms of the GNU Affero General Public License, version 3
+// or later ("AGPL"), as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package snapshot_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/pingcap/parser/ast"
+	"github.com/pingcap/parser/format"
+	_ "github.com/pingcap/tidb/types/parser_driver"
+
+	"github.com/pingcap/parser"
+
+	"github.com/erda-project/erda/pkg/database/sqlparser/snapshot"
+)
+
+const sqlWithCollate = "CREATE TABLE `fdp_master_reco_scenario_workflows` (`id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',`org_id` BIGINT(20) DEFAULT NULL COMMENT '企业 id',`scenario_name` VARCHAR(128) NOT NULL DEFAULT '',`scenario_code` VARCHAR(128) NOT NULL DEFAULT '',`process_type` VARCHAR(128) NOT NULL COMMENT '处理类型',`name` VARCHAR(128) NOT NULL DEFAULT '' COMMENT '工作流名',`name_pinyin` VARCHAR(128) NOT NULL DEFAULT '' COMMENT '工作流拼音',`description` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT '工作流描述',`source` VARCHAR(32) DEFAULT '' COMMENT 'workflow 来源: dl/cdp',`run_type` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '运行类型: ATONCE、SCHEDULE',`cron` VARCHAR(128) DEFAULT '' COMMENT '任务周期',`cron_start_from` DATETIME DEFAULT NULL COMMENT '延时执行时间',`category_id` BIGINT(20) NOT NULL COMMENT '工作流目录 ID',`pipeline_name` VARCHAR(128) DEFAULT '' COMMENT 'pipeline 名称',`pipeline` MEDIUMTEXT NOT NULL COMMENT 'pipeline 内容',`pipeline_id` BIGINT(20) DEFAULT NULL COMMENT 'pipeline id',`node_params` TEXT COMMENT '工作流节点参数',`locations` VARCHAR(1024) DEFAULT NULL,`creator_id` VARCHAR(128) CHARACTER SET UTF8MB4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT '创建者 ID',`updater_id` VARCHAR(128) DEFAULT NULL COMMENT '更新者ID',`extra` MEDIUMTEXT COMMENT '扩展信息',`delete_yn` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '逻辑删除标记',`created_at` DATETIME DEFAULT NULL COMMENT '创建时间',`updated_at` DATETIME DEFAULT NULL COMMENT '更新时间',PRIMARY KEY(`id`),INDEX `idx_category_id_name`(`scenario_code`, `process_type`)) ENGINE = InnoDB DEFAULT CHARACTER SET = UTF8 COMMENT = '场景工作流表'"
+const sqlWithConstraintCheck = "CREATE TABLE `migration_records` (`version` VARCHAR(10) NOT NULL COMMENT '服务版本号-',`module` VARCHAR(50) NOT NULL COMMENT '服务名',`version_b` VARCHAR(10) NOT NULL COMMENT '服务 B 版本号',`done` VARCHAR(1) NOT NULL DEFAULT '0',`create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '创建时间',PRIMARY KEY(`version`, `module`, `version_b`),CONSTRAINT `migration_records_chk_1` CHECK(((`done`='0') OR (`done`='1'))) ENFORCED) ENGINE = InnoDB DEFAULT CHARACTER SET = UTF8MB4"
+const (
+	sqlWithUtf32_1 = "CREATE TABLE `migration_records` (`version` VARCHAR(10) NOT NULL COMMENT '服务版本号-',`module` VARCHAR(50) NOT NULL COMMENT '服务名',`version_b` VARCHAR(10) NOT NULL COMMENT '服务 B 版本号',`done` VARCHAR(1) NOT NULL DEFAULT '0',`create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '创建时间',PRIMARY KEY(`version`, `module`, `version_b`),CONSTRAINT `migration_records_chk_1` CHECK(((`done`='0') OR (`done`='1'))) ENFORCED) ENGINE = InnoDB DEFAULT CHARACTER SET = UTF32"
+	sqlWithUtf32_2 = "CREATE TABLE `migration_records` (`version` VARCHAR(10) NOT NULL COMMENT '服务版本号-',`module` VARCHAR(50) NOT NULL COMMENT '服务名',`version_b` VARCHAR(10) NOT NULL COMMENT '服务 B 版本号',`done` VARCHAR(1) NOT NULL DEFAULT '0',`create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '创建时间',PRIMARY KEY(`version`, `module`, `version_b`),CONSTRAINT `migration_records_chk_1` CHECK(((`done`='0') OR (`done`='1'))) ENFORCED) ENGINE = InnoDB character SET = UTF32"
+)
+
+func TestTrimCollate(t *testing.T) {
+	stmt, err := parser.New().ParseOneStmt(sqlWithCollate, "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	create := stmt.(*ast.CreateTableStmt)
+	for i := range create.Cols {
+		t.Log(create.Cols[i].Name)
+		for j := range create.Cols[i].Options {
+			t.Log("\t", create.Cols[i].Options[j])
+		}
+	}
+}
+
+func TestTrimCollation(t *testing.T) {
+	stmt, err := parser.New().ParseOneStmt(sqlWithCollate, "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	create := stmt.(*ast.CreateTableStmt)
+	testTrimCollateOptionFromCols(t, create)
+	testTrimCollateOptionFromCreateTable(t, create)
+
+	var buf = bytes.NewBuffer(nil)
+	if err := create.Restore(&format.RestoreCtx{
+		Flags:     format.DefaultRestoreFlags,
+		In:        buf,
+		JoinLevel: 0,
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	sql := buf.String()
+	t.Log(sql)
+
+	stmt, err = parser.New().ParseOneStmt(sql, "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	create = stmt.(*ast.CreateTableStmt)
+	for _, opt := range create.Options {
+		if opt.Tp == ast.TableOptionCollate {
+			t.Fatal("table option collate was not trimmed")
+		}
+	}
+
+	for _, col := range create.Cols {
+		for _, opt := range col.Options {
+			if opt.Tp == ast.ColumnOptionCollate {
+				t.Fatal("col optoin collate was not trimmed")
+			}
+		}
+	}
+}
+
+func testTrimCollateOptionFromCols(t *testing.T, create *ast.CreateTableStmt) {
+	snapshot.TrimCollateOptionFromCols(create)
+}
+
+func testTrimCollateOptionFromCreateTable(t *testing.T, create *ast.CreateTableStmt) {
+	snapshot.TrimCollateOptionFromCreateTable(create)
+}
+
+func TestTrimConstraintCheckFromCreateTable(t *testing.T) {
+	stmt, err := parser.New().ParseOneStmt(sqlWithConstraintCheck, "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	create := stmt.(*ast.CreateTableStmt)
+	snapshot.TrimConstraintCheckFromCreateTable(create)
+
+	var buf = bytes.NewBuffer(nil)
+	if err := create.Restore(&format.RestoreCtx{
+		Flags:     format.DefaultRestoreFlags,
+		In:        buf,
+		JoinLevel: 0,
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	sql := buf.String()
+	t.Log(sql)
+
+	if strings.Contains(strings.ToLower(sql), "check") {
+		t.Fatal("constraint check was not trimmed")
+	}
+
+	stmt, err = parser.New().ParseOneStmt(sql, "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	create = stmt.(*ast.CreateTableStmt)
+	for _, con := range create.Constraints {
+		if con.Tp == ast.ConstraintCheck {
+			t.Fatal("constraint check was not trimmed")
+		}
+	}
+}
+
+func TestTrimCharacterSetFromRawCreateTableSQL(t *testing.T) {
+	for _, sql := range []string{sqlWithUtf32_1, sqlWithUtf32_2} {
+		sql := snapshot.TrimCharacterSetFromRawCreateTableSQL(sql)
+		if strings.Contains(strings.ToLower(sql), "utf32") {
+			t.Fatal("failed to trim character from sql")
+		}
+		if _, err := parser.New().ParseOneStmt(sql, "", ""); err != nil {
+			t.Fatal(err)
+		}
+		t.Log(sql)
+	}
+}
