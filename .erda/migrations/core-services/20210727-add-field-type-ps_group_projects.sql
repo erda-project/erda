8008f0ac25d9579c8a65767b244f0be338e01348
innerpeacez <innerpeace.zhai@gmail.com>
2021-07-27 11:44:50 +0800

    update sql

--- .erda/migrations/core-services/20210727-add-field-type-ps_group_projects.sql
+++ .erda/migrations/core-services/20210727-add-field-type-ps_group_projects.sql
@@ -1,1 +1,1 @@
-ALTER TABLE ps_group_projects ADD type VARCHAR(20) COMMENT 'Project type' DEFAULT '';
+ALTER TABLE ps_group_projects ADD type VARCHAR(20) COMMENT 'Project type'  NOT NULL DEFAULT '';
