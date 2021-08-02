503f12c5277e00f683d250a8658f3a8fb0071d05
悟空 <rainchan365@163.com>
2021-08-02 23:19:33 +0800

    feature: delete uesless files

--- .erda/migrations/_patches_/migrator-20210802-01-correct-cheksum.sql
+++
@@ -1,7 +0,0 @@
-UPDATE schema_migration_history
-SET checksum='9dea1f2ebc31ed9e5515c41bab748e9277c51c88ca6d8ded00c0859a913bffb9'
-WHERE filename='20210528-pipeline-base.sql';
-
-UPDATE schema_migration_history
-SET checksum='d631f36002157ecaa58ccddf7a74da49d75c22a47c82311598d65616e8df75b9'
-WHERE filename='20210528-qa-base.sql';
--- .erda/migrations/_patches_/pipeline-20210802-01-normalize-base.sql
+++
@@ -1,3 +0,0 @@
-UPDATE pipeline_extras SET snippets='' WHERE snippets IS NULL ;
-
-UPDATE pipeline_tasks SET result='' WHERE result IS NULL ;
