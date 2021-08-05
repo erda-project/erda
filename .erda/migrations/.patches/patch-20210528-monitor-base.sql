b9b9acce5228a2cb966e99e125bafb2d0b307309
悟空 <rainchan365@163.com>
2021-08-05 15:57:41 +0800

    patches migrations (#1289)
    * feature: add paches migrations
    
    * feature: delete udpate sql
    
    * feature: rename patches
    
    * feature: add patches sql
    
    * feature: patches bases
    
    * feature: patches bases
    
    * feature: alter the charset of tables with a very long unique key

---
+++ .erda/migrations/.patches/patch-20210528-monitor-base.sql
@@ -0,0 +1,5 @@
+ALTER TABLE sp_dashboard_block DEFAULT CHARACTER SET utf8;
+
+ALTER TABLE sp_dashboard_block_system DEFAULT CHARACTER SET utf8;
+
+ALTER TABLE sp_report_task DEFAULT CHARACTER SET utf8;
