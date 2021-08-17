f9f5a3efab163563de2aad09c6a6618af1899479
ZLValien <74653472+ZLValien@users.noreply.github.com>
2021-08-17 14:09:26 +0800

    BUG fix: if MODULE_PATH is not dicehub,build image will false (#1468)

--- build/scripts/dicehub/extension-push.sh
+++ build/scripts/dicehub/extension-push.sh
@@ -3,11 +3,9 @@
 string=$*
 array=(${string//,/ })
 
-mkdir -p $GOPATH/src/extension
-
 for param in ${array[@]}
 do
     wget $param -O extension$count.zip
-    unzip extension$count.zip -d $GOPATH/src/extension
+    unzip extension$count.zip -d /tmp/dicehub-extension
     count=$[$count+1]
 done
