e932fd772ae9d6f52dcde36be4fd8919ce16cc0a
郭刚平 <512979011@qq.com>
2021-08-18 19:44:51 +0800

    update quick-start image to v1.2 (#1501)
    * feature: quick-start update image to v1.2
    
    * feature: quick-start add sysctl-init container to change the max_map_count if the value is too low for elasticsearch

--- quick-start/quick-start.sh
+++ quick-start/quick-start.sh
@@ -140,6 +140,20 @@ EOABORT
 )"
 fi
 
+if ! command -v docker-compose >/dev/null; then
+    abort "$(cat <<EOABORT
+You must install Docker-Compose before installing Erda.
+EOABORT
+)"
+fi
+
+if ! command -v netcat >/dev/null; then
+    abort "$(cat <<EOABORT
+You must install netcat before installing Erda.
+EOABORT
+)"
+fi
+
 INSTALL_LOCATION="/opt/erda-quickstart"
 ERDA_REPOSITORY="https://github.com/erda-project/erda.git"
 
@@ -170,7 +184,7 @@ ohai "Start clone Erda[${ERDA_REPOSITORY}] to ${INSTALL_LOCATION}"
   execute "git" "fetch" "--force" "origin"
   execute "git" "fetch" "--force" "--tags" "origin"
 
-  execute "git" "reset" "--hard" "origin/master"
+  execute "git" "reset" "--hard" "v1.2.0"
 
 ) || exit 1
 
@@ -192,6 +206,7 @@ do
 done
 
 execute "docker-compose" "up" "erda-migration" || exit 1
+execute "docker-compose" "up" "sysctl-init" || exit 1
 execute "docker-compose" "up" "-d" "elasticsearch" || exit 1
 execute "docker-compose" "up" "-d" "cassandra" || exit 1
 execute "docker-compose" "up" "-d" "kafka" || exit 1
