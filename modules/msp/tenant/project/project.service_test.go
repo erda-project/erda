c607bd043086e2414662a3bec8b19614cbd59599
innerpeacez <innerpeace.zhai@gmail.com>
2021-08-23 15:15:52 +0800

    add test ut

--- modules/msp/tenant/project/project.service_test.go
+++ modules/msp/tenant/project/project.service_test.go
@@ -79,6 +79,63 @@ func Test_projectService_GetProjects(t *testing.T) {
 	}
 }
 
+func Test_projectService_GetProjectsTenantsIDs(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.GetProjectsTenantsIDsRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.GetProjectsTenantsIDsResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.tenant.project.ProjectService",
+		//			`
+		//erda.msp.tenant.project:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.GetProjectsTenantsIDsRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.GetProjectsTenantsIDsResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ProjectServiceServer)
+			got, err := srv.GetProjectsTenantsIDs(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("projectService.GetProjectsTenantsIDs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("projectService.GetProjectsTenantsIDs() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
+
 func Test_projectService_CreateProject(t *testing.T) {
 	type args struct {
 		ctx context.Context
@@ -131,6 +188,234 @@ func Test_projectService_CreateProject(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.wantResp) {
 				t.Errorf("projectService.CreateProject() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
+
+func Test_projectService_UpdateProject(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.UpdateProjectRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.UpdateProjectResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.tenant.project.ProjectService",
+		//			`
+		//erda.msp.tenant.project:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.UpdateProjectRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.UpdateProjectResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ProjectServiceServer)
+			got, err := srv.UpdateProject(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("projectService.UpdateProject() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("projectService.UpdateProject() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
+
+func Test_projectService_DeleteProject(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.DeleteProjectRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.DeleteProjectResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.tenant.project.ProjectService",
+		//			`
+		//erda.msp.tenant.project:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.DeleteProjectRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.DeleteProjectResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ProjectServiceServer)
+			got, err := srv.DeleteProject(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("projectService.DeleteProject() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("projectService.DeleteProject() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
+
+func Test_projectService_GetProject(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.GetProjectRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.GetProjectResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.tenant.project.ProjectService",
+		//			`
+		//erda.msp.tenant.project:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.GetProjectRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.GetProjectResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ProjectServiceServer)
+			got, err := srv.GetProject(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("projectService.GetProject() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("projectService.GetProject() = %v, want %v", got, tt.wantResp)
+			}
+		})
+	}
+}
+
+func Test_projectService_GetProjectOverview(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *pb.GetProjectOverviewRequest
+	}
+	tests := []struct {
+		name     string
+		service  string
+		config   string
+		args     args
+		wantResp *pb.GetProjectOverviewResponse
+		wantErr  bool
+	}{
+		// TODO: Add test cases.
+		//		{
+		//			"case 1",
+		//			"erda.msp.tenant.project.ProjectService",
+		//			`
+		//erda.msp.tenant.project:
+		//`,
+		//			args{
+		//				context.TODO(),
+		//				&pb.GetProjectOverviewRequest{
+		//					// TODO: setup fields
+		//				},
+		//			},
+		//			&pb.GetProjectOverviewResponse{
+		//				// TODO: setup fields.
+		//			},
+		//			false,
+		//		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hub := servicehub.New()
+			events := hub.Events()
+			go func() {
+				hub.RunWithOptions(&servicehub.RunOptions{Content: tt.config})
+			}()
+			err := <-events.Started()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			srv := hub.Service(tt.service).(pb.ProjectServiceServer)
+			got, err := srv.GetProjectOverview(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("projectService.GetProjectOverview() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantResp) {
+				t.Errorf("projectService.GetProjectOverview() = %v, want %v", got, tt.wantResp)
 			}
 		})
 	}
