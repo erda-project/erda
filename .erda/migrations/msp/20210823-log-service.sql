7f77c6781483e3814a15deff27b90bd53800be94
snakorse <512979011@qq.com>
2021-08-24 20:01:37 +0800

    optimize: sql lint

--- .erda/migrations/msp/20210823-log-service.sql
+++ .erda/migrations/msp/20210823-log-service.sql
@@ -1,12 +1,12 @@
 CREATE TABLE sp_log_service_instance
 (
-    `id`          int           AUTO_INCREMENT PRIMARY KEY,
+    `id`          bigint        AUTO_INCREMENT PRIMARY KEY COMMENT 'Id',
     `instance_id` varchar(128)  NOT NULL COMMENT 'log-instance唯一ID',
     `es_urls`     varchar(1024) NOT NULL COMMENT 'es urls, split by comma',
     `es_config`   varchar(1024) NOT NULL COMMENT 'es connection info, serialized json format',
-    `created`     datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated`     datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    UNIQUE INDEX uniq_instance_id(`instance_id`)
+    `created_at`  datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created time',
+    `updated_at`  datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'updated time',
+    UNIQUE INDEX uk_instance_id(`instance_id`)
 ) COMMENT '日志服务实例表';
 
 
