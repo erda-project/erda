16f52d7274ed890db74d89bb8243edd9fdfe5337
innerpeacez <innerpeace.zhai@gmail.com>
2021-08-25 15:30:13 +0800

    add license and add uts

--- modules/msp/tenant/db/project_test.go
+++ modules/msp/tenant/db/project_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package db
 
 import (
@@ -8,19 +22,17 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-func dbMockInit() (*MSPProjectDB, sqlmock.Sqlmock, error) {
+func dbMockInit() (*gorm.DB, sqlmock.Sqlmock, error) {
 	db, mock, err := sqlmock.New()
 	if nil != err {
 		return nil, nil, nil
 	}
 	mydb, err := gorm.Open("mysql", db)
-	mspProjectDB := &MSPProjectDB{mydb}
-	return mspProjectDB, mock, err
+	return mydb, mock, err
 }
 
 func TestMSPProjectDB_Query(t *testing.T) {
-	db, _, _ := sqlmock.New()
-	mydb, _ := gorm.Open("mysql", db)
+	mydb, _, _ := dbMockInit()
 	type fields struct {
 		DB *gorm.DB
 	}
@@ -48,6 +60,108 @@ func TestMSPProjectDB_Query(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Query() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMSPProjectDB_Delete(t *testing.T) {
+	mydb, _, _ := dbMockInit()
+	type fields struct {
+		DB *gorm.DB
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *MSPProject
+		wantErr bool
+	}{
+		{name: "case1", fields: fields{DB: mydb}, args: args{id: "1"}, want: nil, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := &MSPProjectDB{
+				DB: tt.fields.DB,
+			}
+			got, err := db.Delete(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Delete() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMSPProjectDB_Update(t *testing.T) {
+	mydb, _, _ := dbMockInit()
+	type fields struct {
+		DB *gorm.DB
+	}
+	type args struct {
+		project *MSPProject
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *MSPProject
+		wantErr bool
+	}{
+		{name: "case1", fields: fields{DB: mydb}, args: args{project: nil}, want: nil, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := &MSPProjectDB{
+				DB: tt.fields.DB,
+			}
+			got, err := db.Update(tt.args.project)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Update() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMSPProjectDB_Create(t *testing.T) {
+	mydb, _, _ := dbMockInit()
+	type fields struct {
+		DB *gorm.DB
+	}
+	type args struct {
+		project *MSPProject
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *MSPProject
+		wantErr bool
+	}{
+		{name: "case1", fields: fields{DB: mydb}, args: args{project: nil}, want: nil, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := &MSPProjectDB{
+				DB: tt.fields.DB,
+			}
+			got, err := db.Create(tt.args.project)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Create() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
