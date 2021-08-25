3685b516695dbf248056dfa7f7e52bfcab00a84d
snakorse <512979011@qq.com>
2021-08-25 10:07:35 +0800

    optimize: sql lint

--- .erda/migrations/msp/20210823-log-service.sql
+++ .erda/migrations/msp/20210823-log-service.sql
@@ -7,7 +7,7 @@ CREATE TABLE sp_log_service_instance
     `created_at`  datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created time',
     `updated_at`  datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'updated time',
     UNIQUE INDEX uk_instance_id(`instance_id`)
-) COMMENT '日志服务实例表';
+) DEFAULT CHARSET=utf8mb4 COMMENT '日志服务实例表';
 
 
 INSERT INTO tb_tmc_ini (ini_name, ini_desc, ini_value, create_time, update_time, is_deleted)
