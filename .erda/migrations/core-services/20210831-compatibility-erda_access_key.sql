7f30f3cb302bc52e2da7ee7047493b6bb2467ccc
erenming <erenming233@gmail.com>
2021-08-31 19:19:26 +0800

    compatibility SQL

---
+++ .erda/migrations/core-services/20210831-compatibility-erda_access_key.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `erda_access_key` MODIFY COLUMN `access_key_id` char(24) NOT NULL DEFAULT '' COMMENT 'deprecated!';
+ALTER TABLE `erda_access_key` MODIFY COLUMN `is_system` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'deprecated!';
+ALTER TABLE `erda_access_key` MODIFY COLUMN `id` VARCHAR(36) NOT NULL COMMENT 'Primary Key';
+DROP INDEX `uk_ak` ON `erda_access_key`;
+ALTER TABLE `erda_access_key` ADD UNIQUE KEY `uk_ak` (`access_key`);
