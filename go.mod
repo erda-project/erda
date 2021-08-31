35fab266cc4e069e301d31b21fa2f4dafe3db136
pjy <1736989646@qq.com>
2021-08-31 09:20:20 +0800

    delete displayname

--- go.mod
+++ go.mod
@@ -32,7 +32,7 @@ require (
 	github.com/elastic/cloud-on-k8s v0.0.0-20210205172912-5ce0eca90c60
 	github.com/elazarl/goproxy v0.0.0-20200421181703-e76ad31c14f6
 	github.com/erda-project/erda-infra v0.0.0-20210830020850-d53df158bcd0
-	github.com/erda-project/erda-proto-go v0.0.0-20210824100650-1d7d288aebec
+	github.com/erda-project/erda-proto-go v0.0.0-20210830081737-53132cb0c50a
 	github.com/extrame/ole2 v0.0.0-20160812065207-d69429661ad7 // indirect
 	github.com/extrame/xls v0.0.1
 	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
--- modules/msp/apm/adapter/adapter.service.go
+++ modules/msp/apm/adapter/adapter.service.go
@@ -35,8 +35,7 @@ func (s *adapterService) GetInstrumentationLibrary(ctx context.Context, request 
 	}
 	for k, v := range s.p.libraryMap {
 		libraryList := &pb.InstrumentationLibrary{
-			DisplayName: k,
-			Strategy:    k,
+			Strategy: k,
 		}
 		languages := make([]*pb.Language, 0)
 		languageList, ok := v.([]interface{})
@@ -45,8 +44,7 @@ func (s *adapterService) GetInstrumentationLibrary(ctx context.Context, request 
 		}
 		for _, v := range languageList {
 			language := &pb.Language{
-				Language:    v.(string),
-				DisplayName: v.(string),
+				Language: v.(string),
 			}
 			languages = append(languages, language)
 		}
