fabcb22e339767b024245f162aaee5ca943a276f
悟空 <rainchan365@163.com>
2021-09-01 14:35:53 +0800

    feature: supprot to generate example for application/x-www-form-urlencoded

--- pkg/swagger/oas3/gen_example.go
+++ pkg/swagger/oas3/gen_example.go
@@ -16,52 +16,116 @@ package oas3
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/getkin/kin-openapi/openapi3"
+
+	"github.com/erda-project/erda/pkg/http/httputil"
 )
 
 // 为 *openapi3.Schema 生成 Example, 就地修改 schema.Example 字段
 // 生成前 Schema 应当是展开的, 不能包含引用
-func GenExample(schema *openapi3.Schema) {
-	genExample(schema, true)
+func GenExampleFromExpandedSchema(mediaType httputil.ContentType, schema *openapi3.Schema) {
+	switch mediaType {
+	case httputil.URLEncodedFormMime:
+		genURLEncodedFormMime(schema)
+	default:
+		genApplicationJson(schema, true)
+	}
 }
 
-func genExample(schema *openapi3.Schema, convStr bool) {
+func genURLEncodedFormMime(schema *openapi3.Schema) {
+	switch schema.Type {
+	case "object":
+		if len(schema.Properties) == 0 {
+			return
+		}
+		values := make(url.Values)
+		for name, property := range schema.Properties {
+			if property == nil || property.Value == nil {
+				continue
+			}
+			genApplicationJson(property.Value, true)
+			example, ok := property.Value.Example.(string)
+			if ok {
+				values.Add(name, example)
+			} else {
+				values.Add(name, fmt.Sprintf("%v", property.Value.Example))
+			}
+		}
+		schema.Example = values.Encode()
+	default:
+		return
+	}
+}
+
+func genApplicationJson(schema *openapi3.Schema, convStr bool) {
 	if schema.Example != nil {
+		switch schema.Example.(type) {
+		case string:
+			return
+		case []byte:
+			if convStr {
+				schema.Example = string(schema.Example.([]byte))
+			}
+			return
+		case json.RawMessage:
+			if convStr {
+				schema.Example = string(schema.Example.(json.RawMessage))
+			}
+			return
+		}
+
+		data, err := json.Marshal(schema.Example)
+		if err != nil {
+			schema.Example = fmt.Sprintf("%v", schema.Example)
+			return
+		}
+		schema.Example = string(data)
 		return
 	}
 
-	switch schema.Type {
+	switch strings.ToLower(schema.Type) {
 	case "boolean":
 		schema.Example = true
 	case "string":
 		schema.Example = ""
-	case "number":
+	case "number", "int", "integer":
 		schema.Example = 0
 	case "object":
-		var m = make(map[string]interface{}, 0)
+		var m = make(map[string]interface{})
 		for key, property := range schema.Properties {
 			if property.Value == nil {
 				continue
 			}
-			genExample(property.Value, false)
+			genApplicationJson(property.Value, false)
 			m[key] = property.Value.Example
 		}
-		data, _ := json.MarshalIndent(m, "", "  ")
+		data, err := json.Marshal(m)
+		if err != nil {
+			return
+		}
+		schema.Example = json.RawMessage(data)
 		if convStr {
 			schema.Example = string(data)
-		} else {
-			schema.Example = json.RawMessage(data)
 		}
 	case "array":
 		var li []interface{}
 		if schema.Items == nil || schema.Items.Value == nil {
 			schema.Example = json.RawMessage("[]")
+			if convStr {
+				schema.Example = "[]"
+			}
 			return
 		}
-		genExample(schema.Items.Value, false)
+		genApplicationJson(schema.Items.Value, false)
 		li = append(li, schema.Items.Value.Example)
-		data, _ := json.MarshalIndent(li, "", "")
+		data, err := json.Marshal(li)
+		if err != nil {
+			return
+		}
 		if convStr {
 			schema.Example = string(data)
 		} else {
