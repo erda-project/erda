10c041d327b48aef2df70f006848b6e5efa41ddf
ZLValien <1006058235@qq.com>
2021-09-06 19:31:46 +0800

    add auto test plan sql

--- modules/openapi/component-protocol/scenarios/auto-test-plan-detail/components/executeInfo/render_test.go
+++ modules/openapi/component-protocol/scenarios/auto-test-plan-detail/components/executeInfo/render_test.go
@@ -25,17 +25,17 @@ import (
 
 func Test_convertReportToConfig(t *testing.T) {
 	m := apistructs.PipelineReport{}
-	bt := `{"id":17046,"pipelineID":18564,"type":"auto-test-plan","meta":{"data":"{\"domain\":\"https://openapi.hkci.terminus.io\",\"header\":{\"Cookie\":\"u_c_captain_hkci_local=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsb2dpbiIsInBhdGgiOiIvIiwidG9rZW5LZXkiOiI5NTA4NjI2ZTczZDdiODdjYjY1N2M3YTUzMGY1NWM4ZDg0OTJjYjc0YjFmZDdlZTNlNjhmZThkNThmZjg4ZDM2IiwibmJmIjoxNjI5ODkwMTMyLCJkb21haW4iOiJ0ZXJtaW51cy5pbyIsImlzcyI6ImRyYWNvIiwidGVuYW50SWQiOjEsImV4cGlyZV90aW1lIjo2MDQ4MDAsImV4cCI6MTYzMDQ5NDkzMiwiaWF0IjoxNjI5ODkwMTMyfQ.jTI2D54sC-aM90p8TX9pfeXXwOFi8Yns6smQhNSVZKA; OPENAPISESSION=6cccd9ac-38f0-4906-9da8-0143186dd631; OPENAPI-CSRF-TOKEN=12bde30a9db3d9aad0159ecbd9ae79b89005c3a5eca326a2b7d0f349cf897441342e1bc368ab501084ed6de62d8e57ca\",\"cluster-id\":\"2\",\"cluster-name\":\"ZXJkYS1ob25na29uZw==\",\"org\":\"erda\",\"project-id\":\"13\"},\"global\":{\"111\":{\"name\":\"111\",\"type\":\"string\",\"value\":\"111\",\"desc\":\"111\"}}}"},"creatorID":"","updaterID":"","createdAt":"2021-09-03T17:25:48+08:00","updatedAt":"2021-09-03T17:25:48+08:00"}`
+	bt := `{"id":123,"pipelineID":123,"type":"auto-test-plan","meta":{"data":"{\"domain\":\"domain\",\"header\":{\"Cookie\":\"cookie\",\"cluster-id\":\"2\",\"cluster-name\":\"name\",\"org\":\"erda\",\"project-id\":\"13\"},\"global\":{\"111\":{\"name\":\"111\",\"type\":\"string\",\"value\":\"111\",\"desc\":\"111\"}}}"},"creatorID":"","updaterID":"","createdAt":"2021-09-03T17:25:48+08:00","updatedAt":"2021-09-03T17:25:48+08:00"}`
 	err := json.Unmarshal([]byte(bt), &m)
 	assert.NoError(t, err)
 	c, err := convertReportToConfig(m)
 	assert.NoError(t, err)
 	want := apistructs.AutoTestAPIConfig{
-		Domain: "https://openapi.hkci.terminus.io",
+		Domain: "domain",
 		Header: map[string]string{
-			"Cookie":       "u_c_captain_hkci_local=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsb2dpbiIsInBhdGgiOiIvIiwidG9rZW5LZXkiOiI5NTA4NjI2ZTczZDdiODdjYjY1N2M3YTUzMGY1NWM4ZDg0OTJjYjc0YjFmZDdlZTNlNjhmZThkNThmZjg4ZDM2IiwibmJmIjoxNjI5ODkwMTMyLCJkb21haW4iOiJ0ZXJtaW51cy5pbyIsImlzcyI6ImRyYWNvIiwidGVuYW50SWQiOjEsImV4cGlyZV90aW1lIjo2MDQ4MDAsImV4cCI6MTYzMDQ5NDkzMiwiaWF0IjoxNjI5ODkwMTMyfQ.jTI2D54sC-aM90p8TX9pfeXXwOFi8Yns6smQhNSVZKA; OPENAPISESSION=6cccd9ac-38f0-4906-9da8-0143186dd631; OPENAPI-CSRF-TOKEN=12bde30a9db3d9aad0159ecbd9ae79b89005c3a5eca326a2b7d0f349cf897441342e1bc368ab501084ed6de62d8e57ca",
+			"Cookie":       "cookie",
 			"cluster-id":   "2",
-			"cluster-name": "ZXJkYS1ob25na29uZw==",
+			"cluster-name": "name",
 			"org":          "erda",
 			"project-id":   "13",
 		},
