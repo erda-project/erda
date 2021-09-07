ac5d0d2a3f5abb7a1128c986fe5c579929f4c39f
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-09-07 17:27:00 +0800

    Fix format

--- modules/oap/collector/receivers/common/tags.go
+++ modules/oap/collector/receivers/common/tags.go
@@ -1,11 +1,24 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package common
 
 var (
-	TAG_SERVICE_NAME      = "service_name"
-	TAG_SERVICE_ID        = "service_id"
-	TAG_MSP_ENV_ID        = "msp_env_id"
+	TAG_SERVICE_NAME  = "service_name"
+	TAG_SERVICE_ID    = "service_id"
+	TAG_MSP_ENV_ID    = "msp_env_id"
 	TAG_MSP_AK_ID     = "msp_ak_id"
 	TAG_MSP_AK_SECRET = "msp_ak_secret"
-
-	TAG_TERMINUS_KEY = "terminus_key"
+	TAG_TERMINUS_KEY  = "terminus_key"
 )
