26c73165e46dd59fff2341f07039c7d515aa00b3
Effet <nesuadark@gmail.com>
2021-09-08 19:17:39 +0800

    re-order func & add test

--- modules/dop/component-protocol/components/issue-manage/issueTable/render.go
+++ modules/dop/component-protocol/components/issue-manage/issueTable/render.go
@@ -150,7 +150,33 @@ type AssigneeOperationData struct {
 	} `json:"meta"`
 }
 
-type ComponentAction struct{ base.DefaultProvider }
+type ComponentAction struct {
+	base.DefaultProvider
+	labels  []apistructs.ProjectLabel
+	isGuest bool
+	userMap map[string]apistructs.UserInfo
+}
+
+var (
+	priorityIcon = map[apistructs.IssuePriority]string{
+		apistructs.IssuePriorityUrgent: "ISSUE_ICON.priority.URGENT", // 紧急
+		apistructs.IssuePriorityHigh:   "ISSUE_ICON.priority.HIGH",   // 高
+		apistructs.IssuePriorityNormal: "ISSUE_ICON.priority.NORMAL", // 中
+		apistructs.IssuePriorityLow:    "ISSUE_ICON.priority.LOW",    // 低
+	}
+
+	stateIcon = map[string]string{
+		string(apistructs.IssueStateOpen):     "ISSUE_ICON.state.OPEN",
+		string(apistructs.IssueStateWorking):  "ISSUE_ICON.state.WORKING",
+		string(apistructs.IssueStateTesting):  "ISSUE_ICON.state.TESTING",
+		string(apistructs.IssueStateDone):     "ISSUE_ICON.state.DONE",
+		string(apistructs.IssueStateResolved): "ISSUE_ICON.state.RESOLVED",
+		string(apistructs.IssueStateReopen):   "ISSUE_ICON.state.REOPEN",
+		string(apistructs.IssueStateWontfix):  "ISSUE_ICON.state.WONTFIX",
+		string(apistructs.IssueStateDup):      "ISSUE_ICON.state.DUP",
+		string(apistructs.IssueStateClosed):   "ISSUE_ICON.state.CLOSED",
+	}
+)
 
 func (ca *ComponentAction) Render(ctx context.Context, c *cptype.Component, scenario cptype.Scenario, event cptype.ComponentEvent, gs *cptype.GlobalStateData) error {
 	// visible
@@ -172,6 +198,7 @@ func (ca *ComponentAction) Render(ctx context.Context, c *cptype.Component, scen
 	if err != nil {
 		return err
 	}
+	ca.isGuest = isGuest
 
 	projectid, err := strconv.ParseUint(sdk.InParams["projectId"].(string), 10, 64)
 	orgid, err := strconv.ParseUint(sdk.Identity.OrgID, 10, 64)
@@ -407,248 +434,19 @@ func (ca *ComponentAction) Render(ctx context.Context, c *cptype.Component, scen
 	if err != nil {
 		return err
 	}
-	userMap := make(map[string]apistructs.UserInfo)
+	ca.userMap = make(map[string]apistructs.UserInfo)
 	for _, v := range uInfo {
-		userMap[v.ID] = v
+		ca.userMap[v.ID] = v
 	}
 
 	labels, err := bdl.Bdl.Labels("issue", projectid, sdk.Identity.UserID)
 	if err != nil {
 		return err
 	}
-	priorityIcon := map[apistructs.IssuePriority]string{
-		apistructs.IssuePriorityUrgent: "ISSUE_ICON.priority.URGENT", // 紧急
-		apistructs.IssuePriorityHigh:   "ISSUE_ICON.priority.HIGH",   // 高
-		apistructs.IssuePriorityNormal: "ISSUE_ICON.priority.NORMAL", // 中
-		apistructs.IssuePriorityLow:    "ISSUE_ICON.priority.LOW",    // 低
-	}
-	stateIcon := map[string]string{
-		string(apistructs.IssueStateOpen):     "ISSUE_ICON.state.OPEN",
-		string(apistructs.IssueStateWorking):  "ISSUE_ICON.state.WORKING",
-		string(apistructs.IssueStateTesting):  "ISSUE_ICON.state.TESTING",
-		string(apistructs.IssueStateDone):     "ISSUE_ICON.state.DONE",
-		string(apistructs.IssueStateResolved): "ISSUE_ICON.state.RESOLVED",
-		string(apistructs.IssueStateReopen):   "ISSUE_ICON.state.REOPEN",
-		string(apistructs.IssueStateWontfix):  "ISSUE_ICON.state.WONTFIX",
-		string(apistructs.IssueStateDup):      "ISSUE_ICON.state.DUP",
-		string(apistructs.IssueStateClosed):   "ISSUE_ICON.state.CLOSED",
-	}
-	l := []TableItem{}
+	ca.labels = labels.List
+	var l []TableItem
 	for _, data := range r.Data.List {
-		var issuestate *apistructs.IssueStateButton
-		for _, s := range data.IssueButton {
-			if s.StateID == data.State {
-				issuestate = &s
-				break
-			}
-		}
-		tags := []Tag{}
-		for _, datalabel := range data.Labels {
-			for _, label := range labels.List {
-				if datalabel == label.Name {
-					tags = append(tags, Tag{Color: label.Color, Tag: label.Name})
-				}
-			}
-		}
-		progress := Progress{
-			RenderType: "progress",
-			Value:      "",
-		}
-		if data.IssueSummary != nil && data.IssueSummary.DoneCount+data.IssueSummary.ProcessingCount > 0 {
-			progress.Value = fmt.Sprintf("%d", int(100*(float64(data.IssueSummary.DoneCount)/float64(data.IssueSummary.DoneCount+data.IssueSummary.ProcessingCount))))
-		}
-
-		severityOps := map[string]interface{}{}
-		severityAuto := map[apistructs.IssueSeverity]string{
-			apistructs.IssueSeverityFatal:   "a",
-			apistructs.IssueSeveritySerious: "b",
-			apistructs.IssueSeverityNormal:  "c",
-			apistructs.IssueSeveritySlight:  "d",
-			apistructs.IssueSeverityLow:     "e",
-		}
-		for _, s := range []apistructs.IssueSeverity{
-
-			apistructs.IssueSeverityFatal,
-			apistructs.IssueSeveritySerious,
-			apistructs.IssueSeverityNormal,
-			apistructs.IssueSeveritySlight,
-			apistructs.IssueSeverityLow} {
-			severityOps["changeSeverityTo"+severityAuto[s]+string(s)] = map[string]interface{}{
-				"key":        "changeSeverityTo" + severityAuto[s] + string(s),
-				"reload":     true,
-				"text":       cputil.I18n(ctx, string(s)),
-				"prefixIcon": "ISSUE_ICON.severity." + string(s),
-				"meta": map[string]string{
-					"id":       strconv.FormatInt(data.ID, 10),
-					"severity": string(s),
-				},
-			}
-		}
-		severity := Severity{
-			RenderType: "operationsDropdownMenu",
-			Value:      cputil.I18n(ctx, strings.ToLower(string(data.Severity))),
-			PrefixIcon: "ISSUE_ICON.severity." + string(data.Severity),
-			Operations: severityOps,
-			Disabled:   isGuest,
-			DisabledTip: map[bool]string{
-				true: "无权限",
-			}[isGuest],
-		}
-		stateOperations := map[string]interface{}{}
-		stateAllDisable := true
-		for i, s := range data.IssueButton {
-			if s.Permission {
-				stateAllDisable = false
-			}
-			if isGuest {
-				stateAllDisable = true
-			}
-			if s.Permission {
-				stateOperations["changeStateTo"+strconv.Itoa(i)+s.StateName] = map[string]interface{}{
-					"meta": map[string]string{
-						"state": strconv.FormatInt(s.StateID, 10),
-						"id":    strconv.FormatInt(data.ID, 10),
-					},
-					"prefixIcon": stateIcon[string(s.StateBelong)],
-					"text":       s.StateName,
-					"reload":     true,
-					"key":        "changeStateTo" + strconv.Itoa(i) + s.StateName,
-					"disabled":   !s.Permission,
-					"disabledTip": map[bool]string{
-						false: "无法转移",
-					}[s.Permission],
-				}
-			}
-		}
-		AssigneeMapOperations := map[string]interface{}{}
-		AssigneeMapOperations["onChange"] = map[string]interface{}{
-			"meta": map[string]string{
-				"assignee": "",
-				"id":       strconv.FormatInt(data.ID, 10),
-			},
-			"text":     userMap[data.Assignee].Nick,
-			"reload":   true,
-			"key":      "updateAssignee",
-			"disabled": false,
-			"fillMeta": "assignee",
-		}
-		deadline := Deadline{
-			RenderType: "datePicker",
-			Value:      "",
-			NoBorder:   true,
-			Operations: map[string]interface{}{
-				"onChange": map[string]interface{}{
-					"key":      "changeDeadline",
-					"reload":   true,
-					"disabled": false,
-					"fillMeta": "deadlineValue",
-					"meta": map[string]string{
-						"id":            strconv.FormatInt(data.ID, 10),
-						"deadlineValue": "",
-					},
-				},
-			},
-		}
-		if data.PlanFinishedAt != nil {
-			deadline.Value = data.PlanFinishedAt.Format(time.RFC3339)
-		}
-		if data.PlanStartedAt != nil {
-			deadline.DisabledBefore = data.PlanStartedAt.Format(time.RFC3339)
-		}
-		closedAt := ClosedAt{
-			RenderType: "datePicker",
-			Value:      "",
-			NoBorder:   true,
-		}
-		if data.FinishTime != nil {
-			closedAt.Value = data.FinishTime.Format(time.RFC3339)
-		}
-		l = append(l, TableItem{
-			//Assignee:    map[string]string{"value": data.Assignee, "renderType": "userAvatar"},
-			Id:          strconv.FormatInt(data.ID, 10),
-			IterationID: data.IterationID,
-			Type:        string(data.Type),
-			Progress:    progress,
-			Severity:    severity,
-			Complexity:  cputil.I18n(ctx, string(data.Complexity)),
-			Priority: Priority{
-				Value:      cputil.I18n(ctx, strings.ToLower(string(data.Priority))),
-				RenderType: "operationsDropdownMenu",
-				PrefixIcon: priorityIcon[data.Priority],
-				Operations: map[string]interface{}{
-					"changePriorityTodLOW": map[string]interface{}{
-						"meta": map[string]string{
-							"id":       strconv.FormatInt(data.ID, 10),
-							"priority": "LOW",
-						},
-						"prefixIcon": priorityIcon[apistructs.IssuePriorityLow],
-						"text":       cputil.I18n(ctx, "low"),
-						"reload":     true,
-						"key":        "changePriorityTodLOW",
-					}, "changePriorityTocNORMAL": map[string]interface{}{
-						"meta": map[string]string{
-							"id":       strconv.FormatInt(data.ID, 10),
-							"priority": "NORMAL",
-						},
-						"prefixIcon": priorityIcon[apistructs.IssuePriorityNormal],
-						"text":       cputil.I18n(ctx, "normal"),
-						"reload":     true,
-						"key":        "changePriorityTocNORMAL",
-					}, "changePriorityTobHIGH": map[string]interface{}{
-						"meta": map[string]string{
-							"id":       strconv.FormatInt(data.ID, 10),
-							"priority": "HIGH",
-						},
-						"prefixIcon": priorityIcon[apistructs.IssuePriorityHigh],
-						"text":       cputil.I18n(ctx, "high"),
-						"reload":     true,
-						"key":        "changePriorityTobHIGH",
-					},
-					"changePriorityToaURGENT": map[string]interface{}{
-						"meta": map[string]string{
-							"id":       strconv.FormatInt(data.ID, 10),
-							"priority": "URGENT",
-						},
-						"prefixIcon": priorityIcon[apistructs.IssuePriorityUrgent],
-						"text":       cputil.I18n(ctx, "urgent"),
-						"reload":     true,
-						"key":        "changePriorityToaURGENT",
-					},
-				},
-				Disabled: isGuest,
-				DisabledTip: map[bool]string{
-					true: "无权限",
-				}[isGuest],
-			},
-			State: State{
-				Operations: stateOperations,
-				PrefixIcon: stateIcon[string(issuestate.StateBelong)],
-				Value:      issuestate.StateName,
-				RenderType: "operationsDropdownMenu",
-				Disabled:   stateAllDisable,
-				DisabledTip: map[bool]string{
-					true: "无权限",
-				}[stateAllDisable],
-			},
-			Assignee: Assignee{
-				Value:      data.Assignee,
-				RenderType: "memberSelector",
-				Scope:      "project",
-				Operations: AssigneeMapOperations,
-				Disabled:   isGuest,
-				DisabledTip: map[bool]string{
-					true: "无权限",
-				}[isGuest],
-			},
-			Title: Title{
-				PrefixIcon: getPrefixIcon(string(data.Type)),
-				Value:      data.Title,
-				Tags:       tags,
-				RenderType: "textWithTags",
-			},
-			Deadline: deadline,
-			ClosedAt: closedAt,
-		})
+		l = append(l, *ca.buildTableItem(ctx, &data))
 	}
 	c.Data = map[string]interface{}{}
 	c.Data["list"] = l
@@ -735,6 +533,226 @@ func (ca *ComponentAction) Render(ctx context.Context, c *cptype.Component, scen
 	urlquery := fmt.Sprintf(`{"pageNo":%d, "pageSize":%d}`, cond.PageNo, cond.PageSize)
 	c.State["issueTable__urlQuery"] = base64.StdEncoding.EncodeToString([]byte(urlquery))
 	return nil
+}
+
+func (ca *ComponentAction) buildTableItem(ctx context.Context, data *apistructs.Issue) *TableItem {
+	var issuestate *apistructs.IssueStateButton
+	for _, s := range data.IssueButton {
+		if s.StateID == data.State {
+			issuestate = &s
+			break
+		}
+	}
+	tags := []Tag{}
+	for _, datalabel := range data.Labels {
+		for _, label := range ca.labels {
+			if datalabel == label.Name {
+				tags = append(tags, Tag{Color: label.Color, Tag: label.Name})
+			}
+		}
+	}
+	progress := Progress{
+		RenderType: "progress",
+		Value:      "",
+	}
+	if data.IssueSummary != nil && data.IssueSummary.DoneCount+data.IssueSummary.ProcessingCount > 0 {
+		progress.Value = fmt.Sprintf("%d", int(100*(float64(data.IssueSummary.DoneCount)/float64(data.IssueSummary.DoneCount+data.IssueSummary.ProcessingCount))))
+	}
+
+	severityOps := map[string]interface{}{}
+	severityAuto := map[apistructs.IssueSeverity]string{
+		apistructs.IssueSeverityFatal:   "a",
+		apistructs.IssueSeveritySerious: "b",
+		apistructs.IssueSeverityNormal:  "c",
+		apistructs.IssueSeveritySlight:  "d",
+		apistructs.IssueSeverityLow:     "e",
+	}
+	for _, s := range []apistructs.IssueSeverity{
+
+		apistructs.IssueSeverityFatal,
+		apistructs.IssueSeveritySerious,
+		apistructs.IssueSeverityNormal,
+		apistructs.IssueSeveritySlight,
+		apistructs.IssueSeverityLow} {
+		severityOps["changeSeverityTo"+severityAuto[s]+string(s)] = map[string]interface{}{
+			"key":        "changeSeverityTo" + severityAuto[s] + string(s),
+			"reload":     true,
+			"text":       cputil.I18n(ctx, string(s)),
+			"prefixIcon": "ISSUE_ICON.severity." + string(s),
+			"meta": map[string]string{
+				"id":       strconv.FormatInt(data.ID, 10),
+				"severity": string(s),
+			},
+		}
+	}
+	severity := Severity{
+		RenderType: "operationsDropdownMenu",
+		Value:      cputil.I18n(ctx, strings.ToLower(string(data.Severity))),
+		PrefixIcon: "ISSUE_ICON.severity." + string(data.Severity),
+		Operations: severityOps,
+		Disabled:   ca.isGuest,
+		DisabledTip: map[bool]string{
+			true: "无权限",
+		}[ca.isGuest],
+	}
+	stateOperations := map[string]interface{}{}
+	stateAllDisable := true
+	for i, s := range data.IssueButton {
+		if s.Permission {
+			stateAllDisable = false
+		}
+		if ca.isGuest {
+			stateAllDisable = true
+		}
+		if s.Permission {
+			stateOperations["changeStateTo"+strconv.Itoa(i)+s.StateName] = map[string]interface{}{
+				"meta": map[string]string{
+					"state": strconv.FormatInt(s.StateID, 10),
+					"id":    strconv.FormatInt(data.ID, 10),
+				},
+				"prefixIcon": stateIcon[string(s.StateBelong)],
+				"text":       s.StateName,
+				"reload":     true,
+				"key":        "changeStateTo" + strconv.Itoa(i) + s.StateName,
+				"disabled":   !s.Permission,
+				"disabledTip": map[bool]string{
+					false: "无法转移",
+				}[s.Permission],
+			}
+		}
+	}
+	AssigneeMapOperations := map[string]interface{}{}
+	AssigneeMapOperations["onChange"] = map[string]interface{}{
+		"meta": map[string]string{
+			"assignee": "",
+			"id":       strconv.FormatInt(data.ID, 10),
+		},
+		"text":     ca.userMap[data.Assignee].Nick,
+		"reload":   true,
+		"key":      "updateAssignee",
+		"disabled": false,
+		"fillMeta": "assignee",
+	}
+	deadline := Deadline{
+		RenderType: "datePicker",
+		Value:      "",
+		NoBorder:   true,
+		Operations: map[string]interface{}{
+			"onChange": map[string]interface{}{
+				"key":      "changeDeadline",
+				"reload":   true,
+				"disabled": false,
+				"fillMeta": "deadlineValue",
+				"meta": map[string]string{
+					"id":            strconv.FormatInt(data.ID, 10),
+					"deadlineValue": "",
+				},
+			},
+		},
+	}
+	if data.PlanFinishedAt != nil {
+		deadline.Value = data.PlanFinishedAt.Format(time.RFC3339)
+	}
+	if data.PlanStartedAt != nil {
+		deadline.DisabledBefore = data.PlanStartedAt.Format(time.RFC3339)
+	}
+	closedAt := ClosedAt{
+		RenderType: "datePicker",
+		Value:      "",
+		NoBorder:   true,
+	}
+	if data.FinishTime != nil {
+		closedAt.Value = data.FinishTime.Format(time.RFC3339)
+	}
+	state := State{
+		Operations: stateOperations,
+		RenderType: "operationsDropdownMenu",
+		Disabled:   stateAllDisable,
+		DisabledTip: map[bool]string{
+			true: "无权限",
+		}[stateAllDisable],
+	}
+	if issuestate != nil {
+		state.PrefixIcon = stateIcon[string(issuestate.StateBelong)]
+		state.Value = issuestate.StateName
+	}
+	return &TableItem{
+		//Assignee:    map[string]string{"value": data.Assignee, "renderType": "userAvatar"},
+		Id:          strconv.FormatInt(data.ID, 10),
+		IterationID: data.IterationID,
+		Type:        string(data.Type),
+		Progress:    progress,
+		Severity:    severity,
+		Complexity:  cputil.I18n(ctx, string(data.Complexity)),
+		Priority: Priority{
+			Value:      cputil.I18n(ctx, strings.ToLower(string(data.Priority))),
+			RenderType: "operationsDropdownMenu",
+			PrefixIcon: priorityIcon[data.Priority],
+			Operations: map[string]interface{}{
+				"changePriorityTodLOW": map[string]interface{}{
+					"meta": map[string]string{
+						"id":       strconv.FormatInt(data.ID, 10),
+						"priority": "LOW",
+					},
+					"prefixIcon": priorityIcon[apistructs.IssuePriorityLow],
+					"text":       cputil.I18n(ctx, "low"),
+					"reload":     true,
+					"key":        "changePriorityTodLOW",
+				}, "changePriorityTocNORMAL": map[string]interface{}{
+					"meta": map[string]string{
+						"id":       strconv.FormatInt(data.ID, 10),
+						"priority": "NORMAL",
+					},
+					"prefixIcon": priorityIcon[apistructs.IssuePriorityNormal],
+					"text":       cputil.I18n(ctx, "normal"),
+					"reload":     true,
+					"key":        "changePriorityTocNORMAL",
+				}, "changePriorityTobHIGH": map[string]interface{}{
+					"meta": map[string]string{
+						"id":       strconv.FormatInt(data.ID, 10),
+						"priority": "HIGH",
+					},
+					"prefixIcon": priorityIcon[apistructs.IssuePriorityHigh],
+					"text":       cputil.I18n(ctx, "high"),
+					"reload":     true,
+					"key":        "changePriorityTobHIGH",
+				},
+				"changePriorityToaURGENT": map[string]interface{}{
+					"meta": map[string]string{
+						"id":       strconv.FormatInt(data.ID, 10),
+						"priority": "URGENT",
+					},
+					"prefixIcon": priorityIcon[apistructs.IssuePriorityUrgent],
+					"text":       cputil.I18n(ctx, "urgent"),
+					"reload":     true,
+					"key":        "changePriorityToaURGENT",
+				},
+			},
+			Disabled: ca.isGuest,
+			DisabledTip: map[bool]string{
+				true: "无权限",
+			}[ca.isGuest],
+		},
+		State: state,
+		Assignee: Assignee{
+			Value:      data.Assignee,
+			RenderType: "memberSelector",
+			Scope:      "project",
+			Operations: AssigneeMapOperations,
+			Disabled:   ca.isGuest,
+			DisabledTip: map[bool]string{
+				true: "无权限",
+			}[ca.isGuest],
+		},
+		Title: Title{
+			PrefixIcon: getPrefixIcon(string(data.Type)),
+			Value:      data.Title,
+			Tags:       tags,
+			RenderType: "textWithTags",
+		},
+		Deadline: deadline,
+		ClosedAt: closedAt,
+	}
 }
 
 // GetUserPermission  check Guest permission
