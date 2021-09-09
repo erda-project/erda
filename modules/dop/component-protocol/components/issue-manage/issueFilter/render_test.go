d8dbeab2d17796d61dcc0130a13ac504dddd16ae
Effet <nesuadark@gmail.com>
2021-09-09 15:12:56 +0800

    add unit test

--- modules/dop/component-protocol/components/issue-manage/issueFilter/render_test.go
+++ modules/dop/component-protocol/components/issue-manage/issueFilter/render_test.go
@@ -18,6 +18,9 @@ import (
 	"testing"
 
 	"github.com/alecthomas/assert"
+
+	"github.com/erda-project/erda-infra/providers/i18n"
+	"github.com/erda-project/erda/modules/dop/services/issuefilterbm"
 )
 
 func Test_getMeta(t *testing.T) {
@@ -29,4 +32,54 @@ func Test_getMeta(t *testing.T) {
 	var m DeleteMeta
 	assert.NoError(t, getMeta(data, &m))
 	assert.Equal(t, "123", m.ID)
+}
+
+type MockTran struct {
+	i18n.Translator
+}
+
+func (m *MockTran) Text(lang i18n.LanguageCodes, key string) string {
+	return ""
+}
+
+func (m *MockTran) Sprintf(lang i18n.LanguageCodes, key string, args ...interface{}) string {
+	return ""
+}
+
+func Test_flushOptsByFilter(t *testing.T) {
+	//ctx := context.WithValue(context.Background(), cptype.GlobalInnerKeyCtxSDK, &cptype.SDK{Tran: &MockTran{}})
+	f := ComponentFilter{}
+	// {"states":[657],"assigneeIDs":["2"]}
+	assert.NoError(t, f.flushOptsByFilter("123", "eyJzdGF0ZXMiOls2NTddLCJhc3NpZ25lZUlEcyI6WyIyIl19"))
+	assert.Equal(t, FrontendConditions{
+		FilterID:    "123",
+		States:      []int64{657},
+		AssigneeIDs: []string{"2"},
+	}, f.State.FrontendConditionValues)
+
+	f.Bms = []issuefilterbm.MyFilterBm{
+		{
+			ID:           "123",
+			FilterEntity: "eyJzdGF0ZXMiOls2NTddLCJhc3NpZ25lZUlEcyI6WyIyIl19",
+		},
+	}
+	assert.NoError(t, f.flushOptsByFilterID("123"))
+	assert.Equal(t, FrontendConditions{
+		FilterID:    "123",
+		States:      []int64{657},
+		AssigneeIDs: []string{"2"},
+	}, f.State.FrontendConditionValues)
+}
+
+func Test_determineFilterID(t *testing.T) {
+	f := ComponentFilter{}
+	assert.Equal(t, "", f.determineFilterID("eyJzdGF0ZXMiOls2NTddLCJhc3NpZ25lZUlEcyI6WyIyIl19"))
+
+	f.Bms = []issuefilterbm.MyFilterBm{
+		{
+			ID:           "123",
+			FilterEntity: "eyJzdGF0ZXMiOls2NTddLCJhc3NpZ25lZUlEcyI6WyIyIl19",
+		},
+	}
+	assert.Equal(t, "123", f.determineFilterID("eyJzdGF0ZXMiOls2NTddLCJhc3NpZ25lZUlEcyI6WyIyIl19"))
 }
