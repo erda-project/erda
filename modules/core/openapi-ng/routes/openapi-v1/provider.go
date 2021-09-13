5272f1060e8ca13ecd1be3747ee5a15d69cac866
sfwn <sfwnlin@gmail.com>
2021-09-13 17:20:47 +0800

    openapi-ng fix router for openapi-v1 match-any (#1820)

--- modules/core/openapi-ng/routes/openapi-v1/provider.go
+++ modules/core/openapi-ng/routes/openapi-v1/provider.go
@@ -70,6 +70,7 @@ func (p *provider) RegisterTo(router transhttp.Router) (err error) {
 }
 
 func replaceOpenapiV1Path(path string) string {
+	path = strings.ReplaceAll(path, "<*>", "**")
 	newPath := strings.NewReplacer("<", "{", ">", "}").Replace(path)
 	return newPath
 }
