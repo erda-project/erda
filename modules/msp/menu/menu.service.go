e050d099f09355a92c6fe2a43d907c9f691a0322
panjiayao <44628544+Counterflowwind@users.noreply.github.com>
2021-09-23 00:05:01 +0800

    msp menu modify ServiceObservation to AppMonitor (#2039)
    * modify serviceObservation is appMonitor
    
    * modify serviceObservation is appMonitor

--- modules/msp/menu/menu.service.go
+++ modules/msp/menu/menu.service.go
@@ -117,7 +117,7 @@ func (s *menuService) GetMenu(ctx context.Context, req *pb.GetMenuRequest) (*pb.
 		menuMap := make(map[string]*pb.MenuItem)
 		for _, item := range items {
 			isK8s := clusterInfo.IsK8S() || (!splitEDAS && clusterInfo.IsEDAS())
-			if item.EnName == "EnvironmentSet" || item.EnName == "ServiceObservation" {
+			if item.EnName == "EnvironmentSet" || item.EnName == "AppMonitor" {
 				fmt.Println(item.EnName, isK8s)
 				for _, child := range item.Children {
 					child.Params = item.Params
