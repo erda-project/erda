dc76a046313ba82e4d9b2d5aa4594058543b8efa
kakj-go <18579115540@163.com>
2021-10-09 16:11:49 +0800

    add code coverage components

--- modules/dop/component-protocol/components/code-coverage/endButton/render.go
+++ modules/dop/component-protocol/components/code-coverage/endButton/render.go
@@ -16,13 +16,80 @@ package head
 
 import (
 	"context"
+	"fmt"
+
 	"github.com/erda-project/erda-infra/providers/component-protocol/cptype"
+	"github.com/erda-project/erda-infra/providers/component-protocol/utils/cputil"
+	"github.com/erda-project/erda/apistructs"
+	"github.com/erda-project/erda/modules/dop/component-protocol/types"
+	"github.com/erda-project/erda/modules/dop/services/code_coverage"
 )
 
-type ComponentAction struct {
-}
+type ComponentAction struct{}
 
 func (ca *ComponentAction) Render(ctx context.Context, c *cptype.Component, scenario cptype.Scenario, event cptype.ComponentEvent, gs *cptype.GlobalStateData) error {
 
+	svc := ctx.Value(types.CodeCoverageService).(*code_coverage.CodeCoverage)
+	sdk := cputil.SDK(ctx)
+	projectId := sdk.InParams["projectId"].(uint64)
+
+	var disable = false
+	var disabledTip string
+
+	switch event.Operation.String() {
+	case apistructs.ClickOperation.String():
+		data, err := svc.ListCodeCoverageRecode(apistructs.CodeCoverageListRequest{
+			PageSize:  1,
+			PageNo:    1,
+			ProjectID: projectId,
+			Statuses: []apistructs.CodeCoverageExecStatus{
+				apistructs.ReadyStatus,
+			},
+			IdentityInfo: apistructs.IdentityInfo{
+				UserID: sdk.Identity.UserID,
+			},
+		})
+		if err != nil {
+			return err
+		}
+		if data.Total <= 0 {
+			return fmt.Errorf("not find ready recode")
+		}
+
+		err = svc.End(apistructs.CodeCoverageUpdateRequest{
+			ID: data.List[0].ID,
+			IdentityInfo: apistructs.IdentityInfo{
+				UserID: sdk.Identity.UserID,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		disable = true
+		disabledTip = "收集中"
+	case apistructs.InitializeOperation.String(), apistructs.RenderingOperation.String():
+		ok, err := svc.JudgeCanEnd(projectId)
+		if err != nil {
+			disabledTip = err.Error()
+			disable = true
+		}
+		disable = ok
+	}
+
+	c.Type = "Button"
+	c.Props = map[string]interface{}{
+		"text": "结束",
+		"type": "primary",
+	}
+
+	c.Operations = map[string]interface{}{
+		"click": map[string]interface{}{
+			"key":         "click",
+			"reload":      true,
+			"disabledTip": disabledTip,
+			"disabled":    disable,
+		},
+	}
 	return nil
 }
