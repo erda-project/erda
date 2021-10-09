6623d4dc9c6c726252e9b3edac1c2da7d3474a1f
悟空 <rainchan365@163.com>
2021-10-09 19:56:09 +0800

    feature: add resource daily model

---
+++ apistructs/resource_daily.go
@@ -0,0 +1,69 @@
+//  Copyright (c) 2021 Terminus, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package apistructs
+
+import "time"
+
+// ProjectResourceDailyModel is the model cmp_prject_resource_daily
+type ProjectResourceDailyModel struct {
+	ID        uint64    `json:"id" gorm:"id"`
+	CreatedAt time.Time `json:"created_at" gorm:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"updated_at"`
+
+	ProjectID   uint64  `json:"project_id" gorm:"project_id"`
+	ProjectName string  `json:"project_name" gorm:"project_name"`
+	CPUQuota    float64 `json:"cpu_quota" gorm:"cpu_quota"`
+	CPURequest  float64 `json:"cpu_request" gorm:"cpu_request"`
+	MemQuota    float64 `json:"mem_quota" gorm:"mem_quota"`
+	MemRequest  float64 `json:"mem_request" gorm:"mem_request"`
+}
+
+func (m ProjectResourceDailyModel) TableName() string {
+	return "cmp_prject_resource_daily"
+}
+
+func (m ProjectResourceDailyModel) CreatedDay() string {
+	return m.CreatedAt.Format("2006-01-02")
+}
+
+func (m ProjectResourceDailyModel) UpdatedDay() string {
+	return m.UpdatedAt.Format("2006-01-02")
+}
+
+//ClusterResourceDailyModel is the model cmp_cluster_resource_daily
+type ClusterResourceDailyModel struct {
+	ID        uint64    `json:"id" gorm:"id"`
+	CreatedAt time.Time `json:"created_at" gorm:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"updated_at"`
+
+	ClusterID    uint64  `json:"cluster_id" gorm:"cluster_id"`
+	ClusterName  string  `json:"cluster_name" gorm:"cluster_name"`
+	CPUTotal     float64 `json:"cpu_total" gorm:"cpu_total"`
+	CPURequested float64 `json:"cpu_requested" gorm:"cpu_requested"`
+	MemTotal     float64 `json:"mem_total" gorm:"mem_total"`
+	MemRequested float64 `json:"mem_requested" gorm:"mem_requested"`
+}
+
+func (m ClusterResourceDailyModel) TableName() string {
+	return "cmp_cluster_resource_daily"
+}
+
+func (m ClusterResourceDailyModel) CreatedDay() string {
+	return m.CreatedAt.Format("2006-01-02")
+}
+
+func (m ClusterResourceDailyModel) UpdatedDay() string {
+	return m.UpdatedAt.Format("2006-01-02")
+}
