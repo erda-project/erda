b2d49600abc0afec08da71e8e97ddee3f8e3ee81
kakj-go <18579115540@163.com>
2021-10-11 17:04:25 +0800

    asd

--- .erda/migrations/cmdb/20211008-code-coverage-exec-record.sql
+++ .erda/migrations/cmdb/20211008-code-coverage-exec-record.sql
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021 Terminus, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 CREATE TABLE `dice_code_coverage_exec_record`
 (
     `id`             bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -13,8 +29,8 @@ CREATE TABLE `dice_code_coverage_exec_record`
     `time_end`       datetime       NOT NULL DEFAULT '1000-01-01 00:00:00' COMMENT '结束时间',
     `created_at`     DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at`     DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `msg`            varchar(255)   NOT NULL DEFAULT "" COMMENT '日志信息',
-    `report_msg`     varchar(255)   NOT NULL DEFAULT "" COMMENT '报告日志信息',
+    `msg`            varchar(2000)   NOT NULL DEFAULT "" COMMENT '日志信息',
+    `report_msg`     varchar(2000)   NOT NULL DEFAULT "" COMMENT '报告日志信息',
     PRIMARY KEY (`id`),
     KEY              `idx_project_id` (`project_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='代码覆盖率执行记录';
