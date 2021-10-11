83faafdda37f03fb5b35eb40cd17381e1fad9ad6
innerpeacez <innerpeace.zhai@gmail.com>
2021-10-11 15:53:40 +0800

    go imports format

--- modules/msp/apm/checker/plugins/http/triggering/triggering_test.go
+++ modules/msp/apm/checker/plugins/http/triggering/triggering_test.go
@@ -15,12 +15,13 @@
 package triggering
 
 import (
-	"github.com/erda-project/erda-proto-go/msp/apm/checker/pb"
 	"net/http"
 	"reflect"
 	"testing"
 
 	"google.golang.org/protobuf/types/known/structpb"
+
+	"github.com/erda-project/erda-proto-go/msp/apm/checker/pb"
 )
 
 func TestTriggering_HttpCodeStrategy(t *testing.T) {
