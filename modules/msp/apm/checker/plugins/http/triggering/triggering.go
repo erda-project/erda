ab6c1b9eba152ff035d323a374c48e335f81b9da
innerpeacez <innerpeace.zhai@gmail.com>
2021-10-11 11:38:52 +0800

    impl checker for HTTP method

---
+++ modules/msp/apm/checker/plugins/http/triggering/triggering.go
@@ -0,0 +1,113 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package triggering
+
+import (
+	"github.com/erda-project/erda-proto-go/msp/apm/checker/pb"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	_ "github.com/erda-project/erda-proto-go/msp/apm/checker/pb"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+const (
+	BodyStrategy     string = "body"
+	HttpCodeStrategy string = "http_code"
+)
+
+type Interface interface {
+	Executor(resp *http.Response) bool
+	BodyStrategy(resp *http.Response) bool
+	HttpCodeStrategy(resp *http.Response) bool
+}
+
+type Triggering struct {
+	Key     string
+	Operate string
+	Value   *structpb.Value
+}
+
+func New(c *pb.Condition) *Triggering {
+	return &Triggering{
+		Key:     c.Key,
+		Operate: c.Operate,
+		Value:   c.Value,
+	}
+}
+
+func (condition *Triggering) Executor(resp *http.Response) bool {
+	switch condition.Key {
+	case HttpCodeStrategy:
+		return condition.HttpCodeStrategy(resp)
+	case BodyStrategy:
+		return condition.BodyStrategy(resp)
+	default:
+		return true
+	}
+}
+
+func (condition *Triggering) BodyStrategy(resp *http.Response) bool {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+	switch condition.Operate {
+	case "contains":
+		if strings.Contains(string(body), condition.Value.GetStringValue()) {
+			return false
+		}
+	case "not_contains":
+		if !strings.Contains(string(body), condition.Value.GetStringValue()) {
+			return false
+		}
+	default:
+		return true
+	}
+	return true
+}
+
+func (condition *Triggering) HttpCodeStrategy(resp *http.Response) bool {
+	switch condition.Operate {
+	case ">":
+		if resp.StatusCode > int(condition.Value.GetNumberValue()) {
+			return false
+		}
+	case ">=":
+		if resp.StatusCode >= int(condition.Value.GetNumberValue()) {
+			return false
+		}
+	case "=":
+		if resp.StatusCode == int(condition.Value.GetNumberValue()) {
+			return false
+		}
+	case "<":
+		if resp.StatusCode < int(condition.Value.GetNumberValue()) {
+			return false
+		}
+	case "<=":
+		if resp.StatusCode <= int(condition.Value.GetNumberValue()) {
+			return false
+		}
+	case "!=":
+		if resp.StatusCode != int(condition.Value.GetNumberValue()) {
+			return false
+		}
+	default:
+		return true
+	}
+	return true
+}
--- modules/msp/apm/checker/task/fetcher/scheduled/dispatcher.go
+++ modules/msp/apm/checker/task/fetcher/scheduled/dispatcher.go
@@ -22,6 +22,7 @@ import (
 	"github.com/erda-project/erda-infra/base/logs"
 	"github.com/erda-project/erda-proto-go/msp/apm/checker/pb"
 	"github.com/erda-project/erda/modules/msp/apm/checker/task/fetcher"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 // Dispatcher
@@ -164,7 +165,7 @@ func copyChecker(c *pb.Checker) *pb.Checker {
 		Type: c.Type,
 	}
 	if c.Config != nil {
-		ck.Config = make(map[string]string)
+		ck.Config = make(map[string]*structpb.Value)
 		for k, v := range c.Config {
 			ck.Config[k] = v
 		}
