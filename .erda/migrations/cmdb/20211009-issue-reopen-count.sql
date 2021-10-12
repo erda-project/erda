2bba0d995997b8da26ff33141cb7aa4c534a21ab
shuofan <fanshuo2015@gmail.com>
2021-10-12 11:46:29 +0800

    Reopen count init

--- .erda/migrations/cmdb/20211009-issue-reopen-count.sql
+++ .erda/migrations/cmdb/20211009-issue-reopen-count.sql
@@ -3,9 +3,7 @@ ALTER TABLE dice_issues ADD `reopen_count` int(11) NOT NULL DEFAULT 0 COMMENT 'i
 UPDATE dice_issues,
        (SELECT issue_id,
                Count(*) AS reopen
-        FROM   dice_issues
-               LEFT JOIN dice_issue_streams
-                      ON dice_issues.id = dice_issue_streams.issue_id
+        FROM   dice_issue_streams
         WHERE  stream_type = 'TransferState'
                AND stream_params LIKE '%"NewState":"重新打开"%'
         GROUP  BY issue_id) AS stats
