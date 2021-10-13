8b47d1af191815ece8ef6e9cb67c2c46202ceee2
CMC <49681321+CraigMChen@users.noreply.github.com>
2021-10-13 17:57:49 +0800

    fix: start same steve sever redundantly when watch clusters (#2364)
    * fix: start same steve server redundantly in watch clusters
    
    * add age in pods list

--- modules/cmp/component-protocol/components/cmp-dashboard-events-list/eventTable/render.go
+++ modules/cmp/component-protocol/components/cmp-dashboard-events-list/eventTable/render.go
@@ -115,21 +115,21 @@ func (t *ComponentEventTable) GenComponentState(component *cptype.Component) err
 }
 
 func (t *ComponentEventTable) DecodeURLQuery() error {
-	query, ok := t.sdk.InParams["eventTable__urlQuery"].(string)
+	queryData, ok := t.sdk.InParams["eventTable__urlQuery"].(string)
 	if !ok {
 		return nil
 	}
-	decode, err := base64.StdEncoding.DecodeString(query)
+	decoded, err := base64.StdEncoding.DecodeString(queryData)
 	if err != nil {
 		return err
 	}
-	data := make(map[string]interface{})
-	if err := json.Unmarshal(decode, &data); err != nil {
+	query := make(map[string]interface{})
+	if err := json.Unmarshal(decoded, &query); err != nil {
 		return err
 	}
-	t.State.PageNo = uint64(data["pageNo"].(float64))
-	t.State.PageSize = uint64(data["pageSize"].(float64))
-	sorter := data["sorterData"].(map[string]interface{})
+	t.State.PageNo = uint64(query["pageNo"].(float64))
+	t.State.PageSize = uint64(query["pageSize"].(float64))
+	sorter := query["sorterData"].(map[string]interface{})
 	t.State.Sorter.Field = sorter["field"].(string)
 	t.State.Sorter.Order = sorter["order"].(string)
 	return nil
