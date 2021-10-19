f0491e47f5419a19ee8a9a5852572718d0cd2c8b
Effet <nesuadark@gmail.com>
2021-10-19 16:51:33 +0800

    work with example

---
+++ modules/dop/component-protocol/components/addon-account-manage/accountTable/table/table.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package table
+
+type Props struct {
+	PageSizeOptions []string               `json:"pageSizeOptions"`
+	Columns         []*ColumnTitle         `json:"columns"`
+	RowKey          string                 `json:"rowKey"`
+	ClassName       string                 `json:"className"`
+	Title           string                 `json:"title"`
+	Visible         bool                   `json:"visible"`
+	RowSelection    map[string]interface{} `json:"rowSelection"`
+}
+
+type ColumnTitle struct {
+	Title           string      `json:"title"`
+	DataIndex       string      `json:"dataIndex"`
+	TitleRenderType string      `json:"titleRenderType"`
+	Width           int         `json:"width,omitempty"`
+	TitleTip        []string    `json:"titleTip"`
+	Data            interface{} `json:"data"`
+}
+
+type ColumnData struct {
+	Value      string                `json:"value"`
+	RenderType string                `json:"renderType,omitempty"`
+	Tags       []ColumnDataTag       `json:"tags,omitempty"`
+	Operations map[string]*Operation `json:"operations,omitempty"`
+}
+
+type ColumnDataTag struct {
+	Tag   string `json:"tag"`
+	Color string `json:"color"`
+}
