b4978f671681e0b9276f2b0acdc13f9412fdf7a4
guogangping <512979011@qq.com>
2021-10-22 20:56:05 +0800

    optimize: add unit tests.

--- modules/core/monitor/alert/alert-apis/adapt/record_test.go
+++ modules/core/monitor/alert/alert-apis/adapt/record_test.go
@@ -19,6 +19,7 @@ import (
 	"testing"
 
 	"bou.ke/monkey"
+
 	"github.com/erda-project/erda-proto-go/core/monitor/alert/pb"
 )
 
@@ -82,6 +83,66 @@ func Test_QueryAlertHistory_WithBothSuccess_Should_Return_NoneEmpty(t *testing.T
 	if err != nil {
 		t.Error("assert failed, expect err, but got nil")
 	}
+	if len(list) != len(expect) {
+		t.Errorf("assert result failed, expect: %+v, but got: %+v", expect, list)
+	}
+	for i := 0; i < len(expect); i++ {
+		if list[i].Timestamp != expect[i].Timestamp {
+			t.Errorf("assert list order failed, index: %d, expect: %+v, but got: %+v", i, expect[i], list[i])
+		}
+	}
+}
+
+func Test_mergeAlertHistories_WithEmptyResults_Should_Return_Empty(t *testing.T) {
+	a := &Adapt{}
+	result := a.mergeAlertHistories(1, [][]*pb.AlertHistory{}...)
+	if len(result) != 0 {
+		t.Errorf("merge with empty results should return empty list")
+	}
+}
+
+func Test_mergeAlertHistories_WithOneResult_Should_Return_The_One(t *testing.T) {
+	a := &Adapt{}
+	var list1 = []*pb.AlertHistory{{Timestamp: 1}}
+	result := a.mergeAlertHistories(1, list1)
+	if len(result) != len(list1) {
+		t.Errorf("merge results with one item should return exactly the one")
+	}
+}
+
+func Test_mergeAlertHistories_WithNonEmptyResults_Should_Return_SortedList(t *testing.T) {
+	var list1 = []*pb.AlertHistory{
+		{
+			Timestamp: 4,
+		},
+		{
+			Timestamp: 2,
+		},
+	}
+	var list2 = []*pb.AlertHistory{
+		{
+			Timestamp: 3,
+		},
+		{
+			Timestamp: 1,
+		},
+	}
+	expect := []*pb.AlertHistory{
+		{
+			Timestamp: 4,
+		},
+		{
+			Timestamp: 3,
+		},
+		{
+			Timestamp: 2,
+		},
+		{
+			Timestamp: 1,
+		},
+	}
+
+	var list = (&Adapt{}).mergeAlertHistories(10, list1, list2)
 	if len(list) != len(expect) {
 		t.Errorf("assert result failed, expect: %+v, but got: %+v", expect, list)
 	}
