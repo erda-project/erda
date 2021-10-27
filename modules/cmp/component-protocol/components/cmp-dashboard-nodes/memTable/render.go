77be9feff1e38a6c36567ad6f890f5aab6bf3c8f
CMC <49681321+CraigMChen@users.noreply.github.com>
2021-10-27 19:27:39 +0800

    bug fix (#2620)
    * modify width of operate in container table in pod detail
    
    * fix: node label fully match
    
    * remove cache after drain node
    
    * fix: node can not be drained, uncordoned or cordoned if which is offline
    
    * fix: add kind and version in yaml
    
    * remove node offline and online

--- modules/cmp/component-protocol/components/cmp-dashboard-nodes/memTable/render.go
+++ modules/cmp/component-protocol/components/cmp-dashboard-nodes/memTable/render.go
@@ -181,19 +181,23 @@ func (mt *MemInfoTable) GetRowItems(nodes []data.Object, tableType table.TableTy
 		}
 		batchOperations := make([]string, 0)
 		if !strings.Contains(role, "master") {
-			if strings.Contains(status.Value, mt.SDK.I18n("SchedulingDisabled")) {
-				batchOperations = append(batchOperations, "uncordon")
+			if c.String("spec", "unschedulable") == "true" {
+				if !table.IsNodeOffline(c) {
+					batchOperations = append(batchOperations, "uncordon")
+				}
 			} else {
 				batchOperations = append(batchOperations, "cordon")
 			}
 		}
 		if role == "worker" && !table.IsNodeLabelInBlacklist(c) {
+			//if !table.IsNodeOffline(c) {
 			batchOperations = append(batchOperations, "drain")
-			if !table.IsNodeOffline(c) {
-				batchOperations = append(batchOperations, "offline")
-			} else {
-				batchOperations = append(batchOperations, "online")
-			}
+			//	if c.String("spec", "unschedulable") == "true" && !table.IsNodeOffline(c) {
+			//		batchOperations = append(batchOperations, "offline")
+			//	}
+			//} else {
+			//	batchOperations = append(batchOperations, "online")
+			//}
 		}
 
 		items = append(items, table.RowItem{
@@ -252,7 +256,8 @@ func (mt *MemInfoTable) getProps() {
 		"bordered":        true,
 		"selectable":      true,
 		"pageSizeOptions": []string{"10", "20", "50", "100"},
-		"batchOperations": []string{"cordon", "uncordon", "drain", "offline", "online"},
+		//"batchOperations": []string{"cordon", "uncordon", "drain", "offline", "online"},
+		"batchOperations": []string{"cordon", "uncordon", "drain"},
 		"scroll":          table.Scroll{X: 1200},
 	}
 
