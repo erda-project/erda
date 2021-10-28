808d6a477ecd7216fda8239ab6917ffbb339f733
erenming <erenming233@gmail.com>
2021-10-28 19:04:17 +0800

    kafka topic initilizer

---
+++ modules/core/monitor/storekit/kafka/topic/initializer/provider_test.go
@@ -0,0 +1,78 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package initializer
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func Test_getTopicList(t *testing.T) {
+	type args struct {
+		topics   []topic
+		metadata *kafka.Metadata
+	}
+	tests := []struct {
+		name string
+		args args
+		want []kafka.TopicSpecification
+	}{
+		{
+			name: "normal",
+			args: args{
+				topics: []topic{
+					{Name: "abc"},
+					{Name: "edf"},
+					{Name: "xyz"},
+				},
+				metadata: &kafka.Metadata{
+					Brokers: nil,
+					Topics: map[string]kafka.TopicMetadata{
+						"abc": {
+							Topic: "abc",
+							Error: kafka.NewError(kafka.ErrNoError, "", false),
+						},
+						"edf": {
+							Topic: "edf",
+							Error: kafka.NewError(kafka.ErrAllBrokersDown, "", false),
+						},
+					},
+					OriginatingBroker: kafka.BrokerMetadata{},
+				},
+			},
+			want: []kafka.TopicSpecification{
+				{
+					Topic:             "edf",
+					NumPartitions:     3,
+					ReplicationFactor: 1,
+				},
+				{
+					Topic:             "xyz",
+					NumPartitions:     3,
+					ReplicationFactor: 1,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getTopicList(tt.args.topics, tt.args.metadata); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getTopicList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
