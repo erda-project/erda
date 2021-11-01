fdb645970e9043a4f388164367ef70d5b0c30d67
悟空 <rainchan365@163.com>
2021-11-01 14:54:59 +0800

    migration: update old project quota

---
+++ .erda/migrations/cmdb/20211101-update-project-quota.sql
@@ -0,0 +1,10 @@
+UPDATE ps_group_projects
+SET cpu_quota=0,
+    mem_quota=0;
+
+UPDATE ps_group_projects a, ps_group_projects_quota b
+SET a.cpu_quota=(b.prod_cpu_quota + b.staging_cpu_quota + b.test_cpu_quota + b.dev_cpu_quota) / 1000,
+    a.mem_quota=(b.prod_mem_quota + b.staging_mem_quota + b.test_mem_quota + b.dev_mem_quota) / 1024 / 1024 / 1024
+WHERE a.id = b.project_id
+  AND b.project_id IS NOT NULL
+;
