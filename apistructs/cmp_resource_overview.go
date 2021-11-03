544aaf851d72ad9d43c9a2d9b037de8cd2acdcce
悟空 <rainchan365@163.com>
2021-11-03 16:31:52 +0800

    Feature/resource overview refact table (#2821)
    * feature: group by owner
    
    * feature: add ut
    
    * feature: rm cookie value

--- apistructs/cmp_resource_overview.go
+++ apistructs/cmp_resource_overview.go
@@ -15,6 +15,8 @@
 package apistructs
 
 import (
+	"sort"
+
 	calcu "github.com/erda-project/erda/pkg/resourcecalculator"
 )
 
@@ -25,25 +27,85 @@ type ResourceOverviewReportData struct {
 }
 
 type ResourceOverviewReportDataItem struct {
-	ProjectID          int64   `json:"projectID"`
-	ProjectName        string  `json:"projectName"`
-	ProjectDisplayName string  `json:"projectDisplayName"`
-	ProjectDesc        string  `json:"projectDesc"`
+	ProjectID          int64   `json:"projectID,omitempty"`
+	ProjectName        string  `json:"projectName,omitempty"`
+	ProjectDisplayName string  `json:"projectDisplayName,omitempty"`
+	ProjectDesc        string  `json:"projectDesc,omitempty"`
 	OwnerUserID        int64   `json:"ownerUserID"`
 	OwnerUserName      string  `json:"ownerUserName"`
 	OwnerUserNickName  string  `json:"ownerUserNickname"`
 	CPUQuota           float64 `json:"cpuQuota"`
-	CPUWaterLevel      float64 `json:"cpuWaterLevel"`
-	MemQuota           float64 `json:"memQuota"`
-	MemWaterLevel      float64 `json:"memWaterLevel"`
-	Nodes              float64 `json:"nodes"`
+	CPURequest         float64 `json:"cpuRequest"`
+	// CPUWaterLevel = CPURequest / CPUQuota
+	CPUWaterLevel float64 `json:"cpuWaterLevel"`
+	MemQuota      float64 `json:"memQuota"`
+	MemRequest    float64 `json:"memRequest"`
+	// MemWaterLevel = MemRequest / MemQuota
+	MemWaterLevel float64 `json:"memWaterLevel"`
+	Nodes         float64 `json:"nodes"`
 }
 
-func (data *ResourceOverviewReportData) Sum() {
+func (data *ResourceOverviewReportData) GroupByOwner() {
+	var (
+		list []*ResourceOverviewReportDataItem
+		m    = make(map[string]*ResourceOverviewReportDataItem)
+	)
+	for _, item := range data.List {
+		owner, ok := m[item.OwnerUserName]
+		if !ok {
+			owner = &ResourceOverviewReportDataItem{
+				OwnerUserID:       item.OwnerUserID,
+				OwnerUserName:     item.OwnerUserName,
+				OwnerUserNickName: item.OwnerUserNickName,
+			}
+		}
+		owner.CPUQuota += item.CPUQuota
+		owner.CPURequest += item.CPURequest
+		owner.MemQuota += item.MemQuota
+		owner.MemRequest += item.MemRequest
+		m[owner.OwnerUserName] = owner
+	}
+	for _, item := range m {
+		list = append(list, item)
+	}
+	data.List = list
+}
+
+func (data *ResourceOverviewReportData) Calculates(cpuPerNode, memPerNode uint64) {
+	if cpuPerNode == 0 {
+		cpuPerNode = 8
+	}
+	if memPerNode == 0 {
+		memPerNode = 32
+	}
+	for _, item := range data.List {
+		if item == nil {
+			continue
+		}
+		if item.CPUQuota != 0 {
+			item.CPUWaterLevel = calcu.Accuracy(item.CPURequest/item.CPUQuota*100, 2)
+		}
+		if item.MemQuota != 0 {
+			item.MemWaterLevel = calcu.Accuracy(item.MemRequest/item.MemQuota*100, 2)
+		}
+		item.Nodes = item.CPUQuota / float64(cpuPerNode)
+		if nodes := item.MemQuota / float64(memPerNode); nodes > item.Nodes {
+			item.Nodes = nodes
+		}
+		item.Nodes = calcu.Accuracy(item.Nodes, 1)
+	}
+	data.sum()
+	data.sort()
+}
+
+func (data *ResourceOverviewReportData) sum() {
 	data.Total = len(data.List)
 	if data.Summary == nil {
 		data.Summary = new(ResourceOverviewReportSumary)
 	}
+	data.Summary.CPU = 0
+	data.Summary.Memory = 0
+	data.Summary.Node = 0
 	for _, item := range data.List {
 		data.Summary.CPU += item.CPUQuota
 		data.Summary.Memory += item.MemQuota
@@ -52,6 +114,12 @@ func (data *ResourceOverviewReportData) Sum() {
 	data.Summary.CPU = calcu.Accuracy(data.Summary.CPU, 3)
 	data.Summary.Memory = calcu.Accuracy(data.Summary.Memory, 3)
 	data.Summary.Node = calcu.Accuracy(data.Summary.Node, 1)
+}
+
+func (data *ResourceOverviewReportData) sort() {
+	sort.Slice(data.List, func(i, j int) bool {
+		return data.List[i].Nodes > data.List[j].Nodes
+	})
 }
 
 type ResourceOverviewReportSumary struct {
