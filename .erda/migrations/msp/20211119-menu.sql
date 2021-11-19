fffbe7e1a902a7aadab34e231ea181ece213d52b
guogangping <512979011@qq.com>
2021-11-19 16:14:39 +0800

    bugfix: update microservice menu jump key

---
+++ .erda/migrations/msp/20211119-menu.sql
@@ -0,0 +1,3 @@
+UPDATE `tb_tmc_ini` SET `ini_value` = 'ConfigCenter' WHERE `ini_name` = 'MK_JUMP_config-center';
+UPDATE `tb_tmc_ini` SET `ini_value` = 'RegisterCenter' WHERE `ini_name` = 'MK_JUMP_registercenter';
+UPDATE `tb_tmc_ini` SET `ini_value` = 'APIGateway' WHERE `ini_name` = 'MK_JUMP_api-gateway';
