fdea8664dbf9d7cd3998aecb835c4cf3916702b6
悟空 <rainchan365@163.com>
2021-12-07 13:51:37 +0800

    feature: add Dump function

--- pkg/database/sqlparser/snapshot/snapshot.go
+++ pkg/database/sqlparser/snapshot/snapshot.go
@@ -16,14 +16,19 @@ package snapshot
 
 import (
 	"bytes"
+	"fmt"
+	"math/rand"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/pingcap/parser"
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/format"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 
 	"github.com/erda-project/erda/pkg/database/sqlparser/table"
@@ -34,12 +39,17 @@ var (
 	defaultCharsetWhite = []string{"utf8", "utf8mb4"}
 )
 
+// Snapshot maintains the structure of the database tables
 type Snapshot struct {
 	tables map[string]*table.Table
+	from   *gorm.DB
 }
 
+// From snapshots the structure of the database tables, and returns the Snapshot.
+// tx is the connection handler of the goal DB.
+// ignore is the tables you do not want to snapshot.
 func From(tx *gorm.DB, ignore ...string) (s *Snapshot, err error) {
-	s = &Snapshot{tables: make(map[string]*table.Table)}
+	s = &Snapshot{tables: make(map[string]*table.Table), from: tx}
 	ignores := make(map[string]bool, len(ignore))
 	for _, ig := range ignore {
 		ignores[ig] = true
@@ -83,7 +93,9 @@ func From(tx *gorm.DB, ignore ...string) (s *Snapshot, err error) {
 		if err = tx.Row().Scan(&tableName, &stmt); err != nil {
 			return nil, err
 		}
+		// some character set can not be parsed, so trim them from the stmt
 		stmt = TrimCharacterSetFromRawCreateTableSQL(stmt, CharsetWhite()...)
+		// block format syntax can not be parsed, so trim it
 		stmt = TrimBlockFormat(stmt)
 		node, err := parser.New().ParseOneStmt(stmt, "", "")
 		if err != nil {
@@ -97,6 +109,7 @@ func From(tx *gorm.DB, ignore ...string) (s *Snapshot, err error) {
 	return s, err
 }
 
+// DDLNodes returns DDLs
 func (s *Snapshot) DDLNodes() []ast.DDLNode {
 	var nodes []ast.DDLNode
 	for _, t := range s.tables {
@@ -105,10 +118,12 @@ func (s *Snapshot) DDLNodes() []ast.DDLNode {
 	return nodes
 }
 
+// HasAnyTable returns true if there is any table in the Snapshot
 func (s *Snapshot) HasAnyTable() bool {
 	return len(s.tables) > 0
 }
 
+// TableNames returns all tables names
 func (s *Snapshot) TableNames() []string {
 	var names []string
 	for k := range s.tables {
@@ -117,12 +132,69 @@ func (s *Snapshot) TableNames() []string {
 	return names
 }
 
-func (s *Snapshot) RecoverTo(tx *gorm.DB) error {
-	nodes := s.DDLNodes()
-
+// Dump dumps data from DB.
+// tableName is the table you want to dump.
+// lines is the numbers of lines you want to dump.
+// Note: lines is an approximate number rather than an exact number.
+// Note: the max data size is not more than 1<<16 (the MySQL default max placeholders size).
+func (s *Snapshot) Dump(tableName string, lines uint64) ([]map[string]interface{}, uint64, error) {
 	var (
-		installing = make(map[string]*ast.CreateTableStmt, 0)
-		installed  = make(map[string]*ast.CreateTableStmt, 0)
+		selectCount = fmt.Sprintf("SELECT COUNT(*) FROM `%s`", tableName)
+		//selectAll   = fmt.Sprintf("SELECT * FROM (select * from `%s` ORDER BY RAND()) LIMIT 20000", tableName)
+		selectAll = fmt.Sprintf("SELECT * FROM `%s` ORDER BY RAND() LIMIT %d", tableName, lines)
+		count     uint64
+	)
+	tx := s.from.Raw(selectCount)
+	if err := tx.Error; err != nil {
+		return nil, 0, errors.Wrapf(err, "failed to Raw(%s)", strconv.Quote(selectCount))
+	}
+	if err := tx.Row().Scan(&count); err != nil {
+		return nil, 0, errors.Wrapf(err, "failed to Scan count from %s", strconv.Quote(selectCount))
+	}
+	if count == 0 {
+		return nil, count, nil
+	}
+	if err := tx.Raw(selectAll).Error; err != nil {
+		return nil, count, errors.Wrapf(err, "failed to Raw(%s)", strconv.Quote(selectAll))
+	}
+	rows, err := tx.Rows()
+	if err != nil {
+		return nil, count, errors.Wrapf(err, "failed to Rows from %s", strconv.Quote(selectAll))
+	}
+	defer rows.Close()
+
+	rand.Seed(time.Now().Unix())
+	var data []map[string]interface{}
+	for rows.Next() {
+		columns, _ := rows.Columns()
+		values := make([]interface{}, len(columns))
+		for i := 0; i < len(values); i++ {
+			values[i] = &values[i]
+		}
+		if err := rows.Scan(values...); err != nil {
+			return nil, count, errors.Wrapf(err, "failed to Scan %v, from %s", columns, strconv.Quote(selectAll))
+		}
+		var record = make(map[string]interface{})
+		for i := 0; i < len(columns); i++ {
+			record[columns[i]] = values[i]
+		}
+		data = append(data, record)
+		// (1<<16)-1 is the max placeholders size to insert to for mysql default
+		if (len(data)+1)*len(columns) >= 1<<16 {
+			break
+		}
+	}
+	return data, count, nil
+}
+
+// RecoverTo recover the data structure and data to goal db
+func (s *Snapshot) RecoverTo(tx *gorm.DB) error {
+	var (
+		l          = logrus.WithField("func", "*Snapshot.RecoverTo")
+		nodes      = s.DDLNodes()
+		installing = make(map[string]*ast.CreateTableStmt)
+		installed  = make(map[string]*ast.CreateTableStmt)
+		inserted   = make(map[string]*ast.CreateTableStmt)
 	)
 
 	for _, ddl := range nodes {
@@ -130,10 +202,14 @@ func (s *Snapshot) RecoverTo(tx *gorm.DB) error {
 		installing[create.Table.Name.String()] = create
 	}
 
-	// f is used to execute every CreateTableStmt .
-	// f can resolve foreign key dependencies recursively.
-	var f func(stmt *ast.CreateTableStmt) error
-	f = func(create *ast.CreateTableStmt) error {
+	var (
+		// createF is used to execute every CreateTableStmt .
+		// createF can resolve foreign key dependencies recursively.
+		createF func(*ast.CreateTableStmt) error
+		// insertF dumps data and inserts into new db.
+		insertF func(*ast.CreateTableStmt, uint64) (int, uint64, error)
+	)
+	createF = func(create *ast.CreateTableStmt) error {
 		// skip installed table
 		if _, ok := installed[create.Table.Name.String()]; ok {
 			return nil
@@ -143,7 +219,7 @@ func (s *Snapshot) RecoverTo(tx *gorm.DB) error {
 		for _, constraint := range create.Constraints {
 			if constraint.Tp == ast.ConstraintForeignKey {
 				if alia, ok := installing[constraint.Refer.Table.Name.String()]; ok {
-					if err := f(alia); err != nil {
+					if err := createF(alia); err != nil {
 						return err
 					}
 				}
@@ -172,10 +248,66 @@ func (s *Snapshot) RecoverTo(tx *gorm.DB) error {
 		installed[create.Table.Name.String()] = create
 		return nil
 	}
+	insertF = func(create *ast.CreateTableStmt, lines uint64) (int, uint64, error) {
+		// skip inserted table
+		if _, ok := inserted[create.Table.Name.String()]; ok {
+			return 0, 0, nil
+		}
+
+		// resolve reference dependencies
+		exLines := lines * 5
+		for _, constraint := range create.Constraints {
+			if constraint.Tp == ast.ConstraintForeignKey {
+				if alia, ok := installing[constraint.Refer.Table.Name.String()]; ok {
+					lines = exLines
+					if n, count, err := insertF(alia, lines); err != nil {
+						return n, count, err
+					}
+				}
+			}
+		}
+
+		// insert
+		inserted[create.Table.Name.String()] = create
+		data, count, err := s.Dump(create.Table.Name.String(), lines)
+		if err != nil {
+			return 0, count, err
+		}
+		n := len(data)
+		if n == 0 {
+			return n, count, nil
+		}
+		if err = tx.Table(create.Table.Name.String()).Create(data).Error; err == nil {
+			return n, count, nil
+		}
+		l.WithError(err).
+			WithField("func", "*Snapshot.RecoverTo.insertF").
+			WithField("tableName", create.Table.Name.String()).
+			Warnln("failed to bulk insert, try to insert one by one item")
+		n = 0
+		for _, item := range data {
+			if err := tx.Table(create.Table.Name.String()).Create(item).Error; err != nil {
+				if IsCannotAddOrUpdateAChildRowError(err) {
+					continue
+				}
+				return n, count, err
+			}
+			n++
+		}
+
+		return n, count, nil
+	}
 
 	for _, create := range installing {
-		if err := f(create); err != nil {
+		if err := createF(create); err != nil {
 			return err
+		}
+		if Sampling() {
+			n, count, err := insertF(create, MaxSampling())
+			l.WithField("tableName", create.Table.Name.String()).Infof("collect %d/%d (sampling/total) lines", n, count)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
