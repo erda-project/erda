d82113873055327c3abc82c4d1b637472b4ce09d
chengjoey <30427474+chengjoey@users.noreply.github.com>
2021-12-08 13:51:56 +0800

    pipeline loop task clean result (#3270)

--- modules/pipeline/dbclient/op_pipeline_task.go
+++ modules/pipeline/dbclient/op_pipeline_task.go
@@ -262,3 +262,14 @@ func (client *Client) DeletePipelineTasksByPipelineID(pipelineID uint64, ops ...
 		return err
 	}, 3, time.Second)
 }
+
+func (client *Client) CleanPipelineTaskResult(id uint64, ops ...SessionOption) error {
+	session := client.NewSession(ops...)
+	defer session.Close()
+
+	if _, err := session.Table("pipeline_tasks").ID(id).
+		Update(map[string]interface{}{"result": nil}); err != nil {
+		return err
+	}
+	return nil
+}
