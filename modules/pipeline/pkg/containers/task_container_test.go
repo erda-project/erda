9ecd89758957fb7fc99595cef49a542874ab884a
chengjoey <30427474+chengjoey@users.noreply.github.com>
2021-12-08 13:31:27 +0800

    add terminus_define_tag lable for spark job (#3275)

--- modules/pipeline/pkg/containers/task_container_test.go
+++ modules/pipeline/pkg/containers/task_container_test.go
@@ -49,4 +49,19 @@ func TestGenContainers(t *testing.T) {
 	k8sflinkContainers, err := GenContainers(k8sflinkTask)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, 3, len(k8sflinkContainers))
+
+	k8sSparkTask := &spec.PipelineTask{
+		Name: "k8sjob",
+		Extra: spec.PipelineTaskExtra{
+			UUID: "pipeline-123456",
+			Action: pipelineyml.Action{
+				Params: map[string]interface{}{
+					"bigDataConf": "{\n      \"class\": \"io.terminus.erda.fdp.SparkSqlMain\",\n      \"image\": \"docker.io/terminus/spark-py:ai_v1.0\",\n      \"resource\": \"local:///opt/spark/py_action/jobs.py\",\n      \"sparkConf\": {\n        \"deps\": {\n          \"pyFiles\": [\n            \"local:///opt/spark/py_action/predict.zip\"\n          ]\n        },\n        \"driverResource\": {\n          \"cpu\": \"1\",\n          \"memory\": \"1024m\",\n          \"replica\": 1\n        },\n        \"executorResource\": {\n          \"cpu\": \"1\",\n          \"memory\": \"1024m\",\n          \"replica\": 1\n        },\n        \"kind\": \"cluster\",\n        \"type\": \"Java\"\n      }\n    }",
+				},
+			},
+		},
+	}
+	k8sSparkContainers, err := GenContainers(k8sSparkTask)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 2, len(k8sSparkContainers))
 }
