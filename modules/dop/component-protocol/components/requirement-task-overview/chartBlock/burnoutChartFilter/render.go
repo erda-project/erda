80ff796981318c7ffe871ddb73bc205fd554a1da
wangxiaojian <17816869670@163.com>
2021-12-09 09:33:45 +0800

    add requirement-task-overview scenario

---
+++ modules/dop/component-protocol/components/requirement-task-overview/chartBlock/burnoutChartFilter/render.go
@@ -0,0 +1,126 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package burnoutChartFilter
+
+import (
+	"context"
+
+	"github.com/erda-project/erda-infra/base/servicehub"
+	"github.com/erda-project/erda-infra/providers/component-protocol/cptype"
+	"github.com/erda-project/erda/apistructs"
+	"github.com/erda-project/erda/modules/dop/component-protocol/components/requirement-task-overview/common"
+	"github.com/erda-project/erda/modules/dop/component-protocol/components/requirement-task-overview/common/gshelper"
+	"github.com/erda-project/erda/modules/openapi/component-protocol/components/base"
+	"github.com/erda-project/erda/modules/openapi/component-protocol/components/filter"
+)
+
+func init() {
+	base.InitProviderWithCreator(common.ScenarioKeyTestDashboard, "burnoutChartFilter", func() servicehub.Provider {
+		return &Filter{}
+	})
+}
+
+func (f *Filter) Render(ctx context.Context, c *cptype.Component, scenario cptype.Scenario, event cptype.ComponentEvent, gs *cptype.GlobalStateData) error {
+	if err := f.InitFromProtocol(ctx, c); err != nil {
+		return err
+	}
+
+	f.Props = filter.Props{
+		Delay: 500,
+	}
+
+	f.Operations = map[filter.OperationKey]filter.Operation{
+		OperationKeyFilter: {
+			Key:    OperationKeyFilter,
+			Reload: true,
+		},
+		OperationOwnerSelectMe: {
+			Key:    OperationOwnerSelectMe,
+			Reload: true,
+		},
+	}
+
+	f.State = State{
+		Conditions: []filter.PropCondition{
+			{
+				CustomProps: map[string]interface{}{
+					"mode": "single",
+				},
+				Key:       "type",
+				Label:     "类型",
+				EmptyText: "全部",
+				Fixed:     true,
+				Type:      filter.PropConditionTypeSelect,
+				Options: []filter.PropConditionOption{
+					{
+						Label: "需求",
+						Value: "requirement",
+					},
+					{
+						Label: "任务",
+						Value: "task",
+					},
+				},
+			},
+			{
+				CustomProps: map[string]interface{}{
+					"mode": "single",
+				},
+				Key:       "dimension",
+				Label:     "维度",
+				EmptyText: "全部",
+				Fixed:     true,
+				Type:      filter.PropConditionTypeSelect,
+				Options: []filter.PropConditionOption{
+					{
+						Label: "按事项个数",
+						Value: "total",
+					},
+					{
+						Label: "按事项工时",
+						Value: "workTime",
+					},
+				},
+			},
+		},
+		Values: Values{
+			Type: func() string {
+				if event.Operation != cptype.OperationKey(f.Operations[OperationKeyFilter].Key) ||
+					f.State.Values.Type == "" {
+					return "requirement"
+				}
+				return f.State.Values.Type
+			}(),
+			Dimension: func() string {
+				if event.Operation != cptype.OperationKey(f.Operations[OperationKeyFilter].Key) ||
+					f.State.Values.Dimension == "" {
+					return "total"
+				}
+				return f.State.Values.Dimension
+			}(),
+		},
+	}
+
+	h := gshelper.NewGSHelper(gs)
+	h.SetBurnoutChartType(f.State.Values.Type)
+	h.SetBurnoutChartDimension(f.State.Values.Dimension)
+
+	return f.SetToProtocolComponent(c)
+}
+
+func (f *Filter) InitDefaultOperation(ctx context.Context, iterations []apistructs.Iteration) error {
+
+	return nil
+}
---
+++ modules/dop/component-protocol/components/requirement-task-overview/distributePage/topFilter/model.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package topFilter
+
+import (
+	"github.com/erda-project/erda-infra/providers/component-protocol/cptype"
+	"github.com/erda-project/erda/apistructs"
+	"github.com/erda-project/erda/bundle"
+	"github.com/erda-project/erda/modules/dop/component-protocol/components/requirement-task-overview/common"
+	"github.com/erda-project/erda/modules/dop/dao"
+	"github.com/erda-project/erda/modules/dop/services/issue"
+	"github.com/erda-project/erda/modules/openapi/component-protocol/components/base"
+	"github.com/erda-project/erda/modules/openapi/component-protocol/components/filter"
+)
+
+type ComponentFilter struct {
+	filter.CommonFilter
+	base.DefaultProvider
+
+	sdk      *cptype.SDK
+	bdl      *bundle.Bundle
+	issueSvc *issue.Issue
+	State    State    `json:"state,omitempty"`
+	InParams InParams `json:"-"`
+
+	// local vars
+	Iterations     []apistructs.Iteration  `json:"-"`
+	Members        []apistructs.Member     `json:"-"`
+	IssueList      []dao.IssueItem         `json:"-"`
+	IssueStateList []dao.IssueState        `json:"-"`
+	Stages         []apistructs.IssueStage `json:"-"`
+}
+
+type State struct {
+	Conditions           []filter.PropCondition    `json:"conditions,omitempty"`
+	Values               common.FrontendConditions `json:"values,omitempty"`
+	Base64UrlQueryParams string                    `json:"filter__urlQuery,omitempty"`
+}
+
+type InParams struct {
+	FrontEndProjectID string `json:"projectId,omitempty"`
+	FrontendUrlQuery  string `json:"filter__urlQuery,omitempty"`
+	ProjectID         uint64
+}
+
+const OperationKeyFilter filter.OperationKey = "filter"
+const OperationOwnerSelectMe filter.OperationKey = "ownerSelectMe"
