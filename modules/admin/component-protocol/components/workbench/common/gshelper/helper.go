6559e90013a3676b8d04272dbef2df2fee724213
WeiXuSeu <xuweiroot@163.com>
2021-12-22 15:36:47 +0800

    admin cp common handler

---
+++ modules/admin/component-protocol/components/workbench/common/gshelper/helper.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gshelper
+
+import (
+	"github.com/erda-project/erda-infra/providers/component-protocol/cptype"
+	"github.com/erda-project/erda-infra/providers/component-protocol/utils/cputil"
+)
+
+const (
+	KeyWorkbenchItemType = "WorkbenchItemType"
+	KeyFilterName        = "FilterName"
+)
+
+type GSHelper struct {
+	gs *cptype.GlobalStateData
+}
+
+func NewGSHelper(gs *cptype.GlobalStateData) *GSHelper {
+	return &GSHelper{gs: gs}
+}
+
+func (h *GSHelper) SetWorkbenchItemType(wbType string) {
+	if h.gs == nil {
+		return
+	}
+	(*h.gs)[KeyWorkbenchItemType] = wbType
+}
+
+func (h *GSHelper) GetWorkbenchItemType() (string, bool) {
+	if h.gs == nil {
+		return "", false
+	}
+	v, ok := (*h.gs)[KeyWorkbenchItemType]
+	if !ok {
+		return "", false
+	}
+	var wbType string
+	cputil.MustObjJSONTransfer(v, &wbType)
+	return wbType, true
+}
+
+func (h *GSHelper) SetFilterName(name string) {
+	if h.gs == nil {
+		return
+	}
+	(*h.gs)[KeyFilterName] = name
+}
+
+func (h *GSHelper) GetFilterName() (string, bool) {
+	if h.gs == nil {
+		return "", false
+	}
+	v, ok := (*h.gs)[KeyFilterName]
+	if !ok {
+		return "", false
+	}
+	var name string
+	cputil.MustObjJSONTransfer(v, &name)
+	return name, true
+}
