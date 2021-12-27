5c1e9989ff84c5a213dc7d4bb47af5ff318795ee
wangxiaojian <17816869670@163.com>
2021-12-27 17:47:44 +0800

    the count of addon filter deleted

--- modules/orchestrator/dbclient/addon_instance.go
+++ modules/orchestrator/dbclient/addon_instance.go
@@ -275,7 +275,8 @@ func (db *DBClient) ListAddonInstancesByProjectIDs(projectIDs []uint64, exclude 
 func (db *DBClient) CountAddonReferenceByClusterAndOrg(clusterName, orgID string) (int, error) {
 	var total int
 
-	dbClient := db.Where("org_id = ?", orgID).Where("az = ?", clusterName)
+	dbClient := db.Where("org_id = ?", orgID).Where("az = ?", clusterName).
+		Where("is_deleted = ?", apistructs.AddonNotDeleted)
 	if err := dbClient.Model(&AddonInstance{}).Count(&total).Error; err != nil {
 		return 0, err
 	}
