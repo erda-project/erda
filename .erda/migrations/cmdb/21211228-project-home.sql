6ff38fd0bcccade9c42a16ae9a76e9d2730f0ec3
shuofan <fanshuo2015@gmail.com>
2021-12-28 20:18:05 +0800

    Add project home content related api

---
+++ .erda/migrations/cmdb/21211228-project-home.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `erda_project_home` (
+    `id` varchar(36) NOT NULL COMMENT 'primary key',
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'created time',
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'updated time',
+    `project_id` varchar(36) NOT NULL COMMENT 'project id',
+    `readme` text NOT NULL COMMENT 'text',
+    `links` text NOT NULL COMMENT 'links',
+    `updater_id`    varchar(191)  NOT NULL default '' COMMENT 'updater user id',
+    PRIMARY KEY (`id`),
+    KEY `idx_project_id` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='project home content';
