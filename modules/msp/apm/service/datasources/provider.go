f267d844dd6405d40c101d3d92a0a38a4d155a5c
guogangping <512979011@qq.com>
2021-12-30 17:08:21 +0800

    feature: add interface for datasource

--- modules/msp/apm/service/datasources/provider.go
+++ modules/msp/apm/service/datasources/provider.go
@@ -15,16 +15,28 @@
 package datasources
 
 import (
+	"context"
+
 	"github.com/erda-project/erda-infra/base/logs"
 	"github.com/erda-project/erda-infra/base/servicehub"
 	"github.com/erda-project/erda-infra/providers/i18n"
 	metricpb "github.com/erda-project/erda-proto-go/core/monitor/metric/pb"
+	"github.com/erda-project/erda-proto-go/msp/apm/service/pb"
+	"github.com/erda-project/erda/modules/msp/apm/service/view/card"
+	"github.com/erda-project/erda/modules/msp/apm/service/view/common"
+	"github.com/erda-project/erda/modules/msp/apm/service/view/table"
 )
 
 type provider struct {
 	Log    logs.Logger
 	I18n   i18n.Translator              `autowired:"i18n" translator:"msp-i18n"`
 	Metric metricpb.MetricServiceServer `autowired:"erda.core.monitor.metric.MetricService"`
+}
+
+type ServiceDataSource interface {
+	GetChart(ctx context.Context, chartType pb.ChartType, start, end int64, tenantId, serviceId string, layer common.TransactionLayerType, path string) (interface{}, error)
+	GetCard(ctx context.Context, cardType card.CardType, start, end int64, tenantId, serviceId string, layer common.TransactionLayerType, path string) (interface{}, error)
+	GetTable(ctx context.Context, tableType table.TableType, start, end int64, tenantId, serviceId string, layer common.TransactionLayerType, path string, pageNo int, pageSize int) (interface{}, error)
 }
 
 func init() {
