41fc5eb00e98a4f51c2ef02cd562a60464722fb9
guogangping <512979011@qq.com>
2021-12-30 21:56:37 +0800

    feature: import msp-datasource

--- cmd/msp/main.go
+++ cmd/msp/main.go
@@ -92,6 +92,7 @@ import (
 	_ "github.com/erda-project/erda-infra/providers/component-protocol"
 	// components
 	_ "github.com/erda-project/erda/modules/msp/apm/service/components"
+	_ "github.com/erda-project/erda/modules/msp/apm/service/datasources"
 )
 
 func main() {
