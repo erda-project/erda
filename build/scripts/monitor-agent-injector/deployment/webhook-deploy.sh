1bd6fdcacd15efba57a4dabe034fc2bf1da46562
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-12-30 15:40:59 +0800

    fix: Update webhook deploy script (#3555)
    * Update webhook deploy script
    
    * remove un used options

--- build/scripts/monitor-agent-injector/deployment/webhook-deploy.sh
+++ build/scripts/monitor-agent-injector/deployment/webhook-deploy.sh
@@ -7,7 +7,7 @@ keydir="/netdata/dice-ops/dice-config/certificates"
 
 # Read the PEM-encoded CA certificate, base64 encode it, and replace the `${CA_PEM_B64}` placeholder in the YAML
 # template with it. Then, create the Kubernetes resources.
-ca_pem_b64="$(openssl base64 -A <"${keydir}/monitor-agent-ca.pem")"
+ca_pem_b64="$(openssl base64 -A <"${keydir}/monitor-agent-injector-ca.pem")"
 sed -e 's@${CA_PEM_B64}@'"$ca_pem_b64"'@g' <"${basedir}/webhook.yaml.template" \
     | kubectl create -f -
 
