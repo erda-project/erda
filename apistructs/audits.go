4d92cbfd9500777f861cd35177f091e55891a46f
CMC <49681321+CraigMChen@users.noreply.github.com>
2021-12-31 14:57:16 +0800

    add audit (#3590)

--- apistructs/audits.go
+++ apistructs/audits.go
@@ -231,6 +231,16 @@ const (
 	RepoLockedTemplate   TemplateName = "repoLocked"
 	DeleteTagTemplate    TemplateName = "deleteTag"
 	DeleteBranchTemplate TemplateName = "deleteBranch"
+	// =========================release=====================================
+	CreateProjectReleaseTemplate TemplateName = "createProjectRelease"
+	UpdateProjectReleaseTemplate TemplateName = "updateProjectRelease"
+	UpdateAppReleaseTemplate     TemplateName = "updateAppRelease"
+	DeleteProjectReleaseTemplate TemplateName = "deleteProjectRelease"
+	DeleteAppReleaseTemplate     TemplateName = "deleteAppRelease"
+	FormalProjectReleaseTemplate TemplateName = "formalProjectRelease"
+	FormalAppReleaseTemplate     TemplateName = "formalAppRelease"
+	BatchDeleteReleaseTemplate   TemplateName = "batchDeleteRelease"
+	BatchFormalReleaseTemplate   TemplateName = "batchFormalRelease"
 )
 
 // AuditTemplateMap 解析前端审计模版全家桶
@@ -392,6 +402,10 @@ type Audit struct {
 	ProjectID uint64 `json:"projectId"`
 	// +optional 应用id
 	AppID uint64 `json:"appId"`
+	// +optional 制品类型(application or project)
+	ReleaseType string `json:"releaseType,omitempty"`
+	// +optional 制品id
+	ReleaseID string `json:"releaseId,omitempty"`
 	// +optional 事件上下文，前端用来渲染的键值对，如appName，projectName
 	Context map[string]interface{} `json:"context"`
 	// +required 前端模版名，告诉前端应该用哪个模版来渲染
