694216d3d207e97f87f18015b9f34ad8e68ff8ab
bugaolengdeyuxiaoer <46627662+bugaolengdeyuxiaoer@users.noreply.github.com>
2022-01-04 16:33:13 +0800

    fix(admin cp): fix msp url (#3602)

--- modules/admin/component-protocol/components/personal-workbench/workCards/provider_test.go
+++ modules/admin/component-protocol/components/personal-workbench/workCards/provider_test.go
@@ -173,7 +173,7 @@ func TestWorkCards_getProjTextMeta(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			wc := &WorkCards{}
-			wc.getProjTextMeta(tt.args.sdk, tt.args.project, workbench.IssueUrlQueries{})
+			wc.getProjTextMeta(tt.args.sdk, tt.args.project, workbench.IssueUrlQueries{}, workbench.UrlParams{})
 		})
 	}
 }
