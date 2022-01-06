35f03aa88d302895d5e683fd72250895af3af4b0
sfwn <sfwnlin@gmail.com>
2022-01-06 17:17:26 +0800

    cp: msp remove: useless comp register; embeded base.DefaultProvider

--- modules/msp/apm/service/components/transaction-http-detail/scenario.go
+++ modules/msp/apm/service/components/transaction-http-detail/scenario.go
@@ -17,9 +17,7 @@ package transaction_http_detail
 import (
 	_ "github.com/erda-project/erda/modules/msp/apm/service/components/transaction-http-detail/avg_duration"
 	_ "github.com/erda-project/erda/modules/msp/apm/service/components/transaction-http-detail/error_count"
-	_ "github.com/erda-project/erda/modules/msp/apm/service/components/transaction-http-detail/grid"
 	_ "github.com/erda-project/erda/modules/msp/apm/service/components/transaction-http-detail/kv_grid"
-	_ "github.com/erda-project/erda/modules/msp/apm/service/components/transaction-http-detail/page"
 	_ "github.com/erda-project/erda/modules/msp/apm/service/components/transaction-http-detail/rps"
 	_ "github.com/erda-project/erda/modules/msp/apm/service/components/transaction-http-detail/slow_count"
 )
