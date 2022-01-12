d5ccf71bf9a99e0d2876f4eda5aadd9345ca868f
Asher Liu <root@viper.run>
2022-01-12 17:24:37 +0800

    fix: scheduler detail error at service and image secret (#3693)

--- modules/scheduler/impl/servicegroup/create.go
+++ modules/scheduler/impl/servicegroup/create.go
@@ -18,6 +18,9 @@ import (
 	"context"
 	"time"
 
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
 	"github.com/erda-project/erda/apistructs"
 	"github.com/erda-project/erda/modules/scheduler/impl/clusterinfo"
 	"github.com/erda-project/erda/modules/scheduler/task"
@@ -26,15 +29,17 @@ import (
 func (s ServiceGroupImpl) Create(req apistructs.ServiceGroupCreateV2Request) (apistructs.ServiceGroup, error) {
 	sg, err := convertServiceGroupCreateV2Request(req, s.clusterinfo)
 	if err != nil {
-		return apistructs.ServiceGroup{}, err
+		return apistructs.ServiceGroup{}, errors.Errorf("failed to convert sg createV2Request, err: %v", err)
 	}
 
 	if err := s.js.Put(context.Background(), mkServiceGroupKey(sg.Type, sg.ID), sg); err != nil {
+		logrus.Errorf("failed to put sg to jsonStore, err: %v", err)
 		return apistructs.ServiceGroup{}, err
 	}
 
 	sg.Labels = appendServiceTags(sg.Labels, sg.Executor)
 	if _, err := s.handleServiceGroup(context.Background(), &sg, task.TaskCreate); err != nil {
+		logrus.Errorf("failed to handle sg, err: %v", err)
 		return apistructs.ServiceGroup{}, err
 	}
 
