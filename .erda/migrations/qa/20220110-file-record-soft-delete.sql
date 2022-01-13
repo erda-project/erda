93a81432437046cf314458071a22476f7188e38f
chengjoey <30427474+chengjoey@users.noreply.github.com>
2022-01-13 10:11:13 +0800

    fix file record migration (#3705)

--- .erda/migrations/qa/20220110-file-record-soft-delete.sql
+++ .erda/migrations/qa/20220110-file-record-soft-delete.sql
@@ -1,4 +1,3 @@
 ALTER TABLE `dice_test_file_records` ADD `soft_deleted_at` bigint(20) NOT NULL DEFAULT 0 COMMENT '删除时间(时间戳)';
 ALTER TABLE `dice_test_file_records` ADD `org_id`          bigint(20) DEFAULT 0 COMMENT '组织ID';
-ALTER TABLE `dice_test_file_records` ADD UNIQUE KEY `uk_soft_deleted_at_org_id_project_id` (`soft_deleted_at`,`org_id`,`project_id`);
 ALTER TABLE `dice_test_file_records` RENAME TO `erda_file_record`;
