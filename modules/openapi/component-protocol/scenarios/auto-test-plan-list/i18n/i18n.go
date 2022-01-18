eea03f760e4b2bf39e137bb2ef88f27c4536a29f
chengjoey <30427474+chengjoey@users.noreply.github.com>
2022-01-18 09:56:03 +0800

    i18n of autotest plan list (#3735)

---
+++ modules/openapi/component-protocol/scenarios/auto-test-plan-list/i18n/i18n.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package i18n
+
+const (
+	I18nKeyPlan                     = "autotest.plan.plan"
+	I18nKeyNewPlan                  = "autotest.plan.new.plan"
+	I18nKeyPlanName                 = "autotest.plan.plan.name"
+	I18nKeyPlanNameRegex            = "autotest.plan.plan.regex"
+	I18nKeyTestSpace                = "autotest.plan.test.space"
+	I18nKeyTaskParameter            = "autotest.plan.task.parameter"
+	I18nKeyParameterConfiguration   = "autotest.plan.parameter.configuration"
+	I18nKeySceneSet                 = "autotest.plan.autotest.scene.set"
+	I18nKeyScene                    = "autotest.plan.autotest.scene"
+	I18nKeyIteration                = "autotest.plan.iteration"
+	I18nKeyPrincipal                = "autotest.plan.principal"
+	I18nKeyArchive                  = "autotest.plan.archive"
+	I18nKeyUnarchive                = "autotest.plan.un.archive"
+	I18nKeyInProgress               = "autotest.plan.in.progress"
+	I18nKeyArchived                 = "autotest.plan.archived"
+	I18nKeyAll                      = "autotest.plan.all"
+	I18nKeyPlanID                   = "autotest.plan.id"
+	I18nKeyTotalInterfaces          = "autotest.plan.total.executed.interfaces"
+	I18nKeyPassRate                 = "autotest.plan.execution.pass.rate"
+	I18nKeyExecutionTime            = "autotest.plan.execution.time"
+	I18nKeyOperations               = "autotest.plan.operations"
+	I18nKeyEdit                     = "autotest.plan.edit"
+	I18nKeyExecute                  = "autotest.plan.execute"
+	I18nKeyConfigurationInformation = "autotest.plan.configuration.information"
+)
