77af671597c9375ba51235273907b9e94cafed9e
kakj <18579115540@163.com>
2022-01-19 17:23:25 +0800

    DOP failed to initialize mobile app (#3811)

--- modules/dop/services/application/application.go
+++ modules/dop/services/application/application.go
@@ -27,7 +27,7 @@ import (
 	cmspb "github.com/erda-project/erda-proto-go/core/pipeline/cms/pb"
 	"github.com/erda-project/erda/apistructs"
 	"github.com/erda-project/erda/bundle"
-	"github.com/erda-project/erda/modules/core-services/conf"
+	"github.com/erda-project/erda/modules/dop/conf"
 	"github.com/erda-project/erda/modules/dop/dao"
 	"github.com/erda-project/erda/modules/dop/services/apierrors"
 	"github.com/erda-project/erda/pkg/common/apis"
