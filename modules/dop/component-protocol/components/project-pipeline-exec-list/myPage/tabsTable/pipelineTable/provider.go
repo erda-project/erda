3d1252fbc7bf7f6f3bb4336861a331c88ba88339
littlejian <17816869670@163.com>
2022-01-21 16:53:39 +0800

    fix: update project pipeline style (#3876)
    * fix: remove the space of costTime and update exec history userIDs
    
    * fix: fix the bug of pipelineTabs
    
    * update table style

--- modules/dop/component-protocol/components/project-pipeline-exec-list/myPage/tabsTable/pipelineTable/provider.go
+++ modules/dop/component-protocol/components/project-pipeline-exec-list/myPage/tabsTable/pipelineTable/provider.go
@@ -156,7 +156,7 @@ func (p *provider) RegisterInitializeOp() (opFunc cptype.OperationFunc) {
 			if pipeline.DefinitionPageInfo == nil {
 				continue
 			}
-			userIDs = append(userIDs, pipeline.DefinitionPageInfo.Creator)
+			userIDs = append(userIDs, pipeline.GetUserID())
 			tableValue.Rows = append(tableValue.Rows, p.pipelineToRow(pipeline))
 		}
 
@@ -228,13 +228,15 @@ func (p *provider) pipelineToRow(pipeline apistructs.PagePipeline) table.Row {
 					return commodel.DefaultStatus
 				}(),
 			}).Build(),
-			ColumnCostTimeOrder: table.NewTextCell(func() string {
-				if pipeline.CostTimeSec <= 0 {
-					return "-"
-				} else {
-					return fmt.Sprintf("%v s", pipeline.CostTimeSec)
-				}
-			}()).Build(),
+			ColumnCostTimeOrder: table.NewDurationCell(commodel.Duration{
+				Value: func() int64 {
+					if !pipeline.Status.IsRunningStatus() &&
+						!pipeline.Status.IsEndStatus() {
+						return -1
+					}
+					return pipeline.CostTimeSec
+				}(),
+			}).Build(),
 			ColumnApplicationName: table.NewTextCell(getApplicationNameFromDefinitionRemote(pipeline.DefinitionPageInfo.SourceRemote)).Build(),
 			ColumnBranch:          table.NewTextCell(pipeline.DefinitionPageInfo.SourceRef).Build(),
 			ColumnExecutor:        table.NewUserCell(commodel.User{ID: pipeline.GetUserID()}).Build(),
