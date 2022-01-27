4ea62f2cae3905f6a7accad4361b8bb1f9ed7936
CMC <49681321+CraigMChen@users.noreply.github.com>
2022-01-27 14:02:02 +0800

    add console button (#3979)

--- modules/cmp/component-protocol/components/cmp-dashboard-pods/scenario.go
+++ modules/cmp/component-protocol/components/cmp-dashboard-pods/scenario.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/erda-project/erda/modules/cmp/component-protocol/components/cmp-dashboard-pods/addPodFilter"
 	_ "github.com/erda-project/erda/modules/cmp/component-protocol/components/cmp-dashboard-pods/chartContainer"
 	_ "github.com/erda-project/erda/modules/cmp/component-protocol/components/cmp-dashboard-pods/charts"
+	_ "github.com/erda-project/erda/modules/cmp/component-protocol/components/cmp-dashboard-pods/consoleButton"
 	_ "github.com/erda-project/erda/modules/cmp/component-protocol/components/cmp-dashboard-pods/filter"
 	_ "github.com/erda-project/erda/modules/cmp/component-protocol/components/cmp-dashboard-pods/page"
 	_ "github.com/erda-project/erda/modules/cmp/component-protocol/components/cmp-dashboard-pods/podsCharts"
