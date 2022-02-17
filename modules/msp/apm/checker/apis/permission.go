cec434c23ed76e11b5a7b76ce3f2f299bc14f86e
innerpeacez <innerpeace.zhai@gmail.com>
2022-02-17 17:01:27 +0800

    fix: fix msp project permissions (#4045)
    * fix msp project permissions
    
    * go imports format
    
    * add UT

--- modules/msp/apm/checker/apis/permission.go
+++ modules/msp/apm/checker/apis/permission.go
@@ -19,32 +19,26 @@ import (
 	"fmt"
 	"strconv"
 
+	projectpb "github.com/erda-project/erda-proto-go/msp/tenant/project/pb"
 	"github.com/erda-project/erda/modules/msp/apm/checker/storage/db"
 	"github.com/erda-project/erda/pkg/common/errors"
 	perm "github.com/erda-project/erda/pkg/common/permission"
 )
 
-func (s *checkerV1Service) getProjectFromMetricID() func(ctx context.Context, req interface{}) (string, error) {
-	return getProjectFromMetricID(s.metricDB, s.projectDB)
+func (s *checkerV1Service) GetProjectFromMetricID() func(ctx context.Context, req interface{}) (string, error) {
+	return GetProjectFromMetricID(s.metricDB, s.projectDB, s.projectServer)
 }
 
-func getProjectFromMetricID(metricDB *db.MetricDB, projectDB *db.ProjectDB) func(ctx context.Context, req interface{}) (string, error) {
+func GetProjectFromMetricID(metricDB *db.MetricDB, projectDB *db.ProjectDB, projectService projectpb.ProjectServiceServer) func(ctx context.Context, req interface{}) (string, error) {
 	getter := perm.FieldValue("Id")
 	return func(ctx context.Context, req interface{}) (string, error) {
 		mid, err := getter(ctx, req)
 		if err != nil {
 			return "", err
 		}
-		metricID, err := strconv.ParseInt(mid, 10, 64)
+		m, err := GetMetric(mid, metricDB)
 		if err != nil {
-			return "", fmt.Errorf("invalid metricID")
-		}
-		m, err := metricDB.GetByID(metricID)
-		if err != nil {
-			return "", errors.NewDatabaseError(err)
-		}
-		if m == nil {
-			return "", fmt.Errorf("not found id for permission")
+			return "", err
 		}
 		if m.Extra != "" {
 			extra, err := strconv.ParseInt(m.Extra, 10, 64)
@@ -66,8 +60,31 @@ func getProjectFromMetricID(metricDB *db.MetricDB, projectDB *db.ProjectDB) func
 			return "", errors.NewDatabaseError(err)
 		}
 		if proj == nil {
-			return "", fmt.Errorf("not found id for permission")
+			project, err := projectService.GetProject(ctx, &projectpb.GetProjectRequest{ProjectID: strconv.FormatInt(m.ProjectID, 10)})
+			if err != nil {
+				return "", errors.NewDatabaseError(err)
+			}
+			if project == nil {
+				return "", fmt.Errorf("not found id for permission")
+			}
+			return project.Data.Id, nil
 		}
+
 		return strconv.FormatInt(proj.ProjectID, 10), nil
 	}
+}
+
+func GetMetric(mid string, metricDB *db.MetricDB) (*db.Metric, error) {
+	metricID, err := strconv.ParseInt(mid, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid metricID")
+	}
+	m, err := metricDB.GetByID(metricID)
+	if err != nil {
+		return nil, errors.NewDatabaseError(err)
+	}
+	if m == nil {
+		return nil, fmt.Errorf("not found id for permission")
+	}
+	return m, nil
 }
