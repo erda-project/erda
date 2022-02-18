872b0838687acb9e001d06b021b0bd693597a369
pjy <1736989646@qq.com>
2022-02-18 17:59:17 +0800

    modify notify send time

--- modules/core/monitor/alert/alert-apis/adapt/alert.go
+++ modules/core/monitor/alert/alert-apis/adapt/alert.go
@@ -142,7 +142,7 @@ const (
 	WebhookNotifyTarget            NotifyTargetType = "webhook"
 
 	dashboardPath = "/dataCenter/customDashboard"
-	recordPath    = "/cmp/alarm/record/events"
+	recordPath    = "/cmp/alarm/record/events/{{family_id}}"
 
 	TriggerCondition = "trigger_condition"
 )
