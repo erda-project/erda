9652402ff97a5d65c58b36f075719df15a97985b
pjy <1736989646@qq.com>
2022-03-01 17:41:38 +0800

    expression.pb

--- api/proto-go/core/monitor/expression/pb/expression.form.pb.go
+++ api/proto-go/core/monitor/expression/pb/expression.form.pb.go
@@ -266,7 +266,11 @@ func (m *AlertNotify) UnmarshalURLValues(prefix string, values url.Values) error
 				if m.NotifyTarget == nil {
 					m.NotifyTarget = &NotifyTarget{}
 				}
-				m.NotifyTarget.GroupId = vals[0]
+				val, err := strconv.ParseInt(vals[0], 10, 64)
+				if err != nil {
+					return err
+				}
+				m.NotifyTarget.GroupId = val
 			case "notifyTarget.groupType":
 				if m.NotifyTarget == nil {
 					m.NotifyTarget = &NotifyTarget{}
@@ -299,7 +303,11 @@ func (m *NotifyTarget) UnmarshalURLValues(prefix string, values url.Values) erro
 			case "type":
 				m.Type = vals[0]
 			case "groupId":
-				m.GroupId = vals[0]
+				val, err := strconv.ParseInt(vals[0], 10, 64)
+				if err != nil {
+					return err
+				}
+				m.GroupId = val
 			case "groupType":
 				m.GroupType = vals[0]
 			case "level":
