7a5997f0554e17df805b62efc4786e9df6310f0b
littlejian <17816869670@163.com>
2022-03-01 13:33:34 +0800

    feat(dop,gittar): update preReceiveHook and add create pipeline link to remote (#4173)
    * feat: update push event and add create pipeline link to remote
    
    * add unit test
    
    * polish the code

--- modules/gittar/helper/git_cmd.go
+++ modules/gittar/helper/git_cmd.go
@@ -183,10 +183,11 @@ func RunProcess(service string, c *webcontext.Context) {
 		}
 		logrus.Infof("push header:" + string(header))
 		re := regexp.MustCompile(
-			`(?mi)(?P<before>[0-9a-fA-F]{40}) (?P<after>[0-9a-fA-F]{40}) (?P<ref>refs\/(heads|tags)\/.*?)\0`,
+			`(?mi)(?P<before>[0-9a-fA-F]{40}) (?P<after>[0-9a-fA-F]{40}) (?P<ref>refs\/(heads|tags)\/\S*)`,
 		)
 
-		for _, matches := range re.FindAllSubmatch(header, -1) {
+		matchHeader := removeEndMarkerFromHeader(header)
+		for _, matches := range re.FindAllSubmatch(matchHeader, -1) {
 			pushEvent := &models.PayloadPushEvent{
 				Before:            string(matches[1]),
 				After:             string(matches[2]),
@@ -201,7 +202,16 @@ func RunProcess(service string, c *webcontext.Context) {
 
 		repository := c.MustGet("repository").(*gitmodule.Repository)
 		if preReceiveHook(pushEvents, c) {
-
+			// Only when one branch is created will it be written to the writer
+			// Refer to github
+			if len(pushEvents) == 1 && pushEvents[0].IsCreateNewBranch() {
+				// TODO write create pipeline link to writer
+				//c.GetWriter().Write(NewReportStatus(
+				//	"unpack ok",
+				//	"ok "+pushEvents[0].Ref,
+				//	fmt.Sprintf("\nCreate a pipeline request for '%s' on Erda by visiting:\n"+
+				//		"https://erda.cloud\n", pushEvents[0].Ref[len("refs/heads/"):])))
+			}
 			runCommand2(c.GetWriter(), gitCommand(
 				version,
 				service,
@@ -218,6 +228,16 @@ func RunProcess(service string, c *webcontext.Context) {
 			c.MustGet("repository").(*gitmodule.Repository).DiskPath(),
 		), reqBody)
 	}
+}
+
+// removeEndMarkerFromHeader remove end marker '0000' from header
+// see https://github.com/git/git/blob/master/Documentation/technical/http-protocol.txt line:342
+func removeEndMarkerFromHeader(header []byte) []byte {
+	matchHeader := make([]byte, 0, len(header)-4)
+	for i := 0; i < len(header)-4; i++ {
+		matchHeader = append(matchHeader, header[i])
+	}
+	return matchHeader
 }
 
 func RunArchive(c *webcontext.Context, ref string, format string) {
