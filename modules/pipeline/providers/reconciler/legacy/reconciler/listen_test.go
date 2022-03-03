d9427e31230498b0e50c1ebf0143d774566b50b5
sfwn <sfwnlin@gmail.com>
2022-03-03 10:05:31 +0800

    chore: fix unit-test of legacy reconciler

--- modules/pipeline/providers/reconciler/legacy/reconciler/listen_test.go
+++ modules/pipeline/providers/reconciler/legacy/reconciler/listen_test.go
@@ -16,18 +16,11 @@ package reconciler
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"reflect"
-	"sync"
 	"testing"
 
-	"bou.ke/monkey"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/erda-project/erda-proto-go/pipeline/pb"
 	"github.com/erda-project/erda/apistructs"
-	"github.com/erda-project/erda/modules/pipeline/providers/queuemanager/types"
 	"github.com/erda-project/erda/modules/pipeline/spec"
 )
 
@@ -57,138 +50,4 @@ func TestMakeContextForPipelineReconcile(t *testing.T) {
 func TestMakePipelineWatchedKey(t *testing.T) {
 	key := makePipelineWatchedKey(1)
 	assert.Equal(t, "/devops/pipeline/reconciler/1", key)
-}
-
-func TestListen(t *testing.T) {
-	r := &Reconciler{}
-	pm1 := monkey.PatchInstanceMethod(reflect.TypeOf(r), "Listen", func(r *Reconciler, ctx context.Context) {
-		return
-	})
-	defer pm1.Unpatch()
-	t.Run("listen", func(t *testing.T) {
-		r.Listen(context.Background())
-	})
-}
-
-func TestReconciler_doWatch(t *testing.T) {
-	type fields struct {
-		PutPipelineIntoQueue struct {
-			err            error
-			needRetryIfErr bool
-			popCh          <-chan struct{}
-		}
-	}
-	type args struct {
-		ctx context.Context
-		key string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		{
-			name: "test_processingPipelines",
-			fields: fields{
-				PutPipelineIntoQueue: struct {
-					err            error
-					needRetryIfErr bool
-					popCh          <-chan struct{}
-				}{err: fmt.Errorf("error"), needRetryIfErr: true, popCh: nil},
-			},
-			args: args{
-				key: etcdReconcilerWatchPrefix + "1",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := &Reconciler{
-				processingPipelines: sync.Map{},
-			}
-
-			ctx := context.Background()
-			queueManager := mockQueueManager{
-				putPipelineIntoQueue: PutPipelineIntoQueue{
-					err:            tt.fields.PutPipelineIntoQueue.err,
-					needRetryIfErr: tt.fields.PutPipelineIntoQueue.needRetryIfErr,
-					popCh:          tt.fields.PutPipelineIntoQueue.popCh,
-				},
-			}
-			r.QueueManager = queueManager
-
-			patch1 := monkey.PatchInstanceMethod(reflect.TypeOf(r), "Add", func(r *Reconciler, pipelineID uint64) {
-				return
-			})
-			defer patch1.Unpatch()
-
-			r.Reconcile(ctx, tt.args.key)
-		})
-	}
-}
-
-type PutPipelineIntoQueue struct {
-	err            error
-	needRetryIfErr bool
-	popCh          <-chan struct{}
-}
-
-type mockQueueManager struct {
-	putPipelineIntoQueue PutPipelineIntoQueue
-}
-
-func (m mockQueueManager) IdempotentAddQueue(pq *apistructs.PipelineQueue) types.Queue {
-	panic("implement me")
-}
-
-func (m mockQueueManager) QueryQueueUsage(pq *apistructs.PipelineQueue) *pb.QueueUsage {
-	panic("implement me")
-}
-
-func (m mockQueueManager) PutPipelineIntoQueue(pipelineID uint64) (popCh <-chan struct{}, needRetryIfErr bool, err error) {
-	return m.putPipelineIntoQueue.popCh, m.putPipelineIntoQueue.needRetryIfErr, m.putPipelineIntoQueue.err
-}
-
-func (m mockQueueManager) PopOutPipelineFromQueue(pipelineID uint64) {
-	panic("implement me")
-}
-
-func (m mockQueueManager) BatchUpdatePipelinePriorityInQueue(pq *apistructs.PipelineQueue, pipelineIDs []uint64) error {
-	panic("implement me")
-}
-
-func (m mockQueueManager) Stop() {
-	panic("implement me")
-}
-
-func (m mockQueueManager) SendQueueToEtcd(queueID uint64) {
-	panic("implement me")
-}
-
-func (m mockQueueManager) ListenInputQueueFromEtcd(ctx context.Context) {
-	panic("implement me")
-}
-
-func (m mockQueueManager) SendUpdatePriorityPipelineIDsToEtcd(queueID uint64, pipelineIDS []uint64) {
-	panic("implement me")
-}
-
-func (m mockQueueManager) ListenUpdatePriorityPipelineIDsFromEtcd(ctx context.Context) {
-	panic("implement me")
-}
-
-func (m mockQueueManager) SendPopOutPipelineIDToEtcd(pipelineID uint64) {
-	panic("implement me")
-}
-
-func (m mockQueueManager) ListenPopOutPipelineIDFromEtcd(ctx context.Context) {
-	panic("implement me")
-}
-
-func (m mockQueueManager) Export() json.RawMessage {
-	panic("implement me")
-}
-
-func (m mockQueueManager) Import(message json.RawMessage) error {
-	panic("implement me")
 }
