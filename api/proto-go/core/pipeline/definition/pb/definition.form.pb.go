8b945de058f70b9bd6b42bbed4856f745b48716b
wangxiaojian <17816869670@163.com>
2022-03-23 12:08:15 +0800

    fix: Pipeline category statistics only include applications that have participated

--- api/proto-go/core/pipeline/definition/pb/definition.form.pb.go
+++ api/proto-go/core/pipeline/definition/pb/definition.form.pb.go
@@ -1728,6 +1728,8 @@ func (m *PipelineDefinitionStatisticsRequest) UnmarshalURLValues(prefix string, 
 			switch prefix + key {
 			case "location":
 				m.Location = vals[0]
+			case "remote":
+				m.Remote = vals
 			}
 		}
 	}
