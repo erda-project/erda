54fd795c79504762bd20ab8b847f4d8155b331e6
shuofan <fanshuo2015@gmail.com>
2022-04-07 14:09:41 +0800

    feat: Update requirement progress format in issue table (#4562)
    * Update requirement progress format
    
    * Code refactor

--- modules/dop/component-protocol/components/issue-manage/issueTable/render.go
+++ modules/dop/component-protocol/components/issue-manage/issueTable/render.go
@@ -40,10 +40,12 @@ import (
 	"github.com/erda-project/erda/modules/openapi/hooks/posthandle"
 )
 
-type Progress struct {
+type ProgressBlock struct {
 	Value      string `json:"value"`
 	RenderType string `json:"renderType"`
+	HiddenText bool   `json:"hiddenText"`
 }
+
 type Severity struct {
 	Value       string                 `json:"value"`
 	RenderType  string                 `json:"renderType"`
@@ -53,11 +55,12 @@ type Severity struct {
 	DisabledTip string                 `json:"disabledTip"`
 }
 
-type Title TableColumnMultiple
+type Progress TableColumnMultiple
 
 type TableColumnMultiple struct {
-	RenderType string          `json:"renderType,omitempty"`
-	Renders    [][]interface{} `json:"renders,omitempty"`
+	RenderType string        `json:"renderType,omitempty"`
+	Direction  string        `json:"direction,omitempty"`
+	Renders    []interface{} `json:"renders,omitempty"`
 }
 
 type TableColumnTextWithIcon struct {
@@ -561,13 +564,33 @@ func (ca *ComponentAction) buildTableItem(ctx context.Context, data *apistructs.
 	}
 	nameColumn := ca.getNameColumn(data)
 	progress := Progress{
-		RenderType: "text",
+		RenderType: "multiple",
+		Direction:  "row",
 	}
 	if data.Type == apistructs.IssueTypeRequirement {
 		if data.IssueSummary == nil {
 			data.IssueSummary = &apistructs.IssueSummary{}
 		}
-		progress.Value = fmt.Sprintf("%d/%d", data.IssueSummary.DoneCount, data.IssueSummary.DoneCount+data.IssueSummary.ProcessingCount)
+		s := data.IssueSummary.DoneCount + data.IssueSummary.ProcessingCount
+		progressPercentage := ProgressBlock{
+			RenderType: "progress",
+			Value:      "0",
+			HiddenText: true,
+		}
+		if s != 0 {
+			progressPercentage.Value = fmt.Sprintf("%d", int(100*(float64(data.IssueSummary.DoneCount)/float64(s))))
+		}
+		progress.Renders = []interface{}{
+			[]interface{}{
+				progressPercentage,
+			},
+			[]interface{}{
+				ProgressBlock{
+					RenderType: "text",
+					Value:      fmt.Sprintf("%d/%d", data.IssueSummary.DoneCount, s),
+				},
+			},
+		}
 	}
 
 	severityOps := map[string]interface{}{}
