b715d953b66597aad0ae25dd812710cf1ce0b78a
sixther-dc <382998946@qq.com>
2022-05-26 19:18:30 +0800

    fix: improve sql when query instance list

--- modules/orchestrator/endpoints/instance.go
+++ modules/orchestrator/endpoints/instance.go
@@ -75,16 +75,11 @@ func (e *Endpoints) ListServiceInstance(ctx context.Context, r *http.Request, va
 	ip := r.URL.Query().Get("ip")
 
 	req := apistructs.InstanceInfoRequest{
-		OrgID:       strconv.FormatUint(orgID, 10),
 		RuntimeID:   runtimeID,
 		ServiceName: serviceName,
 	}
 	if ip != "" {
 		req.InstanceIP = ip
-	}
-	rID, _ := strconv.Atoi(runtimeID)
-	if runtime, err := e.db.GetRuntime(uint64(rID)); err == nil {
-		req.Cluster = runtime.ClusterName
 	}
 	switch status {
 	case "", "running":
