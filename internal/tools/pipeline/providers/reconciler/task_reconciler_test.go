c74df03a57bb11d7b1135dae90e84d20fe083861
sfwn <sfwnlin@gmail.com>
2022-06-14 14:15:14 +0800

    fix unit-test of pipeline

--- internal/tools/pipeline/providers/reconciler/task_reconciler_test.go
+++ internal/tools/pipeline/providers/reconciler/task_reconciler_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/erda-project/erda/apistructs"
 	"github.com/erda-project/erda/internal/tools/pipeline/dbclient"
 	"github.com/erda-project/erda/internal/tools/pipeline/spec"
+	"github.com/erda-project/erda/pkg/metadata"
 )
 
 func Test_overwriteTaskWithLatest(t *testing.T) {
