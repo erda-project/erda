56b15e372e3bf820a1d6deaf23b23646c1506227
sfwn <sfwnlin@gmail.com>
2022-07-11 19:40:28 +0800

    erda-base Dockerfile support multi-arch

--- build/dockerfiles/base/deploy.sh
+++ build/dockerfiles/base/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 image=registry.erda.cloud/erda/erda-base:$(date +"%Y%m%d")
 
-docker buildx build --platform linux/amd64 -t ${image} --push . -f Dockerfile
+docker buildx build --platform linux/amd64,linux/arm64 -t ${image} --push . -f Dockerfile
 
 echo "action meta: erda-base=$image"
