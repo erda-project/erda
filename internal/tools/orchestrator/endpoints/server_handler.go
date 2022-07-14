01f7e4279016e0322d234843cf7564fa344c13f1
wangzhuzhen <zhuzhenwang1984@gmail.com>
2022-07-14 17:50:00 +0800

    fix: manual scale not update addon attachment

--- internal/tools/orchestrator/endpoints/server_handler.go
+++ internal/tools/orchestrator/endpoints/server_handler.go
@@ -108,6 +108,7 @@ func (s *Endpoints) processRuntimeScaleRecord(rsc apistructs.RuntimeScaleRecord,
 		Workspace:     rsc.Workspace,
 		Name:          rsc.Name,
 	}
+	serviceManualScale := false
 	logrus.Errorf("process runtime scale for runtime %#v", uniqueId)
 
 	appliedScaledObjects, err := s.runtime.AppliedScaledObjects(uniqueId)
@@ -222,6 +223,7 @@ func (s *Endpoints) processRuntimeScaleRecord(rsc apistructs.RuntimeScaleRecord,
 
 		// 未设置 action，则需要自行判断用于执行 scale 操作之后的 runtime 状态更新
 		if action == "" {
+			serviceManualScale = true
 			// 如果所有的 serice 的副本数都调整为0，则是停止操作，Runtime 状态应该为 Stopped，否则不是停止操作，Runtime 状态应该为 Healthy
 			action = apistructs.ScaleActionDown
 			for _, svc := range sg.Services {
@@ -241,7 +243,7 @@ func (s *Endpoints) processRuntimeScaleRecord(rsc apistructs.RuntimeScaleRecord,
 			return apistructs.PreDiceDTO{}, err, errMsg
 		}
 
-		if action == apistructs.ScaleActionDown || action == apistructs.ScaleActionUp {
+		if (action == apistructs.ScaleActionDown || action == apistructs.ScaleActionUp) && !serviceManualScale {
 			addons, err := s.db.GetUnDeletableAttachMentsByRuntimeID(runtime.ID)
 			if err != nil {
 				logrus.Warnf("process runtime scale successed, but update runtime referenced addon attact_count for runtime %#v failed, err: %v", uniqueId, err)
@@ -290,7 +292,6 @@ func (s *Endpoints) processRuntimeScaleRecord(rsc apistructs.RuntimeScaleRecord,
 				}
 			}
 		}
-		s.db.UpdateRuntime(runtime)
 	}
 
 	// 保留非停止状态下的副本数
