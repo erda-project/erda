26a69897ae62c9f07084d1263dfca603eca62f53
flamingo <lion.zhang@mail.com>
2022-07-26 17:16:04 +0800

    fix: too many conditions to save (#5279)
    * fix: too many conditions to save
    
    * empty

---
+++ .erda/migrations/monitor/20220726-sp_alert.sql
@@ -0,0 +1,4 @@
+ALTER TABLE sp_alert MODIFY attributes text COMMENT '';
+ALTER TABLE sp_alert_rules MODIFY attributes text COMMENT '';
+ALTER TABLE sp_alert_expression MODIFY attributes text COMMENT '';
+ALTER TABLE sp_alert_expression MODIFY expression text COMMENT '';
