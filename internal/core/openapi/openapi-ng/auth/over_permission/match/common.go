58cb882c9014b46dc40139910ecbd7ca0cc69715
tomatopunk <lion.zhang@mail.com>
2022-12-13 14:31:19 +0800

    rename

---
+++ internal/core/openapi/openapi-ng/auth/over_permission/match/common.go
@@ -0,0 +1,7 @@
+package match
+
+const (
+	// ProtoComponent is common.OpenAPIOption Key
+	// https://github.com/erda-project/erda/blob/3fd7387891cee46323066f64de0a6f6224966b4e/internal/core/openapi/openapi-ng/routes/proto/proto.go#L159
+	ProtoComponent = "CheckOverPermission"
+)
