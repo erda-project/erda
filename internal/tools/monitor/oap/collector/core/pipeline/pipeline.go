feb627140c3962785075fb27676dd671b551a275
flamingo <lion.zhang@mail.com>
2023-04-21 11:30:57 +0800

    fix

--- internal/tools/monitor/oap/collector/core/pipeline/pipeline.go
+++ internal/tools/monitor/oap/collector/core/pipeline/pipeline.go
@@ -276,10 +276,11 @@ func (p *Pipeline) startReceivers(out chan<- odata2.ObservableData) {
 
 func (p *Pipeline) newConsumer(pctx context.Context, name string, out chan<- odata2.ObservableData) model.ObservableDataConsumerFunc {
 	return func(od odata2.ObservableData) error {
-		dataReceived.WithLabelValues(p.name, string(p.dtype), name, od.GetTags()["org_name"]).Inc()
+		org := od.GetTags()["org_name"]
 
 		select {
 		case out <- od:
+			dataReceived.WithLabelValues(p.name, string(p.dtype), name, org).Inc()
 		case <-pctx.Done():
 			return nil
 		}
