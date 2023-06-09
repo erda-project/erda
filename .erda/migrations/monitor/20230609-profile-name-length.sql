2c4e99faf585159086a5f83e4fc37c34c62aef4a
chengjoey <30427474+chengjoey@users.noreply.github.com>
2023-06-09 17:18:23 +0800

    extend profile name field length (#5915)

---
+++ .erda/migrations/monitor/20230609-profile-name-length.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `erda_profile_app` MODIFY `name` VARCHAR(256) DEFAULT NULL COMMENT '指标名称';
+ALTER TABLE `erda_profile_app` MODIFY `project_name` VARCHAR(256) DEFAULT NULL COMMENT '项目名称';
+ALTER TABLE `erda_profile_app` MODIFY `app_name` VARCHAR(256) DEFAULT NULL COMMENT '应用名称';
+ALTER TABLE `erda_profile_app` MODIFY `service_name` VARCHAR(256) DEFAULT NULL COMMENT '服务名称';
