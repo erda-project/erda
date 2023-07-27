f9703bb19b509d4c0ac1a664eec5c344bd0e3bf6
iutx <root@viper.run>
2023-07-27 14:45:18 +0800

    add dnspolicy unit test
    Signed-off-by: iutx <root@viper.run>

---
+++ internal/tools/pipeline/pipengine/actionexecutor/logic/job_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/erda-project/erda/apistructs"
+)
+
+func TestGetDNSPolicy(t *testing.T) {
+	testCases := []struct {
+		input    apistructs.PodNetwork
+		expected corev1.DNSPolicy
+	}{
+		{
+			input: map[string]string{
+				"mode": "host",
+			},
+			expected: corev1.DNSClusterFirstWithHostNet,
+		},
+		{
+			input:    nil,
+			expected: "",
+		},
+	}
+
+	// Run the test cases
+	for _, tc := range testCases {
+		t.Run("Test GetDNSPolicy", func(t *testing.T) {
+			result := GetDNSPolicy(tc.input)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
