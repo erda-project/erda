99a5fd7f6d4ffcb889d023f63537849cf069d980
Wang JingYu <89286835+jing-zhi@users.noreply.github.com>
2023-10-11 00:21:30 +0800

    fix: repair sql injection again

--- modules/orchestrator/scheduler/instanceinfo/instance.go
+++ modules/orchestrator/scheduler/instanceinfo/instance.go
@@ -15,16 +15,15 @@
 package instanceinfo
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/erda-project/erda/pkg/database/dbengine"
-	"github.com/erda-project/erda/pkg/strutil"
 )
 
 type InstanceReader struct {
 	db         *dbengine.DBEngine
 	conditions []string
+	values     []interface{}
 	limit      int
 }
 
@@ -37,121 +36,145 @@ func (c *Client) InstanceReader() *InstanceReader {
 }
 
 func (r *InstanceReader) ByCluster(clustername string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("cluster = \"%s\"", clustername))
+	r.conditions = append(r.conditions, "cluster = ?")
+	r.values = append(r.values, clustername)
 	return r
 }
 func (r *InstanceReader) ByNamespace(ns string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("namespace = \"%s\"", ns))
+	r.conditions = append(r.conditions, "namespace = ?")
+	r.values = append(r.values, ns)
 	return r
 }
 func (r *InstanceReader) ByName(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("name = \"%s\"", name))
+	r.conditions = append(r.conditions, "name = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByOrgName(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("org_name = \"%s\"", name))
+	r.conditions = append(r.conditions, "org_name = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByOrgID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("org_id = \"%s\"", id))
+	r.conditions = append(r.conditions, "org_id = ?")
+	r.values = append(r.values, id)
 	return r
 }
 func (r *InstanceReader) ByProjectName(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("project_name = \"%s\"", name))
+	r.conditions = append(r.conditions, "project_name = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByProjectID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("project_id = \"%s\"", id))
+	r.conditions = append(r.conditions, "project_id = ?")
+	r.values = append(r.values, id)
 	return r
 }
 func (r *InstanceReader) ByApplicationName(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("application_name = \"%s\"", name))
+	r.conditions = append(r.conditions, "application_name = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByEdgeApplicationName(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("edge_application_name = \"%s\"", name))
+	r.conditions = append(r.conditions, "edge_application_name = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByEdgeSite(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("edge_site = \"%s\"", name))
+	r.conditions = append(r.conditions, "edge_site = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByApplicationID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("application_id = \"%s\"", id))
+	r.conditions = append(r.conditions, "application_id = ?")
+	r.values = append(r.values, id)
 	return r
 }
 func (r *InstanceReader) ByRuntimeName(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("runtime_name = \"%s\"", name))
+	r.conditions = append(r.conditions, "runtime_name = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByRuntimeID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("runtime_id = \"%s\"", id))
+	r.conditions = append(r.conditions, "runtime_id = ?")
+	r.values = append(r.values, id)
 	return r
 }
 func (r *InstanceReader) ByService(name string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("service_name = \"%s\"", name))
+	r.conditions = append(r.conditions, "service_name = ?")
+	r.values = append(r.values, name)
 	return r
 }
 func (r *InstanceReader) ByWorkspace(ws string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("workspace = \"%s\"", ws))
+	r.conditions = append(r.conditions, "workspace = ?")
+	r.values = append(r.values, ws)
 	return r
 }
 func (r *InstanceReader) ByContainerID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("container_id = \"%s\"", id))
+	r.conditions = append(r.conditions, "container_id = ?")
+	r.values = append(r.values, id)
 	return r
 }
 func (r *InstanceReader) ByServiceType(tp string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("service_type = \"%s\"", tp))
+	r.conditions = append(r.conditions, "service_type = ?")
+	r.values = append(r.values, tp)
 	return r
 }
 func (r *InstanceReader) ByPhase(phase string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("phase = \"%s\"", phase))
+	r.conditions = append(r.conditions, "phase = ?")
+	r.values = append(r.values, phase)
 	return r
 }
 func (r *InstanceReader) ByPhases(phases ...string) *InstanceReader {
-	phasesStr := strutil.Map(phases, func(s string) string { return "\"" + s + "\"" })
-	r.conditions = append(r.conditions, fmt.Sprintf("phase in (%s)", strutil.Join(phasesStr, ",")))
+	r.conditions = append(r.conditions, "phase IN (?)")
+	r.values = append(r.values, phases)
 	return r
 }
 func (r *InstanceReader) ByFinishedTime(beforeNday int) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("finished_at < now() - interval %d day", beforeNday))
+	r.conditions = append(r.conditions, "finished_at < now() - interval ？ day")
+	r.values = append(r.values, beforeNday)
 	return r
 }
 func (r *InstanceReader) ByUpdatedTime(beforeNSecs int) *InstanceReader {
 	// Use scheduler time query to avoid the inconsistency between sceduler and database time and cause the instance to GC by mistake
 	now := time.Now().Format("2006-01-02 15:04:05")
-	r.conditions = append(r.conditions, fmt.Sprintf("updated_at < '%s' - interval %d second", now, beforeNSecs))
+	r.conditions = append(r.conditions, "updated_at < "+now+" - interval ? second")
+	r.values = append(r.values, beforeNSecs)
 	return r
 }
 func (r *InstanceReader) ByTaskID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("task_id = \"%s\"", id))
+	r.conditions = append(r.conditions, "task_id = ?")
+	r.values = append(r.values, id)
 	return r
 }
 
 func (r *InstanceReader) ByNotTaskID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("task_id <> \"%s\"", id))
+	r.conditions = append(r.conditions, "task_id <> ?")
+	r.values = append(r.values, id)
 	return r
 }
 
 func (r *InstanceReader) ByAddonID(id string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("addon_id = \"%s\"", id))
+	r.conditions = append(r.conditions, "addon_id = ?")
+	r.values = append(r.values, id)
 	return r
 }
 
 func (r *InstanceReader) ByInstanceIP(ips ...string) *InstanceReader {
-	ipsStr := strutil.Map(ips, func(s string) string { return "\"" + s + "\"" })
-	r.conditions = append(r.conditions, fmt.Sprintf("container_ip in (%s)", strutil.Join(ipsStr, ",")))
+	r.conditions = append(r.conditions, "container_ip IN (?)")
+	r.values = append(r.values, ips)
 	return r
 }
 
 func (r *InstanceReader) ByHostIP(ips ...string) *InstanceReader {
-	ipsStr := strutil.Map(ips, func(s string) string { return "\"" + s + "\"" })
-	r.conditions = append(r.conditions, fmt.Sprintf("host_ip in (%s)", strutil.Join(ipsStr, ",")))
+	r.conditions = append(r.conditions, "host_ip IN (?)")
+	r.values = append(r.values, ips)
 	return r
 }
 
 func (r *InstanceReader) ByMetaLike(s string) *InstanceReader {
-	r.conditions = append(r.conditions, fmt.Sprintf("meta LIKE '%%%s%%'", s))
+	r.conditions = append(r.conditions, "meta LIKE ?")
+	r.values = append(r.values, "%"+s+"%")
 	return r
 }
 
@@ -161,7 +184,10 @@ func (r *InstanceReader) Limit(n int) *InstanceReader {
 }
 func (r *InstanceReader) Do() ([]InstanceInfo, error) {
 	instanceinfo := []InstanceInfo{}
-	expr := r.db.Where(strutil.Join(r.conditions, " AND ", true)).Order("started_at desc")
+	expr := r.db.Order("started_at desc")
+	for k := range r.conditions {
+		expr = expr.Where(r.conditions[k], r.values[k])
+	}
 	if r.limit != 0 {
 		expr = expr.Limit(r.limit)
 	}
