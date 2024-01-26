963007492daa1893d3c5384c44749b01ecb247f2
CeerDecy <1748788674@qq.com>
2024-01-26 16:19:15 +0800

    feat: refactoring daemonset to interact with apiserver from http to client-go

--- internal/tools/orchestrator/scheduler/executor/plugins/k8s/daemonset/daemonset.go
+++ internal/tools/orchestrator/scheduler/executor/plugins/k8s/daemonset/daemonset.go
@@ -15,40 +15,29 @@
 package daemonset
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/url"
 	"strings"
 
 	"github.com/pkg/errors"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 
-	"github.com/erda-project/erda/internal/tools/orchestrator/scheduler/executor/plugins/k8s/k8sapi"
 	"github.com/erda-project/erda/internal/tools/orchestrator/scheduler/executor/plugins/k8s/k8serror"
-	"github.com/erda-project/erda/pkg/http/httpclient"
 )
 
 type Daemonset struct {
-	addr   string
-	client *httpclient.HTTPClient
-	cs     kubernetes.Interface
+	addr string
+	cs   kubernetes.Interface
 }
 
 type Option func(*Daemonset)
-
-// WithCompleteParams provides an Option
-func WithCompleteParams(addr string, client *httpclient.HTTPClient) Option {
-	return func(d *Daemonset) {
-		d.addr = addr
-		d.client = client
-	}
-}
 
 func New(options ...Option) *Daemonset {
 	ds := &Daemonset{}
@@ -66,113 +55,59 @@ func WithClientSet(c kubernetes.Interface) Option {
 }
 
 func (d *Daemonset) Create(ds *appsv1.DaemonSet) error {
-	var b bytes.Buffer
-
-	resp, err := d.client.Post(d.addr).
-		Path("/apis/apps/v1/namespaces/" + ds.Namespace + "/daemonsets").
-		JSONBody(ds).
-		Do().
-		Body(&b)
-	if err != nil {
-		return fmt.Errorf("failed to create ds, %s/%s", ds.Namespace, ds.Name)
-	}
-	if !resp.IsOK() {
-		errMsg := fmt.Sprintf("failed to create ds, statuscode: %v, body: %v", resp.StatusCode(), b.String())
-		return fmt.Errorf(errMsg)
-	}
-	return nil
+	_, err := d.cs.AppsV1().DaemonSets(ds.Namespace).Create(context.Background(), ds, metav1.CreateOptions{})
+	return err
 }
 
 func (d *Daemonset) Get(namespace, name string) (*appsv1.DaemonSet, error) {
-	var b bytes.Buffer
-	resp, err := d.client.Get(d.addr).
-		Path("/apis/apps/v1/namespaces/" + namespace + "/daemonsets/" + name).
-		Do().
-		Body(&b)
+	daemonSet, err := d.cs.AppsV1().DaemonSets(namespace).Get(context.Background(), name, metav1.GetOptions{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to get ds info, %s/%s", namespace, name)
-	}
-	if !resp.IsOK() {
-		if resp.IsNotfound() {
+		if k8serrors.IsNotFound(err) {
 			return nil, k8serror.ErrNotFound
 		}
-		return nil, fmt.Errorf("failed to get ds info, %s/%s, statuscode: %v, body: %v",
-			namespace, name, resp.StatusCode(), b.String())
-	}
-	ds := &appsv1.DaemonSet{}
-	if err := json.NewDecoder(&b).Decode(ds); err != nil {
 		return nil, err
 	}
-	return ds, nil
+	return daemonSet, err
 }
 
-func (d *Daemonset) List(namespace string, labelSelector map[string]string) (appsv1.DaemonSetList, error) {
-	var dsList appsv1.DaemonSetList
-	var params url.Values
+func (d *Daemonset) List(namespace string, labelSelector map[string]string) (*appsv1.DaemonSetList, error) {
+	options := metav1.ListOptions{}
+
 	if len(labelSelector) > 0 {
-		var kvs []string
-		params = make(url.Values, 0)
+		kvs := make([]string, 0, len(labelSelector))
 		for key, value := range labelSelector {
 			kvs = append(kvs, fmt.Sprintf("%s=%s", key, value))
 		}
-		params.Add("labelSelector", strings.Join(kvs, ","))
+
+		selector, err := labels.Parse(strings.Join(kvs, ","))
+		if err != nil {
+			return nil, errors.Errorf("failed to parse label selector, %v", err)
+		}
+
+		options.LabelSelector = selector.String()
 	}
 
-	var b bytes.Buffer
-	resp, err := d.client.Get(d.addr).
-		Path("/apis/apps/v1/namespaces/" + namespace + "/daemonsets").
-		Params(params).
-		Do().
-		Body(&b)
+	daemonSetList, err := d.cs.AppsV1().DaemonSets(namespace).List(context.Background(), options)
 	if err != nil {
-		return dsList, fmt.Errorf("failed to get ds list, ns: %s, %v", namespace, err)
-	}
-	if !resp.IsOK() {
-		if resp.IsNotfound() {
-			return dsList, k8serror.ErrNotFound
+		if k8serrors.IsNotFound(err) {
+			return nil, k8serror.ErrNotFound
 		}
-		return dsList, fmt.Errorf("failed to get ds list, ns: %s, statuscode: %v, body: %v",
-			namespace, resp.StatusCode(), b.String())
+		return nil, err
 	}
-	if err := json.NewDecoder(&b).Decode(&dsList); err != nil {
-		return dsList, err
-	}
-	return dsList, nil
+	return daemonSetList, nil
 }
 
 func (d *Daemonset) Update(daemonset *appsv1.DaemonSet) error {
-	var b bytes.Buffer
-	resp, err := d.client.Put(d.addr).
-		Path("/apis/apps/v1/namespaces/" + daemonset.Namespace + "/daemonsets/" + daemonset.Name).
-		JSONBody(daemonset).
-		Do().
-		Body(&b)
-	if err != nil {
-		return fmt.Errorf("failed to put daemonset, %s/%s, %v", daemonset.Namespace, daemonset.Name, err)
-	}
-	if !resp.IsOK() {
-		return fmt.Errorf("failed to put daemonset, %s/%s, statuscode: %v, body: %v",
-			daemonset.Namespace, daemonset.Name, resp.StatusCode(), b.String())
-	}
-	return nil
+	_, err := d.cs.AppsV1().DaemonSets(daemonset.Namespace).Update(context.Background(), daemonset, metav1.UpdateOptions{})
+	return err
 }
 
 func (d *Daemonset) Delete(namespace, name string) error {
-	var b bytes.Buffer
-	resp, err := d.client.Delete(d.addr).
-		Path("/apis/apps/v1/namespaces/" + namespace + "/daemonsets/" + name).
-		JSONBody(k8sapi.DeleteOptions).
-		Do().
-		Body(&b)
-	if err != nil {
-		return fmt.Errorf("failed to delete daemonsets, %s/%s, %v", namespace, name, err)
-	}
-	if !resp.IsOK() {
-		if resp.IsNotfound() {
+	if err := d.cs.AppsV1().DaemonSets(namespace).Delete(context.Background(), name, metav1.DeleteOptions{}); err != nil {
+		if k8serrors.IsNotFound(err) {
 			return k8serror.ErrNotFound
 		}
-		return fmt.Errorf("failed to delete daemonsets, %s/%s, statuscode: %v, body: %v",
-			namespace, name, resp.StatusCode(), b.String())
+		return err
 	}
 	return nil
 }
