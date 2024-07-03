73ca64666e8f5218b14d82744d5461551cd48854
iutx <root@viper.run>
2024-07-03 17:58:09 +0800

    chore: change all erda migration baseline chracter set to utf8mb4

--- .erda/migrations/.patches/patch-20210528-orchestrator-base.sql
+++ .erda/migrations/.patches/patch-20210528-orchestrator-base.sql
@@ -1,5 +1,5 @@
-ALTER TABLE ps_runtime_instances DEFAULT CHARACTER SET utf8;
+ALTER TABLE ps_runtime_instances CONVERT TO CHARACTER SET utf8mb4;
 
-ALTER TABLE ps_v2_domains DEFAULT CHARACTER SET utf8;
+ALTER TABLE ps_v2_domains CONVERT TO CHARACTER SET utf8mb4;
 
-ALTER TABLE ps_v2_project_runtimes DEFAULT CHARACTER SET utf8;
+ALTER TABLE ps_v2_project_runtimes CONVERT TO CHARACTER SET utf8mb4;
--- .erda/migrations/.patches/patch-20210528-pipeline-base.sql
+++ .erda/migrations/.patches/patch-20210528-pipeline-base.sql
@@ -2,4 +2,4 @@ ALTER TABLE pipeline_extras MODIFY snippets MEDIUMTEXT COMMENT 'snippet 历史';
 
 ALTER TABLE pipeline_tasks MODIFY result MEDIUMTEXT COMMENT 'result';
 
-ALTER TABLE ci_v3_build_caches DEFAULT CHARACTER SET utf8;
+ALTER TABLE ci_v3_build_caches CONVERT TO CHARACTER SET utf8mb4;
