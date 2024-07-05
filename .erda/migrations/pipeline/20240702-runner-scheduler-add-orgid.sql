3ac29b019876e81512072f0343f9b935ef4a51ac
iutx <root@viper.run>
2024-07-05 13:46:55 +0800

    chore: remove testing sql

--- .erda/migrations/pipeline/20240702-runner-scheduler-add-orgid.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `dice_runner_tasks`
-    ADD COLUMN `org_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'org id' AFTER `job_id`;
-# add index
-CREATE INDEX `idx_org_id` ON `dice_runner_tasks` (`org_id`);
