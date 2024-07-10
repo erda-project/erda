52c49053b8d4d14bf5cb42ff8cd89ed6a46089f1
CeerDecy <1748788674@qq.com>
2024-07-10 09:35:35 +0800

    feat: push 'use operator or not' to collector

--- internal/tools/orchestrator/services/addon/addon_handler.go
+++ internal/tools/orchestrator/services/addon/addon_handler.go
@@ -1137,6 +1137,9 @@ func (a *Addon) basicAddonDeploy(addonIns *dbclient.AddonInstance, addonInsRouti
 	}
 	logrus.Infof("sending addon creating request, request body: %+v", *addonCreateReq)
 
+	_, ok := addonCreateReq.DiceYml.Meta["USE_OPERATOR"]
+	a.pushLog(fmt.Sprintf("use operator: %v", ok), params)
+
 	// 请求调度器
 	_, err = a.serviceGroupImpl.Create(*addonCreateReq)
 	if err != nil {
