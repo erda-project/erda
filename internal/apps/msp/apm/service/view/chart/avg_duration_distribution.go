ab1b47b9e134dac81c95ed3fdd360e1f1ca41122
joey <zchengjoey@gmail.com>
2024-07-23 17:51:36 +0800

    fix char/card,slow/error avg duration mistake

--- internal/apps/msp/apm/service/view/chart/avg_duration_distribution.go
+++ internal/apps/msp/apm/service/view/chart/avg_duration_distribution.go
@@ -36,7 +36,7 @@ type AvgDurationDistributionChart struct {
 }
 
 func (avgDuration *AvgDurationDistributionChart) GetChart(ctx context.Context) (*pb.ServiceChart, error) {
-	statement := fmt.Sprintf("SELECT sum(elapsed_sum::field)/sum(elapsed_count::field), sum(elapsed_count::field) "+
+	statement := fmt.Sprintf("SELECT avg(elapsed_sum::field), sum(elapsed_count::field) "+
 		"FROM %s "+
 		"WHERE (target_terminus_key::tag=$terminus_key OR source_terminus_key::tag=$terminus_key) "+
 		"%s "+
