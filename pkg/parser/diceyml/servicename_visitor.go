847d6b32aaa51297f9dbc2fa40ec9459d2879789
Ash <root@viper.run>
2024-09-10 14:15:07 +0800

    recover license
    Signed-off-by: Ash <root@viper.run>

--- pkg/parser/diceyml/servicename_visitor.go
+++ pkg/parser/diceyml/servicename_visitor.go
@@ -1,3 +1,18 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// 检查 service name 合法性
 package diceyml
 
 import (
@@ -5,11 +20,11 @@ import (
 	"regexp"
 )
 
-var (
-	serviceNameRegex = regexp.MustCompile(`^((([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9]))$`)
-	// serviceNameMaxLen = 14 // service name 最长长度
-	envNameRegex = regexp.MustCompile(`^([-._a-zA-Z][-._a-zA-Z0-9]*)$`)
-)
+var serviceNameRegex = regexp.MustCompile("^((([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9])\\.)*([a-z0-9]|[a-z0-9][a-z0-9\\-]*[a-z0-9]))$")
+
+// var serviceNameMaxLen = 14 // service name 最长长度
+
+var envNameRegex = regexp.MustCompile(`^([-._a-zA-Z][-._a-zA-Z0-9]*)$`)
 
 type ServiceNameVisitor struct {
 	DefaultVisitor
