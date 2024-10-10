3c633e12e0b03bd343fdf66eef87a3f5ac50227a
Ash <root@viper.run>
2024-10-10 16:14:22 +0800

    chore: remove conflict change
    Signed-off-by: Ash <root@viper.run>

--- .erda/migrations/.patches/patch-20210528-cmdb-base.sql
+++ .erda/migrations/.patches/patch-20210528-cmdb-base.sql
@@ -5,4 +5,4 @@ ALTER TABLE dice_member CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `dice_config_namespace_relation` MODIFY COLUMN `default_namespace` varchar (191) NOT NULL COMMENT '默认配置命名空间名称';
 ALTER TABLE `dice_config_namespace_relation` MODIFY COLUMN `namespace` varchar (191) NOT NULL COMMENT '配置命名空间名称';
 ALTER TABLE `dice_label_relations` MODIFY `ref_id` varchar (191) NOT NULL COMMENT '标签关联目标 id, eg: issue_id';
-ALTER TABLE `dice_member` MODIFY `user_id` varchar(191) NOT NULL DEFAULT '' COMMENT '用户Id';
+ALTER TABLE `dice_member` MODIFY `user_id` varchar(36) NOT NULL DEFAULT '' COMMENT '用户Id';
--- .erda/migrations/cmdb/20211221-dice-member-userID-varchar.sql
+++
@@ -1,1 +0,0 @@
-alter table `dice_member` modify `user_id` varchar(36) NOT NULL DEFAULT '' COMMENT '用户Id';
