95e243a173fbdb62ff4f848ae2da1712bcf340c6
mac.chenhaiqing <2538302280@qq.com>
2024-12-04 10:51:58 +0800

    add testcase

--- internal/tools/pipeline/providers/dbgc/dbgc_test.go
+++ internal/tools/pipeline/providers/dbgc/dbgc_test.go
@@ -34,7 +34,6 @@ import (
 )
 
 func TestReconciler_doPipelineDatabaseGC(t *testing.T) {
-
 	var pipelines = []spec.Pipeline{
 		{
 			PipelineBase: spec.PipelineBase{ID: 1, Status: apistructs.PipelineStatusAnalyzed},
@@ -234,5 +233,14 @@ func TestReconciler_doPipelineDatabaseGC1(t *testing.T) {
 		defer patch1.Unpatch()
 
 		r.doPipelineDatabaseGC(context.Background(), &pipelinepb.PipelinePagingRequest{PageNum: 1})
+	})
+}
+
+func TestDoDBGC(t *testing.T) {
+	p := provider{}
+	pipeline := &spec.Pipeline{}
+	needArchive := true
+	assert.Panics(t, func() {
+		p.DoDBGC(pipeline, apistructs.PipelineGCDBOption{NeedArchive: needArchive})
 	})
 }
