364d45fb06a973f38dbedc364ab4643877526db9
Ash <root@viper.run>
2025-01-15 18:11:21 +0800

    fix: deployment unit test
    Signed-off-by: Ash <root@viper.run>

--- internal/tools/orchestrator/scheduler/executor/plugins/k8s/deployment_test.go
+++ internal/tools/orchestrator/scheduler/executor/plugins/k8s/deployment_test.go
@@ -53,8 +53,10 @@ func TestNewDeployment(t *testing.T) {
 			Cpu: 0.1,
 			Mem: 512,
 		},
-		Depends:            nil,
-		Env:                nil,
+		Depends: nil,
+		Env: map[string]string{
+			apistructs.DiceWorkspaceEnvKey: apistructs.WORKSPACE_DEV,
+		},
 		Labels:             nil,
 		DeploymentLabels:   nil,
 		Selectors:          nil,
@@ -104,7 +106,9 @@ func TestNewDeployment(t *testing.T) {
 		return nil, nil
 	})
 	deploy, err := k.newDeployment(service, servicegroup)
-	assert.Equal(t, err, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 	k.setDeploymentZeroReplica(deploy)
 	assert.Equal(t, *deploy.Spec.Replicas, int32(0))
 }
@@ -524,14 +528,14 @@ func TestKubernetes_setStatelessServiceVolumes(t *testing.T) {
 
 	service.Volumes = append(service.Volumes, apistructs.Volume{
 		ContainerPath: "/opt/data/xxx02",
-		SCVolume:      apistructs.SCVolume{
+		SCVolume: apistructs.SCVolume{
 			//SourcePath: "/opt/test",
 		},
 	})
 
 	service.Volumes = append(service.Volumes, apistructs.Volume{
 		ContainerPath: "/opt/data/xxx03",
-		SCVolume:      apistructs.SCVolume{
+		SCVolume: apistructs.SCVolume{
 			//SourcePath: "test/data",
 		},
 	})
