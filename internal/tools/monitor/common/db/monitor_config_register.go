f09e3f32dc78a2e6ecb5a9f06087d4b85901ad72
CeerDecy <1748788674@qq.com>
2025-01-17 10:56:45 +0800

    feat: update LICENSE

--- internal/tools/monitor/common/db/monitor_config_register.go
+++ internal/tools/monitor/common/db/monitor_config_register.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Terminus, Inc.
+// Copyright (c) 2021 Terminus, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
