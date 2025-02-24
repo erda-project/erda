c5192059d7cc4ac1762333c57ec5103eb82bf586
Ash <root@viper.run>
2025-02-24 11:07:06 +0800

    chore: dbmigration baseline change order
    Signed-off-by: Ash <root@viper.run>

--- .erda/migrations/.patches/patch-20200528-tmc-base.sql
+++ .erda/migrations/.patches/patch-20200528-tmc-base.sql
@@ -1,12 +1,14 @@
+ALTER TABLE `sp_user` MODIFY COLUMN `username` varchar (191) DEFAULT NULL;
+ALTER TABLE `sp_stage` MODIFY COLUMN `name` varchar (191) DEFAULT NULL;
+ALTER TABLE `sp_status` MODIFY COLUMN `name` varchar (191) DEFAULT NULL;
+ALTER TABLE `sp_project` MODIFY COLUMN `identity` varchar (191) DEFAULT NULL;
+
 ALTER TABLE `sp_account` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_authentication` CONVERT TO CHARACTER SET utf8mb4;
-ALTER TABLE `sp_dashboard_block` CONVERT TO CHARACTER SET utf8mb4;
-ALTER TABLE `sp_dashboard_block_system` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_history` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_maintenance` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_metric` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_project` CONVERT TO CHARACTER SET utf8mb4;
-ALTER TABLE `sp_report_task` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_service` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_stage` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `sp_status` CONVERT TO CHARACTER SET utf8mb4;
@@ -17,8 +19,3 @@ ALTER TABLE `tb_tmc_instance` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `tb_tmc_instance_tenant` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `tb_tmc_request_relation` CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE `tb_tmc_version` CONVERT TO CHARACTER SET utf8mb4;
-
-ALTER TABLE `sp_user` MODIFY COLUMN `username` varchar (191) DEFAULT NULL;
-ALTER TABLE `sp_stage` MODIFY COLUMN `name` varchar (191) DEFAULT NULL;
-ALTER TABLE `sp_status` MODIFY COLUMN `name` varchar (191) DEFAULT NULL;
-ALTER TABLE `sp_project` MODIFY COLUMN `identity` varchar (191) DEFAULT NULL;
