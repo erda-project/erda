580a05d3ea1fb26b602150c5508ba2b37bbc1c4b
haiqing chen <2538302280@qq.com>
2025-04-29 15:38:56 +0800

    add testcase

--- internal/tools/orchestrator/scheduler/executor/plugins/k8s/addon/elasticsearch/elasticsearch_test.go
+++ internal/tools/orchestrator/scheduler/executor/plugins/k8s/addon/elasticsearch/elasticsearch_test.go
@@ -3,7 +3,9 @@ package elasticsearch
 import (
 	"fmt"
 	"github.com/erda-project/erda/apistructs"
+	"github.com/erda-project/erda/internal/tools/orchestrator/scheduler/executor/plugins/k8s/oversubscriberatio"
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 	"testing"
 )
 
@@ -65,6 +67,101 @@ func TestValidate(t *testing.T) {
 				t.Errorf("expected no error, got %v", err)
 			}
 			assert.EqualError(t, err, tc.want.Error())
+		})
+	}
+}
+
+func TestNodeSetsConvert(t *testing.T) {
+	esOperator := ElasticsearchOperator{
+		overcommit: oversubscriberatio.New(map[string]string{}),
+	}
+
+	testcases := []struct {
+		name string
+		sg   *apistructs.ServiceGroup
+	}{
+		{
+			name: "no custom ik configure",
+			sg: &apistructs.ServiceGroup{
+				Dice: apistructs.Dice{
+					Services: []apistructs.Service{
+						{
+							Volumes: []apistructs.Volume{
+								{
+									SCVolume: apistructs.SCVolume{
+										Capacity:         int32(50),
+										StorageClassName: "sc",
+										Snapshot: &apistructs.VolumeSnapshot{
+											MaxHistory: -1,
+										},
+									},
+								},
+							},
+							DeploymentLabels: map[string]string{
+								"USE_OPERATOR": "elasticsearch",
+							},
+							Env: map[string]string{
+								"DICE_WORKSPACE": "test",
+							},
+							Resources: apistructs.Resources{
+								Cpu: 0.2,
+								Mem: 2000,
+							},
+						},
+					},
+					Labels: map[string]string{
+						"dice.test": "test",
+					},
+				},
+			},
+		},
+		{
+			name: "custom ik configure",
+			sg: &apistructs.ServiceGroup{
+				Dice: apistructs.Dice{
+					Services: []apistructs.Service{
+						{
+							Name: "test",
+							Volumes: []apistructs.Volume{
+								{
+									SCVolume: apistructs.SCVolume{
+										Capacity:         int32(50),
+										StorageClassName: "sc",
+										Snapshot: &apistructs.VolumeSnapshot{
+											MaxHistory: 2,
+										},
+									},
+								},
+							},
+							DeploymentLabels: map[string]string{
+								"USE_OPERATOR": "elasticsearch",
+							},
+							Env: map[string]string{
+								"DICE_WORKSPACE": "test",
+								"EXT_DICT":       "https://test.com",
+								"EXT_STOP_DICT":  "https://test.com",
+							},
+							Resources: apistructs.Resources{
+								Cpu: 0.2,
+								Mem: 2000,
+							},
+						},
+					},
+
+					Labels: map[string]string{
+						"dice.test": "test",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := esOperator.NodeSetsConvert(tc.sg, "dice-local-volume", &corev1.NodeAffinity{})
+			if err != nil {
+
+			}
 		})
 	}
 }
