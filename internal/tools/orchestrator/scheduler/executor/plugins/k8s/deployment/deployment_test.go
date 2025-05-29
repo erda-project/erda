582b0bbe40606a17e38e3423935aa198cc6119d4
CeerDecy <1748788674@qq.com>
2025-05-29 16:29:01 +0800

    sort imports

--- internal/tools/orchestrator/scheduler/executor/plugins/k8s/deployment/deployment_test.go
+++ internal/tools/orchestrator/scheduler/executor/plugins/k8s/deployment/deployment_test.go
@@ -20,6 +20,7 @@ import (
 	"testing"
 
 	"github.com/erda-project/erda/pkg/parser/diceyml"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
