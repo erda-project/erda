fefd716d56769f8ba2403b656610788eec9f3933
CeerDecy <1748788674@qq.com>
2025-06-13 17:31:56 +0800

    polish: nice sql

--- .erda/ai-proxy/migrations/ai-proxy/20250321-mcp-server.sql
+++ .erda/ai-proxy/migrations/ai-proxy/20250321-mcp-server.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `ai_proxy_mcp_server`
 (
-    `id`                 CHAR(36)     NOT NULL COMMENT 'primary key',
+    `id`                 CHAR(36)     NOT NULL COMMENT 'Primary key',
     `name`               VARCHAR(64)  NOT NULL COMMENT 'MCP Server 名称',
     `version`            VARCHAR(64)  NOT NULL COMMENT 'MCP Server 版本',
     `description`        TEXT COMMENT '描述信息',
     `instruction`        TEXT COMMENT 'Agent Instruction',
     `endpoint`           VARCHAR(191) NOT NULL COMMENT 'MCP Server URL',
-    `config`             TEXT         NOT NULL COMMENT 'mcp tool配置信息，JSON 结构',
-    `server_config`     TEXT        NOT NULL COMMENT 'Server 配置信息',
+    `config`             TEXT         NOT NULL COMMENT 'MCP Tool 配置信息, JSON 结构',
+    `server_config`     TEXT         NOT NULL COMMENT 'Server 配置信息',
     `is_published`       TINYINT(1)   NOT NULL DEFAULT 0 COMMENT '是否发布',
     `is_default_version` TINYINT(1)   DEFAULT 0 COMMENT '是否默认版本',
     `created_at`         DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
