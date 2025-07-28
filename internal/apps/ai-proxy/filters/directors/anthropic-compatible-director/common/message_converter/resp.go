dd380a250541cf58257ec0f58a1c3dcb6ba8ab0f
sfwn <sfwnlin@gmail.com>
2025-07-28 15:15:41 +0800

    fix(ai-proxy): support tool call (#6558)

--- internal/apps/ai-proxy/filters/directors/anthropic-compatible-director/common/message_converter/resp.go
+++ internal/apps/ai-proxy/filters/directors/anthropic-compatible-director/common/message_converter/resp.go
@@ -25,11 +25,12 @@ import (
 // AnthropicResponse is non-stream response body.
 // See: https://docs.anthropic.com/en/api/messages#response-id
 type AnthropicResponse struct {
-	ID      string                         `json:"id"`
-	Type    string                         `json:"type"` // always "message"
-	Role    string                         `json:"role"` // always "assistant"
-	Content []AnthropicResponseContentPart `json:"content"`
-	Usage   AnthropicResponseUsage         `json:"usage"`
+	ID         string                         `json:"id"`
+	Type       string                         `json:"type"` // always "message"
+	Role       string                         `json:"role"` // always "assistant"
+	Content    []AnthropicResponseContentPart `json:"content"`
+	Usage      AnthropicResponseUsage         `json:"usage"`
+	StopReason string                         `json:"stop_reason"`
 }
 
 type AnthropicResponseUsage struct {
@@ -55,7 +56,7 @@ func (r AnthropicResponse) ConvertToOpenAIFormat(modelID string) (openai.ChatCom
 		choices = append(choices, openai.ChatCompletionChoice{Message: openaiMsg})
 		// Set finish reason for the last message
 		if i == len(r.Content)-1 {
-			choices[i].FinishReason = "stop"
+			choices[i].FinishReason = ConvertStopReason(r.StopReason)
 		}
 	}
 	openaiResp.Choices = choices
@@ -82,6 +83,38 @@ func ConvertAnthropicResponseMessageToOpenAIFormat(role string, anthropicContent
 		openaiMsg.Content = anthropicContentPart["text"].(string)
 	case "thinking": // https://docs.anthropic.com/en/api/messages#thinking
 		openaiMsg.ReasoningContent = anthropicContentPart["thinking"].(string)
+	case "tool_use":
+		// OpenAI Example:
+		// "message": {
+		//   "role": "assistant",
+		//   "content": null,
+		//   "tool_calls": [
+		//     {
+		//       "id": "call_kjMCowjrMJ55JVL6qLf3T5t5",
+		//       "type": "function",
+		//       "function": {
+		//         "name": "get_weather",
+		//         "arguments": "{\"location\":\"杭州, 中国\"}"
+		//       }
+		//     }
+		//   ],
+		//   "refusal": null,
+		//   "annotations": []
+		// },
+		inputJSON, err := json.Marshal(anthropicContentPart["input"])
+		if err != nil {
+			return openaiMsg, fmt.Errorf("failed to marshal tool_use input JSON: %v", err)
+		}
+		openaiMsg.ToolCalls = []openai.ToolCall{
+			{
+				ID:   anthropicContentPart["id"].(string),
+				Type: "tool_use", // always "tool_use" for anthropic tool use
+				Function: openai.FunctionCall{
+					Name:      anthropicContentPart["name"].(string),
+					Arguments: string(inputJSON),
+				},
+			},
+		}
 	}
 
 	return openaiMsg, nil
@@ -104,6 +137,7 @@ func ConvertStreamChunkDataToOpenAIChunk(anthropicDataRaw []byte, inputMsgInfo A
 	}
 
 	switch rawObj["type"].(string) {
+
 	case "message_start":
 		// Example:
 		// event: message_start
@@ -116,7 +150,57 @@ func ConvertStreamChunkDataToOpenAIChunk(anthropicDataRaw []byte, inputMsgInfo A
 		gotMsgInfo.Role = rawObj["message"].(map[string]any)["role"].(string)
 		gotMsgInfo.Usage.InputTokens = int(rawObj["message"].(map[string]any)["usage"].(map[string]any)["input_tokens"].(float64))
 		return &gotMsgInfo, nil, nil
+
+	case "content_block_start":
+		// Example:
+		// text:
+		// event: content_block_start
+		// data: {"type":"content_block_start","index":0,"content_block":{"type":"text","text":""}}
+		// tool:
+		// event: content_block_start
+		// data: {"type":"content_block_start","index":1,"content_block":{"type":"tool_use","id":"toolu_01T1x1fJ34qAmk2tNTrN7Up6","name":"get_weather","input":{}}}
+		openaiChunk := openai.ChatCompletionStreamResponse{
+			ID:      inputMsgInfo.ID,
+			Object:  "chat.completion.chunk", // fixed
+			Created: time.Now().Unix(),
+			Model:   inputMsgInfo.Model,
+			Choices: []openai.ChatCompletionStreamChoice{
+				{
+					Index: 0,
+					Delta: openai.ChatCompletionStreamChoiceDelta{
+						Role: inputMsgInfo.Role,
+					},
+				},
+			},
+		}
+
+		switch rawObj["content_block"].(map[string]any)["type"].(string) {
+		case "text":
+			// For text content block, we need to send a chunk with role but no content.
+			// OpenAI Example:
+			// data: {"id":"chatcmpl-Bx64y0qCiwYAxZTl6Zvap5s4M42GS","object":"chat.completion.chunk","created":1753424420,"model":"gpt-4.1-2025-04-14","system_fingerprint":"fp_07e970ab25","choices":[{"index":0,"delta":{"role":"assistant","content":"","refusal":null},"logprobs":null,"finish_reason":null}],"usage":null}
+			return nil, &openaiChunk, nil
+		case "tool_use":
+			// OpenAI Example:
+			// data: {"id":"chatcmpl-Bx6B6JuBCCcfv6weh9OnZ7jjMgaou","object":"chat.completion.chunk","created":1753424800,"model":"gpt-4.1-2025-04-14","system_fingerprint":"fp_07e970ab25","choices":[{"index":0,"delta":{"role":"assistant","content":null,"tool_calls":[{"index":0,"id":"call_ChoPaqR61B15NXkGXXPFPQvZ","type":"function","function":{"name":"get_weather","arguments":""}}],"refusal":null},"logprobs":null,"finish_reason":null}],"usage":null}
+			openaiChunk.Choices[0].Delta.ToolCalls = []openai.ToolCall{
+				{
+					Index: &[]int{0}[0],
+					ID:    rawObj["content_block"].(map[string]any)["id"].(string),
+					Type:  "function", // always "function" for tool use
+					Function: openai.FunctionCall{
+						Name: rawObj["content_block"].(map[string]any)["name"].(string),
+					},
+				},
+			}
+			return nil, &openaiChunk, nil
+
+		default:
+			return nil, nil, nil
+		}
+
 	case "content_block_delta":
+		// see: https://docs.anthropic.com/en/docs/build-with-claude/streaming#content-block-delta-types
 		openaiChunk := openai.ChatCompletionStreamResponse{
 			ID:      inputMsgInfo.ID,
 			Object:  "chat.completion.chunk", // fixed
@@ -138,10 +222,31 @@ func ConvertStreamChunkDataToOpenAIChunk(anthropicDataRaw []byte, inputMsgInfo A
 			openaiChunk.Choices[0].Delta.Content = deltaObj["text"].(string)
 		case "thinking_delta":
 			openaiChunk.Choices[0].Delta.ReasoningContent = deltaObj["thinking"].(string)
+		case "input_json_delta":
+			// Anthropic Example:
+			// event: content_block_delta
+			// data: {"type":"content_block_delta","index":1,"delta":{"type":"input_json_delta","partial_json":""}}
+			// OpenAI Example:
+			// data: {"id":"chatcmpl-Bx6B6JuBCCcfv6weh9OnZ7jjMgaou","object":"chat.completion.chunk","created":1753424800,"model":"gpt-4.1-2025-04-14","system_fingerprint":"fp_07e970ab25","choices":[{"index":0,"delta":{"tool_calls":[{"index":0,"function":{"arguments":"location"}}]},"logprobs":null,"finish_reason":null}],"usage":null}
+			openaiChunk.Choices[0].Delta.ToolCalls = []openai.ToolCall{
+				{
+					Index: &[]int{0}[0],
+					Function: openai.FunctionCall{
+						Arguments: deltaObj["partial_json"].(string), // partial JSON string
+					},
+				},
+			}
 		default:
 			return nil, nil, nil
 		}
 		return nil, &openaiChunk, nil
+
+	case "content_block_stop":
+		// Example:
+		// event: content_block_stop
+		// data: {"type":"content_block_stop","index":1}
+		return nil, nil, nil
+
 	case "message_delta":
 		// Example:
 		// event: message_delta
@@ -152,6 +257,8 @@ func ConvertStreamChunkDataToOpenAIChunk(anthropicDataRaw []byte, inputMsgInfo A
 		outputTokens := int(rawObj["usage"].(map[string]any)["output_tokens"].(float64))
 		inputMsgInfo.Usage.OutputTokens = outputTokens
 
+		stopReason := rawObj["delta"].(map[string]any)["stop_reason"].(string)
+
 		openaiChunk := openai.ChatCompletionStreamResponse{
 			ID:      inputMsgInfo.ID,
 			Object:  "chat.completion.chunk",
@@ -161,11 +268,12 @@ func ConvertStreamChunkDataToOpenAIChunk(anthropicDataRaw []byte, inputMsgInfo A
 				{
 					Index:        0,
 					Delta:        openai.ChatCompletionStreamChoiceDelta{},
-					FinishReason: "stop", // always stop
+					FinishReason: ConvertStopReason(stopReason),
 				},
 			},
 		}
 		return &inputMsgInfo, &openaiChunk, nil
+
 	case "message_stop":
 		// Example:
 		// event: message_stop
@@ -189,5 +297,20 @@ func ConvertStreamChunkDataToOpenAIChunk(anthropicDataRaw []byte, inputMsgInfo A
 
 	default:
 		return nil, nil, nil
+	}
+}
+
+func ConvertStopReason(stopReason string) openai.FinishReason {
+	// anthropic: https://docs.anthropic.com/en/api/messages#response-stop-reason
+	// openai: https://platform.openai.com/docs/api-reference/chat/object#chat/object-choices
+	switch stopReason {
+	case "tool_use":
+		return openai.FinishReasonToolCalls
+	case "refusal":
+		return openai.FinishReasonContentFilter
+	case "max_tokens":
+		return openai.FinishReasonLength
+	default:
+		return openai.FinishReasonStop
 	}
 }
