1b32c06a010c5f813a1b12e1be11c42280929ac9
sfwn <sfwnlin@gmail.com>
2025-09-26 17:04:03 +0800

    update integration-test cases for thinking

--- internal/apps/ai-proxy/route/filters/request/thinking-handler/encoders/impl/service-provider/aliyun_bailian_test.go
+++ internal/apps/ai-proxy/route/filters/request/thinking-handler/encoders/impl/service-provider/aliyun_bailian_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package service_provider
 
 import (
@@ -6,14 +20,28 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"google.golang.org/protobuf/types/known/structpb"
+
+	metadatapb "github.com/erda-project/erda-proto-go/apps/aiproxy/metadata/pb"
 	modelproviderpb "github.com/erda-project/erda-proto-go/apps/aiproxy/model_provider/pb"
+	"github.com/erda-project/erda/internal/apps/ai-proxy/common/common_types"
 	"github.com/erda-project/erda/internal/apps/ai-proxy/common/ctxhelper"
 	"github.com/erda-project/erda/internal/apps/ai-proxy/route/filters/request/thinking-handler/types"
 )
 
+func newProviderWithServiceType(t common_types.ServiceProviderType) *modelproviderpb.ModelProvider {
+	return &modelproviderpb.ModelProvider{
+		Metadata: &metadatapb.Metadata{
+			Public: map[string]*structpb.Value{
+				"service_provider_type": structpb.NewStringValue(t.String()),
+			},
+		},
+	}
+}
+
 func TestBailianThinkingEncoder(t *testing.T) {
 	ctx := ctxhelper.InitCtxMapIfNeed(context.Background())
-	ctxhelper.PutModelProvider(ctx, &modelproviderpb.ModelProvider{Type: types.ModelProviderTypeAliyunDashScope.String()})
+	ctxhelper.PutModelProvider(ctx, newProviderWithServiceType(common_types.ServiceProviderTypeAliyunBailian))
 
 	encoder := &BailianThinkingEncoder{}
 
@@ -29,13 +57,13 @@ func TestBailianThinkingEncoder(t *testing.T) {
 
 func TestBailianThinkingEncoder_CanEncodeVariants(t *testing.T) {
 	ctx := ctxhelper.InitCtxMapIfNeed(context.Background())
-	ctxhelper.PutModelProvider(ctx, &modelproviderpb.ModelProvider{Type: types.ModelProviderTypeAliyunBailian.String()})
+	ctxhelper.PutModelProvider(ctx, newProviderWithServiceType(common_types.ServiceProviderTypeAliyunBailian))
 
 	encoder := &BailianThinkingEncoder{}
 
 	assert.True(t, encoder.CanEncode(ctx))
 
 	ctx = ctxhelper.InitCtxMapIfNeed(context.Background())
-	ctxhelper.PutModelProvider(ctx, &modelproviderpb.ModelProvider{Type: "Unknown"})
+	ctxhelper.PutModelProvider(ctx, &modelproviderpb.ModelProvider{})
 	assert.False(t, encoder.CanEncode(ctx))
 }
--- internal/apps/ai-proxy/route/filters/request/thinking-handler/encoders/impl/service-provider/volcengine_ark_test.go
+++ internal/apps/ai-proxy/route/filters/request/thinking-handler/encoders/impl/service-provider/volcengine_ark_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package service_provider
 
 import (
@@ -6,14 +20,28 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"google.golang.org/protobuf/types/known/structpb"
+
+	metadatapb "github.com/erda-project/erda-proto-go/apps/aiproxy/metadata/pb"
 	modelproviderpb "github.com/erda-project/erda-proto-go/apps/aiproxy/model_provider/pb"
+	"github.com/erda-project/erda/internal/apps/ai-proxy/common/common_types"
 	"github.com/erda-project/erda/internal/apps/ai-proxy/common/ctxhelper"
 	"github.com/erda-project/erda/internal/apps/ai-proxy/route/filters/request/thinking-handler/types"
 )
 
+func newVolcengineProvider(serviceType common_types.ServiceProviderType) *modelproviderpb.ModelProvider {
+	return &modelproviderpb.ModelProvider{
+		Metadata: &metadatapb.Metadata{
+			Public: map[string]*structpb.Value{
+				"service_provider_type": structpb.NewStringValue(serviceType.String()),
+			},
+		},
+	}
+}
+
 func TestVolcengineThinkingEncoder(t *testing.T) {
 	ctx := ctxhelper.InitCtxMapIfNeed(context.Background())
-	ctxhelper.PutModelProvider(ctx, &modelproviderpb.ModelProvider{Type: types.ModelProviderTypeVolcengine.String()})
+	ctxhelper.PutModelProvider(ctx, newVolcengineProvider(common_types.ServiceProviderTypeVolcengineArk))
 
 	encoder := &VolcengineThinkingEncoder{}
 
@@ -28,7 +56,7 @@ func TestVolcengineThinkingEncoder(t *testing.T) {
 
 func TestVolcengineThinkingEncoder_CanEncode_False(t *testing.T) {
 	ctx := ctxhelper.InitCtxMapIfNeed(context.Background())
-	ctxhelper.PutModelProvider(ctx, &modelproviderpb.ModelProvider{Type: "Unknown"})
+	ctxhelper.PutModelProvider(ctx, &modelproviderpb.ModelProvider{})
 
 	encoder := &VolcengineThinkingEncoder{}
 
