673262df05c29ac6e2d91b4f18f24e1938e7c477
sfwn <sfwnlin@gmail.com>
2025-10-22 16:25:56 +0800

    fix copyright

--- internal/apps/ai-proxy/handlers/handler_model_provider/handler_test.go
+++ internal/apps/ai-proxy/handlers/handler_model_provider/handler_test.go
@@ -1,15 +1,30 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package handler_model_provider
 
 import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/structpb"
+
 	clientpb "github.com/erda-project/erda-proto-go/apps/aiproxy/client/pb"
 	metadatapb "github.com/erda-project/erda-proto-go/apps/aiproxy/metadata/pb"
-	pb "github.com/erda-project/erda-proto-go/apps/aiproxy/model_provider/pb"
+	"github.com/erda-project/erda-proto-go/apps/aiproxy/model_provider/pb"
 	"github.com/erda-project/erda/internal/apps/ai-proxy/common/ctxhelper"
-	"github.com/stretchr/testify/assert"
-	structpb "google.golang.org/protobuf/types/known/structpb"
 )
 
 func TestDesensitizeProvider(t *testing.T) {
