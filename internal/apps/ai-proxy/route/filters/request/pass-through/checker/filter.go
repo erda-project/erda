fdcbe9e2d045d4ec8d1d37425a4e2670c9b8101d
sfwn <sfwnlin@gmail.com>
2025-12-16 11:42:03 +0800

    fix(ai-proxy): add missing pure-proxy apiStyleConfig for aliyun-bailian (#6658)

--- internal/apps/ai-proxy/route/filters/request/pass-through/checker/filter.go
+++ internal/apps/ai-proxy/route/filters/request/pass-through/checker/filter.go
@@ -16,13 +16,9 @@ package checker
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http/httputil"
-	"strings"
 
-	"github.com/erda-project/erda/internal/apps/ai-proxy/models/client_token"
 	"github.com/erda-project/erda/internal/apps/ai-proxy/route/filter_define"
-	"github.com/erda-project/erda/internal/apps/ai-proxy/vars"
 )
 
 const name = "check-pass-through"
@@ -43,10 +39,10 @@ var Creator filter_define.RequestRewriterCreator = func(_ string, _ json.RawMess
 }
 
 func (f *Checker) OnProxyRequest(pr *httputil.ProxyRequest) error {
-	// only support auth by client-token, reject client-ak
-	token := vars.TrimBearer(pr.Out.Header.Get("Authorization"))
-	if !strings.HasPrefix(token, client_token.TokenPrefix) {
-		return fmt.Errorf("pass-through usage only support auth by token")
-	}
+	//// only support auth by client-token, reject client-ak
+	//token := vars.TrimBearer(pr.Out.Header.Get("Authorization"))
+	//if !strings.HasPrefix(token, client_token.TokenPrefix) {
+	//	return fmt.Errorf("pass-through usage only support auth by token")
+	//}
 	return nil
 }
