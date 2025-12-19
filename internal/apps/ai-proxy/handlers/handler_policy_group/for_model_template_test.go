579ee181ebb04fb4ec3389416a3c8cb2e317c62d
sfwn <sfwnlin@gmail.com>
2025-12-19 18:43:25 +0800

    make test-imports & test-copyriht great again

--- internal/apps/ai-proxy/handlers/handler_policy_group/for_model_template_test.go
+++ internal/apps/ai-proxy/handlers/handler_policy_group/for_model_template_test.go
@@ -1,12 +1,27 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package handler_policy_group
 
 import (
 	"testing"
 	"time"
 
+	"google.golang.org/protobuf/types/known/timestamppb"
+
 	policypb "github.com/erda-project/erda-proto-go/apps/aiproxy/policy_group/pb"
 	"github.com/erda-project/erda/internal/apps/ai-proxy/common/common_types"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func Test_buildBranchesFromInstances(t *testing.T) {
