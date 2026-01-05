e72899d8d03b4369708d4e2b54faf30e0105a07e
CeerDecy <1748788674@qq.com>
2026-01-05 10:56:13 +0800

    fix: request ID was not filtered

--- internal/apps/ai-proxy/route/filters/request/mcp-server-request/filter.go
+++ internal/apps/ai-proxy/route/filters/request/mcp-server-request/filter.go
@@ -43,6 +43,7 @@ import (
 const Name = "mcp-server-request"
 
 const TerminusRequestIdHeader = "Terminus-Request-Id"
+const RequestIdHeader = "__request_id__"
 
 const NotificationInitializedMethod = "notifications/initialized"
 
@@ -202,7 +203,9 @@ func (f *Filter) OnMessage(logger logs.Logger, ctx context.Context, name string,
 		return err
 	}
 
-	if err = filterTerminusTraceId(rawBody, req); err != nil {
+	logger.Infof("raw body: %s", string(rawBody))
+
+	if rawBody, err = filterTerminusTraceId(rawBody, req); err != nil {
 		logger.Errorf("filter terminus trace id failed: %v", err)
 		return err
 	}
@@ -318,30 +321,38 @@ func parseEndpoint(endpoint string) (*ParsedEndpoint, error) {
 	}, nil
 }
 
-func filterTerminusTraceId(body []byte, rawReq *http.Request) error {
+func filterTerminusTraceId(body []byte, rawReq *http.Request) ([]byte, error) {
 	var req request.Request
 	if err := json.Unmarshal(body, &req); err != nil {
-		return err
+		return body, err
+	}
+	if req.Method != request.ToolCallMethod {
+		return body, nil
 	}
 
 	var traceId string
 
 	if id, exist := req.Params.Arguments[TerminusRequestIdHeader]; exist {
+		if _, ok := req.Params.Arguments[RequestIdHeader]; ok {
+			delete(req.Params.Arguments, RequestIdHeader)
+		}
+
 		traceId = fmt.Sprintf("%v", id)
 		delete(req.Params.Arguments, TerminusRequestIdHeader)
-	}
-	jsonData, err := json.Marshal(req)
-	if err != nil {
-		return err
-	}
-	rawReq.Body = io.NopCloser(bytes.NewReader(jsonData))
-	rawReq.ContentLength = int64(len(jsonData))
 
-	if traceId != "" {
-		rawReq.Header.Set(TerminusRequestIdHeader, traceId)
-	}
+		jsonData, err := json.Marshal(req)
+		if err != nil {
+			return body, err
+		}
+		rawReq.Body = io.NopCloser(bytes.NewReader(jsonData))
+		rawReq.ContentLength = int64(len(jsonData))
 
-	return nil
+		if traceId != "" {
+			rawReq.Header.Set(TerminusRequestIdHeader, traceId)
+		}
+		return jsonData, nil
+	}
+	return body, nil
 }
 
 func filterInitNotification(ctx context.Context, body []byte, client dao.DAO, name string, tag string) ([]byte, error) {
